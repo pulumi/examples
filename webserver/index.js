4f5fe5cab85f65c6b24972512b6f4e93833eebcb
Donna Malayeri <lindydonna@users.noreply.github.com>
2018-03-23 11:51:14 -0700

    Address review feedback

--- webserver/index.js
+++ webserver/index.js
@@ -3,8 +3,12 @@
 const aws = require("@pulumi/aws");
 
 let size = "t2.micro";    // t2.micro is available in the AWS free tier
+
 let ami  = "ami-7172b611" // AMI for Amazon Linux in us-west-2 (Oregon)
-// let ami  = "ami-6869aa05" // AMI for Amazon Linux in us-east-1 (Virginia)
+// let ami  = "ami-6869aa05" // AMI for us-east-1 (Virginia)
+// let ami  = "ami-31490d51" // AMI for us-west-1 (California)
+// let ami  = "ami-f9dd458a" // AMI for eu-west-1 (Ireland)
+// let ami  = "ami-ea26ce85" // AMI for eu-central-1 (Frankfurt)
 
 // create a new security group for port 80
 let group = new aws.ec2.SecurityGroup("web-secgrp", { 
