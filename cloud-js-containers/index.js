50c780c3a593c787ad54772c445c0b8c3e129c07
Donna Malayeri <lindydonna@users.noreply.github.com>
2018-05-22 19:17:30 -0700

    Add container example (#59)
    * Add container example

---
+++ cloud-js-containers/index.js
@@ -0,0 +1,15 @@
+const cloud = require("@pulumi/cloud-aws");
+
+let service = new cloud.Service("pulumi-nginx", {
+    containers: {
+        nginx: {
+            build: "./app",
+            memory: 128,
+            ports: [{ port: 80 }],
+        },
+    },
+    replicas: 2,
+});
+
+// export just the hostname property of the container frontend
+exports.hostname = service.defaultEndpoint.apply(e => `http://${e.hostname}`);
