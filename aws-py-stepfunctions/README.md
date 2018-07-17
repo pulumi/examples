a295ef59d4fc594bf57a6538d05e5b46080616dc
Joe Duffy <joe@pulumi.com>
2018-07-17 13:24:04 -0700

    Add an example of AWS StepFunctions in Python (#97)

---
+++ aws-py-stepfunctions/README.md
@@ -0,0 +1,21 @@
+# AWS Step Functions (Python)
+
+A basic example that demonstrates using AWS Step Functions with a Lambda function, written in Python.
+
+```
+# Install dependencies
+$ pip install -r ./requirements.txt
+
+# Create and configure a new stack
+$ pulumi stack init stepfunctions-dev
+$ pulumi config set aws:region us-east-2
+
+# Preview and run the deployment
+$ pulumi update
+
+# Start execution using the AWS CLI (or from the console at https://console.aws.amazon.com/states)
+$ aws stepfunctions start-execution --state-machine-arn $(pulumi stack output state_machine_arn)
+
+# Remove the app and its stack
+$ pulumi destroy && pulumi stack rm -y
+```
---
+++ aws-py-stepfunctions/iam.py
@@ -0,0 +1,66 @@
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
+from pulumi_aws import config, iam
+
+lambda_role = iam.Role('lambdaRole',
+    assume_role_policy="""{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "sts:AssumeRole",
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                },
+                "Effect": "Allow",
+                "Sid": ""
+            }
+        ]
+    }"""
+)
+
+lambda_role_policy = iam.RolePolicy('lambdaRolePolicy',
+    role=lambda_role.id,
+    policy="""{
+        "Version": "2012-10-17",
+        "Statement": [{
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "arn:aws:logs:*:*:*"
+        }]
+    }"""
+)
+
+sfn_role = iam.Role('sfnRole',
+    assume_role_policy="""{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "states.%s.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+        ]
+    }""" % config.region
+)
+
+sfn_role_policy = iam.RolePolicy('sfnRolePolicy',
+    role=sfn_role.id,
+    policy="""{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "lambda:InvokeFunction"
+                ],
+                "Resource": "*"
+            }
+        ]
+    }"""
+)
