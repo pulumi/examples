c4e6c648c0702ad99f4b0adebafa974c3de2e470
Pat Gavlin <pgavlin@gmail.com>
2018-08-15 16:24:32 -0700

    Add an EKS example. (#116)
    These changes add an example that deploys all of the bits necessary to
    run an EKS cluster, including:
    - a VPC in which the cluster will run
    - the various security groups and roles required for the cluster and its
      worker nodes
    - the cluster itself
    - an autoscaling group that manages the worker nodes
    - the Kubernetes resources necessary to allow the worker nodes to
      join the cluster
    - the Kubernetes resources necessary to provide an EBS-backed StorageClass
      in the cluster
    - a deployment of the Kubernetes Dashboard into the cluster
    
    The example exports a JSON kubeconfig that can be used to connect to the
    cluster. This config can be saved by running
    `pulumi stack output kubeconfig >kubeconfig.json`.

---
+++ aws-ts-eks/index.ts
@@ -0,0 +1,27 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as awsinfra from "@pulumi/aws-infra";
+import * as k8s from "@pulumi/kubernetes";
+
+import { EKSCluster } from "./cluster";
+
+const config = new pulumi.Config();
+const instanceType = (config.get("instanceType") || "t2.medium") as aws.ec2.InstanceType;
+
+// Create a VPC for our cluster.
+const network = new awsinfra.Network("eksNetwork");
+
+// Create the EKS cluster itself, including a "gp2"-backed StorageClass and a dpeloyment of the Kubernetes dashboard.
+const cluster = new EKSCluster("eksCluster", {
+    vpcId: network.vpcId,
+    subnetIds: network.subnetIds,
+    instanceType: instanceType,
+    desiredCapacity: 2,
+    minSize: 1,
+    maxSize: 2,
+    storageClasses: "gp2",
+    deployDashboard: true,
+});
+
+// Export the cluster's kubeconfig.
+export const kubeconfig = cluster.kubeconfig;
---
+++ aws-ts-eks/serviceRole.ts
@@ -0,0 +1,76 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as crypto from "crypto";
+
+// sha1hash returns a partial SHA1 hash of the input string.
+function sha1hash(s: string): string {
+    const shasum: crypto.Hash = crypto.createHash("sha1");
+    shasum.update(s);
+    // Limit the size of hashes to ensure we generate shorter/ resource names.
+    return shasum.digest("hex").substring(0, 8);
+}
+
+/**
+ * ServiceRoleArgs describe the parameters to a ServiceRole component.
+ */
+export interface ServiceRoleArgs {
+    /**
+     * The service associated with this role.
+     */
+    readonly service: pulumi.Input<string>;
+    /**
+     * The description of the role.
+     */
+    readonly description?: pulumi.Input<string>;
+    /**
+     * One or more managed policy ARNs to attach to this role.
+     */
+    readonly managedPolicyArns?: string[];
+}
+
+/**
+ * The ServiceRole component creates an IAM role for a particular service and attaches to it a list of well-known
+ * managed policies.
+ */
+export class ServiceRole extends pulumi.ComponentResource {
+    // The service role.
+    public readonly role: pulumi.Output<aws.iam.Role>;
+
+    /**
+     * Create a new ServiceRole.
+     *
+     * @param name The _unique_ name of this component.
+     * @param args The arguments for this cluster.
+     * @param opts A bag of options that control this copmonent's behavior.
+     */
+    constructor(name: string, args: ServiceRoleArgs, opts?: pulumi.ResourceOptions) {
+        super("ServiceRole", name, args, opts);
+
+        const assumeRolePolicy = pulumi.output(args.service).apply(service => JSON.stringify({
+            Version: "2012-10-17",
+            Statement: [{
+                Action: [
+                    "sts:AssumeRole",
+                ],
+                Effect: "Allow",
+                Principal: {
+                    Service: [ service ],
+                },
+            }],
+        }));
+        const role = new aws.iam.Role(`${name}-role`, {
+            description: args.description,
+            assumeRolePolicy: assumeRolePolicy,
+        }, { parent: this });
+        const rolePolicyAttachments = [];
+        for (const policy of (args.managedPolicyArns || [])) {
+            rolePolicyAttachments.push(new aws.iam.RolePolicyAttachment(`${name}-${sha1hash(policy)}`, {
+                policyArn: policy,
+                role: role,
+            }, { parent: this }));
+        }
+        this.role = pulumi.all([role.arn, ...rolePolicyAttachments.map(r => r.id)]).apply(() => role);
+
+        this.registerOutputs({ role: this.role });
+    }
+}
