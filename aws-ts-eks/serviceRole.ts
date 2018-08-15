a1c2b312e9d429d3c4a780bb2a09c0e5ab6cee0c
Pat Gavlin <pat@pulumi.com>
2018-08-14 23:21:11 -0700

    Comment the service role component.

--- aws-ts-eks/serviceRole.ts
+++ aws-ts-eks/serviceRole.ts
@@ -10,6 +10,9 @@ function sha1hash(s: string): string {
     return shasum.digest("hex").substring(0, 8);
 }
 
+/**
+ * ServiceRoleArgs describe the parameters to a ServiceRole component.
+ */
 export interface ServiceRoleArgs {
     /**
      * The service associated with this role.
@@ -25,10 +28,21 @@ export interface ServiceRoleArgs {
     readonly managedPolicyArns?: string[];
 }
 
+/**
+ * The ServiceRole component creates an IAM role for a particular service and attaches to it a list of well-known
+ * managed policies.
+ */
 export class ServiceRole extends pulumi.ComponentResource {
     // The service role.
     public readonly role: pulumi.Output<aws.iam.Role>;
 
+    /**
+     * Create a new ServiceRole.
+     *
+     * @param name The _unique_ name of this component.
+     * @param args The arguments for this cluster.
+     * @param opts A bag of options that control this copmonent's behavior.
+     */
     constructor(name: string, args: ServiceRoleArgs, opts?: pulumi.ResourceOptions) {
         super("ServiceRole", name, args, opts);
 
