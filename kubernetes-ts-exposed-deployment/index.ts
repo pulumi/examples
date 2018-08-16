48cfcff1a246a2281723ae943fff0f76ed2ea48d
Alex Clemmer <clemmer.alexander@gmail.com>
2018-08-16 15:39:37 -0700

    Add "hello world" Kubernetes nginx example

---
+++ kubernetes-ts-exposed-deployment/index.ts
@@ -0,0 +1,39 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as k8s from "@pulumi/kubernetes";
+
+// Minikube does not implement services of type `LoadBalancer`; require the user to specify if we're
+// running on minikube, and if so, create only services of type ClusterIP.
+let config = new pulumi.Config("nginx");
+let isMinikube = config.require("isMinikube");
+
+// nginx container, replicated 1 time.
+const appName = "nginx";
+const appLabels = { app: appName };
+const nginx = new k8s.apps.v1beta1.Deployment(appName, {
+    spec: {
+        selector: { matchLabels: appLabels },
+        replicas: 1,
+        template: {
+            metadata: { labels: appLabels },
+            spec: { containers: [{ name: appName, image: "nginx:1.15-alpine" }] }
+        }
+    }
+});
+
+// Allocate an IP to the nginx Deployment.
+const frontend = new k8s.core.v1.Service(appName, {
+    metadata: { labels: nginx.spec.apply(spec => spec.template.metadata.labels) },
+    spec: {
+        type: isMinikube === "true" ? "ClusterIP" : "LoadBalancer",
+        ports: [{ port: 80, targetPort: 80, protocol: "TCP" }],
+        selector: appLabels
+    }
+});
+
+// When "done", this will print the public IP.
+export let frontendIp: pulumi.Output<string>;
+if (isMinikube === "true") {
+    frontendIp = frontend.spec.apply(spec => spec.clusterIP);
+} else {
+    frontendIp = frontend.status.apply(status => status.loadBalancer.ingress[0].ip);
+}
