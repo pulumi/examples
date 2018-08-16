4020ecfa58c6c381f9487c735cf96d0035f75a30
Pat Gavlin <pat@pulumi.com>
2018-08-15 17:53:46 -0700

    Use the EKS package.
    Just what it says on the tin. These changes also elminite the
    instanceType config var.

--- aws-ts-eks/index.ts
+++ aws-ts-eks/index.ts
@@ -1,21 +1,14 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
 import * as awsinfra from "@pulumi/aws-infra";
-import * as k8s from "@pulumi/kubernetes";
-
-import { EKSCluster } from "./cluster";
-
-const config = new pulumi.Config();
-const instanceType = (config.get("instanceType") || "t2.medium") as aws.ec2.InstanceType;
+import * as eks from "@pulumi/eks";
 
 // Create a VPC for our cluster.
-const network = new awsinfra.Network("eksNetwork");
+const vpc = new awsinfra.Network("vpc");
 
 // Create the EKS cluster itself, including a "gp2"-backed StorageClass and a dpeloyment of the Kubernetes dashboard.
-const cluster = new EKSCluster("eksCluster", {
+const cluster = new EKSCluster("cluster", {
     vpcId: network.vpcId,
     subnetIds: network.subnetIds,
-    instanceType: instanceType,
+    instanceType: "t2.medium",
     desiredCapacity: 2,
     minSize: 1,
     maxSize: 2,
