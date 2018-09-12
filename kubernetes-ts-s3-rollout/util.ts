2aa00c3e482298cc73a84b2e084c519e39344422
Alex Clemmer <clemmer.alexander@gmail.com>
2018-09-11 21:46:51 -0700

    Add "triggering a rollout by updating S3" demo

---
+++ kubernetes-ts-s3-rollout/util.ts
@@ -0,0 +1,21 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+// Utility function that creates a container that `curl`s a URL, placing it in a file in some shared
+// volume, namely at `${mount.mountPath}/${fileName}`. For example, `mount.mountPath` might be the
+// nginx config path, `/etc/nginx/conf.d`, and `filename` might be the default file, `default.conf`.
+// This container would them place them in the shared volume at `/etc/nginx/conf.d/default.conf`.
+export function curl(
+    url: pulumi.Output<string>,
+    fileName: string,
+    mount: { name: pulumi.Input<string>; mountPath: pulumi.Input<string> }
+) {
+    return {
+        name: "curl",
+        image: "byrnedo/alpine-curl",
+        args: pulumi
+            .all([url, mount.mountPath])
+            .apply(([url, mountPath]) => ["-o", `${mountPath}/${fileName}`, "-sL", url]),
+        volumeMounts: [mount]
+    };
+}
