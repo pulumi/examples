013db955b3c47ff9bff96710a24220ccfada3f86
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-03-07 17:09:41 -0800

    remove

--- aws-ts-resources/index.ts
+++ aws-ts-resources/index.ts
@@ -250,39 +250,39 @@ const stream = new aws.kinesis.Stream("mystream", {
 // S3
 const bucket = new aws.s3.Bucket("my-bucket");
 
-const bucketMetric = new aws.s3.BucketMetric("my-bucket-metric", {
-    bucket: bucket.bucket
-});
+// const bucketMetric = new aws.s3.BucketMetric("my-bucket-metric", {
+//     bucket: bucket.bucket
+// });
 
-const bucketNotification = new aws.s3.BucketNotification("my-bucket-notification", {
-    bucket: bucket.bucket
-});
+// const bucketNotification = new aws.s3.BucketNotification("my-bucket-notification", {
+//     bucket: bucket.bucket
+// });
 
 // const bucketObject = new aws.s3.BucketObject("my-bucket-object", {
 //     bucket: bucket.bucket,
 //     content: "hello world"
 // });
 
-const bucketPolicy = new aws.s3.BucketPolicy("my-bucket-policy", {
-    bucket: bucket.bucket,
-    policy: bucket.bucket.apply(publicReadPolicyForBucket)
-})
+// const bucketPolicy = new aws.s3.BucketPolicy("my-bucket-policy", {
+//     bucket: bucket.bucket,
+//     policy: bucket.bucket.apply(publicReadPolicyForBucket)
+// })
 
-function publicReadPolicyForBucket(bucketName: string) {
-    return JSON.stringify({
-        Version: "2012-10-17",
-        Statement: [{
-            Effect: "Allow",
-            Principal: "*",
-            Action: [
-                "s3:GetObject"
-            ],
-            Resource: [
-                `arn:aws:s3:::${bucketName}/*` // policy refers to bucket name explicitly
-            ]
-        }]
-    });
-}
+// function publicReadPolicyForBucket(bucketName: string) {
+//     return JSON.stringify({
+//         Version: "2012-10-17",
+//         Statement: [{
+//             Effect: "Allow",
+//             Principal: "*",
+//             Action: [
+//                 "s3:GetObject"
+//             ],
+//             Resource: [
+//                 `arn:aws:s3:::${bucketName}/*` // policy refers to bucket name explicitly
+//             ]
+//         }]
+//     });
+// }
 
 // SQS
 const queue = new aws.sqs.Queue("myqueue");
