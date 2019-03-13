eae7bd729e0b91519715e9060f9d9736aeef16d8
Alex Clemmer <clemmer.alexander@gmail.com>
2019-03-13 11:36:27 -0700

    WIP: admission policy demo

--- kubernetes-ts-configmap-rollout/index.ts
+++ kubernetes-ts-configmap-rollout/index.ts
@@ -1,11 +1,8 @@
 import * as fs from "fs";
 import * as pulumi from "@pulumi/pulumi";
+import { policy } from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
-
-// Minikube does not implement services of type `LoadBalancer`; require the user to specify if we're
-// running on minikube, and if so, create only services of type ClusterIP.
-let config = new pulumi.Config();
-let isMinikube = config.require("isMinikube");
+import * as outputs from "@pulumi/kubernetes/types/output";
 
 const appName = "nginx";
 const appLabels = { app: appName };
@@ -14,7 +11,7 @@ const appLabels = { app: appName };
 // `default.conf` file.
 const nginxConfig = new k8s.core.v1.ConfigMap(appName, {
     metadata: { labels: appLabels },
-    data: { "default.conf": fs.readFileSync("default.conf").toString() }
+    data: { "default.conf": fs.readFileSync("default.conf").toString() },
 });
 const nginxConfigName = nginxConfig.metadata.apply(m => m.name);
 
@@ -31,29 +28,38 @@ const nginx = new k8s.apps.v1beta1.Deployment(appName, {
                     {
                         image: "nginx:1.13.6-alpine",
                         name: "nginx",
-                        volumeMounts: [{ name: "nginx-configs", mountPath: "/etc/nginx/conf.d" }]
-                    }
+                        volumeMounts: [{ name: "nginx-configs", mountPath: "/etc/nginx/conf.d" }],
+                    },
                 ],
-                volumes: [{ name: "nginx-configs", configMap: { name: nginxConfigName } }]
-            }
-        }
-    }
+                volumes: [{ name: "nginx-configs", configMap: { name: nginxConfigName } }],
+            },
+        },
+    },
 });
 
 // Expose proxy to the public Internet.
 const frontend = new k8s.core.v1.Service(appName, {
     metadata: { labels: nginx.spec.apply(spec => spec.template.metadata.labels) },
     spec: {
-        type: isMinikube === "true" ? "ClusterIP" : "LoadBalancer",
+        type: "LoadBalancer",
         ports: [{ port: 80, targetPort: 80, protocol: "TCP" }],
-        selector: appLabels
-    }
+        selector: appLabels,
+    },
 });
 
-// Export the frontend IP.
-export let frontendIp: pulumi.Output<string>;
-if (isMinikube === "true") {
-    frontendIp = frontend.spec.apply(spec => spec.clusterIP);
-} else {
-    frontendIp = frontend.status.apply(status => status.loadBalancer.ingress[0].ip);
-}
+const message = `Services where \`.spec.type\` is set to 'LoadBalancer' will cause
+the underlying cloud provider to spin up a managed load balancer. This is both
+costly and insecure, as it will cause a public IP address to be allocated to
+the Service. You should use one of Ingress objects that is already provisioned
+instead.`;
+
+// Define an admission policy.
+const noLoadBalancers: policy.AdmissionPolicy = {
+    description: "Disallow Services with type LoadBalancer`",
+    tags: [policy.Tags.Cost, policy.Tags.Security],
+    enforcementLevel: policy.EnforcementLevel.SoftMandatory,
+    message: message,
+    rule: (inputs: outputs.core.v1.Service) => inputs.spec.type == "LoadBalancer",
+};
+
+k8s.core.v1.Service.addAdmissionPolicy(noLoadBalancers);
