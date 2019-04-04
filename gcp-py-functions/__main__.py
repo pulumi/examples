53a2e7bdd7ca173cbadcb7c4f710e8bd46d9be77
Erin Krengel <erin.c.krengel@gmail.com>
2019-04-04 09:12:14 -0700

    Add initial gcp python fxn example

---
+++ gcp-py-functions/__main__.py
@@ -0,0 +1,8 @@
+import pulumi
+from pulumi_gcp import storage
+
+# Create a GCP resource (Storage Bucket)
+bucket = storage.Bucket('my-bucket')
+
+# Export the DNS name of the bucket
+pulumi.export('bucket_name',  bucket.url)
