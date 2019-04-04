b0bf1bdc4f94e6c9356ed84deb1f68d5cc179094
Erin Krengel <erin.c.krengel@gmail.com>
2019-04-04 10:07:26 -0700

    Add basic function

--- gcp-py-functions/__main__.py
+++ gcp-py-functions/__main__.py
@@ -1,8 +1,44 @@
 import pulumi
 from pulumi_gcp import storage
+from pulumi_gcp import cloudfunctions
+
+bucket = storage.Bucket('locatebucket')
+
+asset = pulumi.FileAsset(path="./functions/locate.py")
+archive = pulumi.AssetArchive(assets={
+    "main.py": asset
+})
+object = storage.BucketObject("main.py",
+                              name="main.py",
+                              bucket=bucket.name,
+                              source=archive)
 
 # Create a GCP resource (Storage Bucket)
-bucket = storage.Bucket('my-bucket')
 
 # Export the DNS name of the bucket
 pulumi.export('bucket_name',  bucket.url)
+
+
+fxn = cloudfunctions.Function("locatefunction",
+                              opts=None,
+                              available_memory_mb=None,
+                              description=None,
+                              entry_point="hello_get",
+                              environment_variables=None,
+                              event_trigger=None,
+                              https_trigger_url=None,
+                              labels=None,
+                              name=None,
+                              project=None,
+                              region="us-central1",
+                              runtime="python37",
+                              service_account_email=None,
+                              source_archive_bucket=bucket.name,
+                              source_archive_object=object.name,
+                              source_repository=None,
+                              timeout=None,
+                              trigger_http=True,
+                              __name__=None,
+                              __opts__=None)
+
+pulumi.export("fxn_url", fxn.https_trigger_url)
---
+++ gcp-py-functions/functions/locate.py
@@ -0,0 +1,11 @@
+def hello_get(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    """
+    return 'Hello World!'
