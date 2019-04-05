6c4c6fca179fa5e0d5e06bc9122f54b7411b9c3f
Chris Smith <chrsmith@users.noreply.github.com>
2019-04-05 16:28:03 -0700

    Tweaks and polish to the pending gcp-py-functions example (#276)
    * Misc edits to README.md
    
    * Tweak project description
    
    * Lint, format code

--- gcp-py-functions/functions/main.py
+++ gcp-py-functions/functions/main.py
@@ -1,68 +1,77 @@
+"""Google Cloud Function source code for an ETA messaging app.
+
+Defines a single Cloud Function endpoint, get_demo, which will compute the
+estimated travel time to a location. If configured, will also send the result
+via SMS.
+"""
+
+import os
 from datetime import datetime
 import googlemaps
-import os
 import twilio.rest
 
 
-def get_demo(request):
+def get_travel_time(origin, destination, offset):
+    """Returns the estimated travel time using the Google Maps API.
 
-    # Determine origin and destination
-    lat = request.args.get("lat")
-    long = request.args.get("long")
-    if lat and long:
-        origin = "%s, %s" % (request.args.get("lat"), request.args.get("long"))
-    else:
-        origin = "Moscone Center, San Francisco, CA"
-    destination = os.getenv(
-        "DESTINATION", "1525 4th Avenue #800, Seattle, WA 98101")
+    Returns: A string, such as '3 minutes'"""
+    key = os.getenv("GOOGLE_MAPS_API_KEY", "")
+    if key == "":
+        return "[ENABLE GOOGLE MAPS TO DETERMINE TRAVEL TIME]"
 
-    # Get travel offset
-    travel_offset_str = os.getenv("TRAVEL_OFFSET", "0")
-    travel_offset = int(travel_offset_str)
+    gmaps = googlemaps.Client(key=key)
+    now = datetime.now()
+    directions_result = gmaps.directions(
+        origin=origin,
+        destination=destination,
+        mode="driving",
+        departure_time=now)
 
-    # Get travel time from origin to destination using Google Maps
-    travel_time_str = getTravelTime(
-        origin=origin, destination=destination, offset=travel_offset)
+    travel_time = directions_result[0]["legs"][0]["duration"]["value"]
+    travel_time /= 60  # seconds to minutes
+    travel_time += offset
 
-    # Construct text message body
-    salutation = "Hey honey-bunny, letting you know I'm heading out to pick you up."
-    specifics = "I'm leaving now, I'll be at '%s' to pick you up in about %s." % (
-        destination, travel_time_str)
-    signoff = "🏎️ vroom vroom"
-    message_body = "%s\n%s\n\n%s" % (salutation, specifics, signoff)
+    return "%d minutes" % travel_time
 
-    # Send text message using Twilio
-    return sendText(message_body)
-
-
-def getTravelTime(origin, destination, offset):
-    gmapKey = os.getenv("GOOGLE_MAPS_API_KEY", "")
-    if gmapKey:
-        gmaps = googlemaps.Client(key=gmapKey)
-        now = datetime.now()
-        directions_result = gmaps.directions(origin=origin,
-                                             destination=destination,
-                                             mode="driving",
-                                             departure_time=now)
-        travel_time = directions_result[0]["legs"][0]["duration"]["value"]
-        travel_time /= 60
-        travel_time += offset
-        return "%d minutes" % travel_time
-    return "[ENABLE GOOGLE MAPS TO DETERMINE TRAVEL TIME]"
-
-
-def sendText(message_body):
+def send_text(message_body):
+    """Sends an SMS using the Twilio API."""
     to_number = os.getenv("TO_PHONE_NUMBER", "")
     from_number = os.getenv("FROM_PHONE_NUMBER", "")
     account_sid = os.getenv("TWILLIO_ACCOUNT_SID", "")
     auth_token = os.getenv("TWILLIO_ACCESS_TOKEN", "")
+
     if account_sid and auth_token and to_number and from_number:
         client = twilio.rest.Client(account_sid, auth_token)
-
         client.messages.create(
             to=to_number,
             from_=from_number,
             body=message_body)
-
         return "Sent text message to %s\n%s" % (to_number, message_body)
     return "[ENABLE TWILIO TO SEND A TEXT]: \n%s" % (message_body)
+
+def get_demo(request):
+    """The Google Cloud Function computing estimated travel time."""
+
+    # Get origin location from URL-query parameters.
+    lat = request.args.get("lat")
+    long = request.args.get("long")
+    if lat and long:
+        origin = "%s, %s" % (lat, long)
+    else:
+        origin = "Pulumi HQ, Seattle, WA"
+
+    destination = os.getenv(
+        "DESTINATION",
+        "Space Needle, Seattle, WA")
+
+    # Optional travel time offset, e.g. add a static 5m delay.
+    travel_offset_str = os.getenv("TRAVEL_OFFSET", "0")
+    travel_offset = int(travel_offset_str)
+
+    travel_time_str = get_travel_time(
+        origin=origin, destination=destination, offset=travel_offset)
+
+    # Send the message. Returns a summary in the Cloud Function's response.
+    message = "Hey! I'm leaving now, I'll be at '%s' to pick you up in about %s." % (
+        destination, travel_time_str)
+    return send_text(message)
