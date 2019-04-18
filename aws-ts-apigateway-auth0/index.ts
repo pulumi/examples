c25a1220f29865cbddbd83706bc2d85ee457fb51
Erin Krengel <ekrengel@pulumi.com>
2019-04-18 13:50:29 -0700

    WIP - moving authorizer code from js to ts

--- aws-ts-apigateway-auth0/index.ts
+++ aws-ts-apigateway-auth0/index.ts
@@ -15,26 +15,27 @@ import * as awsx from "@pulumi/awsx";
 import * as aws from '@pulumi/aws';
 import * as pulumi from '@pulumi/pulumi';
 
-
-// Create role for our lambda
-const role = new aws.iam.Role("mylambda-role", {
-    assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({ "Service": ["lambda.amazonaws.com", "apigateway.amazonaws.com"] }),
-});
-
-// Create the authorizer lambda whose code lives in the ./authfunction directory
-const authorizer = new aws.lambda.Function("jwtRsaCustomAuthorizer", {
-    code: new pulumi.asset.FileArchive("./authfunction"),
-    role: role.arn,
-    handler: "index.handler",
-    runtime: aws.lambda.NodeJS8d10Runtime,
-});
+import * as jwksClient from 'jwks-rsa';
+import * as jwt from 'jsonwebtoken';
+import * as util from 'util';
 
 
 const authorizers: awsx.apigateway.LambdaAuthorizer[] = [
     awsx.apigateway.getTokenLambdaAuthorizer({
         authorizerName: "jwt-rsa-custom-authorizer",
         header: "Authorization",
-        handler: authorizer,
+        handler: async (event: awsx.apigateway.AuthorizerEvent, context: any): Promise<awsx.apigateway.AuthorizerResponse> => {
+            let data: awsx.apigateway.AuthorizerResponse;
+            try {
+                data = await authenticate(event);
+            }
+            catch (err) {
+                console.log(err);
+                context.fail(`Unauthorized: ${err.message}`);
+                return nil;
+            }
+            return data;
+        },
         identityValidationExpression: "^Bearer [-0-9a-zA-Z\._]*$",
         authorizerResultTtlInSeconds: 3600,
     })];
@@ -54,3 +55,59 @@ const api = new awsx.apigateway.API("myapi", {
 });
 
 export const url = api.url;
+
+// Code copied directly from Auth0's Guide
+// https://github.com/auth0-samples/jwt-rsa-aws-custom-authorizer
+
+require('dotenv').config({ silent: true });
+
+
+// extract and return the Bearer Token from the Lambda event parameters
+const getToken = (event: awsx.apigateway.AuthorizerEvent) => {
+    if (!event.type || event.type !== 'TOKEN') {
+        throw new Error('Expected "event.type" parameter to have value "TOKEN"');
+    }
+
+    const tokenString = event.authorizationToken;
+    if (!tokenString) {
+        throw new Error('Expected "event.authorizationToken" parameter to be set');
+    }
+
+    const match = tokenString.match(/^Bearer (.*)$/);
+    if (!match || match.length < 2) {
+        throw new Error(`Invalid Authorization token - ${tokenString} does not match "Bearer .*"`);
+    }
+    return match[1];
+}
+
+const jwtOptions = {
+    audience: process.env.AUDIENCE,
+    issuer: process.env.TOKEN_ISSUER
+};
+
+const authenticate = (event: awsx.apigateway.AuthorizerEvent): awsx.apigateway.AuthorizerResponse => {
+    console.log(event);
+    const token = getToken(event);
+
+    const decoded = jwt.decode(token, { complete: true });
+    if (!decoded || !decoded.header || !decoded.header.kid) {
+        throw new Error('invalid token');
+    }
+
+    const client = jwksClient({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 10, // Default value
+        jwksUri: process.env.JWKS_URI
+    });
+
+    const getSigningKey = util.promisify(client.getSigningKey);
+    return getSigningKey(decoded.header.kid)
+        .then((key) => {
+            const signingKey = key.publicKey || key.rsaPublicKey;
+            return jwt.verify(token, signingKey, jwtOptions);
+        })
+        .then(decoded => (
+            awsx.apigateway.authorizerResponse(decoded.sub, 'Allow', event.methodArn, { scope: decoded.scope })
+        ));
+}
