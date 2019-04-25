19f66e998cea0aa843eefd75586f78228e283603
Mikhail Shilkov <shilkov@gmail.com>
2019-04-25 21:00:15 +0200

    Refactor to @pulumi/azure-serverless

--- azure-ts-functions/index.ts
+++ azure-ts-functions/index.ts
@@ -1,16 +1,17 @@
-import * as azureFunction from "./azureFunction";
+import { functionApp } from "@pulumi/azure-serverless";
 
 // Create an Azure function that prints a message and the request headers.
 
-function handler(context: azureFunction.Context, request: azureFunction.HttpRequest) {
+function handler(context: functionApp.Context, request: functionApp.HttpRequest) {
     let body = "";
     let headers = context.req!.headers!;
+
     for (let h in headers) {
         body = body + `${h} = ${headers[h]}\n`;
     }
 
-    let res: azureFunction.HttpResponse = {
-        status: azureFunction.HttpStatusCode.OK,
+    let res: functionApp.HttpResponse = {
+        status: 200,
         headers: {
             "content-type": "text/plain",
         },
@@ -20,5 +21,5 @@ function handler(context: azureFunction.Context, request: azureFunction.HttpRequ
     context.done(undefined, res);
 }
 
-let fn = new azureFunction.HttpFunction("fn", handler);
+let fn = new functionApp.HttpFunction("fn", handler);
 export let endpoint = fn.endpoint;
