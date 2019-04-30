c07893573b98c08ffadc6a1f43295b4049c1bcf3
Joe Duffy <joe@pulumi.com>
2019-04-30 11:09:25 -0700

    Add a Docker on AWS Fargate example (#290)

---
+++ aws-ts-hello-fargate/index.ts
@@ -0,0 +1,29 @@
+import * as awsx from "@pulumi/awsx";
+
+// Step 1: Create an ECS Fargate cluster.
+const cluster = new awsx.ecs.Cluster("cluster");
+
+// Step 2: Define the Networking for our service.
+const alb = new awsx.elasticloadbalancingv2.ApplicationLoadBalancer(
+    "net-lb", { external: true, securityGroups: cluster.securityGroups });
+const web = alb.createListener("web", { port: 80, external: true });
+
+// Step 3: Build and publish a Docker image to a private ECR registry.
+const img = awsx.ecs.Image.fromPath("app-img", "./app");
+
+// Step 4: Create a Fargate service task that can scale out.
+const appService = new awsx.ecs.FargateService("app-svc", {
+    cluster,
+    taskDefinitionArgs: {
+        container: {
+            image: img,
+            cpu: 102 /*10% of 1024*/,
+            memory: 50 /*MB*/,
+            portMappings: [ web ],
+        },
+    },
+    desiredCount: 5,
+});
+
+// Step 5: Export the Internet address for the service.
+export const url = web.endpoint.hostname;
