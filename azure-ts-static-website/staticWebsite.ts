b6baf401f5accfceb9bea9908eca699df7a9d3b5
Mikhail Shilkov <shilkov@gmail.com>
2019-05-30 09:31:23 +0200

    Review comments

--- azure-ts-static-website/staticWebsite.ts
+++ azure-ts-static-website/staticWebsite.ts
@@ -12,40 +12,50 @@ const accountNameProp = "accountName";
 // Therefore, we created a custom provider which wraps corresponding Azure CLI commands
 class StorageStaticWebsiteProvider implements pulumi.dynamic.ResourceProvider {
 
-    check = async (olds: any, news: any) => {
-        const failedChecks = [];
+    public async check(olds: any, news: any): Promise<pulumi.dynamic.CheckResult> {
+        const failures = [];
 
         if (news[accountNameProp] === undefined) {
-            failedChecks.push({property: accountNameProp, reason: `required property '${accountNameProp}' missing`});
+            failures.push({property: accountNameProp, reason: `required property '${accountNameProp}' missing`});
         }
 
-        return { inputs: news, failedChecks: failedChecks };
+        return { inputs: news, failures };
     }
 
-    diff = async (id: pulumi.ID, olds: any, news: any) => {
+    public async diff(id: pulumi.ID, olds: any, news: any): Promise<pulumi.dynamic.DiffResult> {
         const replaces = [];
 
         if (olds[accountNameProp] !== news[accountNameProp]) {
             replaces.push(accountNameProp);
         }
 
-        return { replaces: replaces };
+        return { replaces };
     }
 
-    create = async (inputs: any) => {
+    public async create(inputs: any): Promise<pulumi.dynamic.CreateResult> {
         const { execSync } = require("child_process");
+        const url = require('url');
+        const accountName = inputs[accountNameProp];
+
+        // Helper function to execute a command, supress the warnings from polluting the output, and parse the result as JSON
+        const executeToJson = (command: string) => JSON.parse(execSync(command, { stdio: ["pipe", "pipe", "ignore"] }).toString());
+
+        // Install Azure CLI extension for storage (currently, only the preview version has the one we need)
         execSync("az extension add --name storage-preview", { stdio: "ignore" });
 
-        const accountName = inputs[accountNameProp];
-        const executeToJson = (command: string) => JSON.parse(execSync(command, { stdio: ["pipe", "pipe", "ignore"] }).toString());
+        // Update the service properties of the storage account to enable static website and validate the result
         const update = executeToJson(`az storage blob service-properties update --account-name "${accountName}" --static-website --404-document 404.html --index-document index.html`);
         if (!update.staticWebsite.enabled) {
             throw new Error(`Static website update failed: ${update}`);
         }
 
-        const endpoint = executeToJson(`az storage account show -n "${accountName}" --query "primaryEndpoints.web"`);
-        const hostName = endpoint.replace("https://", "").replace("/", "");
+        // Extract the web endpoint and the hostname from the storage account
+        const endpoint = executeToJson(`az storage account show -n "${accountName}" --query "primaryEndpoints.web"`);        
+        const hostName = url.parse(endpoint).hostname;
+
+        // Files for static websites should be stored in a special built-in container called '$web', see https://docs.microsoft.com/en-us/azure/storage/blobs/storage-blob-static-website
         const webContainerName = "$web";
+
         return { 
             id: `${accountName}StaticWebsite`,
             outs: { endpoint, hostName, webContainerName }
