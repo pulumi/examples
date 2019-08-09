ab6b182a6b1b688f4a5e2d2c40feb7338331833e
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-08-09 06:58:35 -0700

    Use different athena names.

--- cloud-js-twitter-athena/index.js
+++ cloud-js-twitter-athena/index.js
@@ -22,20 +22,20 @@ cloud.timer.interval("twitter-search-timer", { minutes: 2 }, async() => {
         access_token_key: accessTokenKey,
         access_token_secret: accessTokenSecret,
     });
-    
+
     client.get('search/tweets', {q: twitterQuery, count: 100}, function(error, tweets, response) {
         let statuses = tweets.statuses;
 
         let results = statuses.map(s => {
             let user = s.user.screen_name;
-    
-            return JSON.stringify({ 
+
+            return JSON.stringify({
                 created_at: s.created_at,
-                id: s.id_str, 
-                text: s.text, 
+                id: s.id_str,
+                text: s.text,
                 user: user,
                 hashtags: s.entities.hashtags,
-                followers: s.user.followers_count, 
+                followers: s.user.followers_count,
                 isVerified: s.user.verified,
                 isRetweet: s.retweeted_status != null,
                 url: `https://twitter.com/${user}/status/${s.id_str}`,
@@ -48,15 +48,15 @@ cloud.timer.interval("twitter-search-timer", { minutes: 2 }, async() => {
         let contents = Buffer.from(results.join("\n"), "utf8");
 
         bucket.put(filename, contents);
-    }); 
+    });
 });
 
 // athena setup
-let athena = new aws.athena.Database("tweets_database", 
-    { name: "tweets_database", bucket: bucket.bucket.id, forceDestroy: true } 
+let athena = new aws.athena.Database("tweets_database_2",
+    { name: "tweets_database_2", bucket: bucket.bucket.id, forceDestroy: true }
 );
 
-// Sadly, there isn't support for Athena tables in Terraform. 
+// Sadly, there isn't support for Athena tables in Terraform.
 // See https://github.com/terraform-providers/terraform-provider-aws/pull/1893#issuecomment-351300973
 // So, we'll instead create a query for the table definition
 function createTableQuery(bucket) {
@@ -74,9 +74,9 @@ function createTableQuery(bucket) {
     LOCATION 's3://${bucket}/${outputFolder}/';`;
 }
 
-let topUsersQuery = 
-    `select distinct user, followers, text, url 
-    from tweets 
+let topUsersQuery =
+    `select distinct user, followers, text, url
+    from tweets
     where isRetweet = false and followers > 1000
     order by followers desc`;
 
@@ -96,4 +96,4 @@ function getQueryUri(queryId) {
 exports.bucketName = bucketName
 exports.athenaDatabase = athena.id;
 exports.topUsersQueryUri = topUsersAthenaQuery.id.apply(getQueryUri);
-exports.createTableQueryUri = createTableAthenaQuery.id.apply(getQueryUri); 
+exports.createTableQueryUri = createTableAthenaQuery.id.apply(getQueryUri);
