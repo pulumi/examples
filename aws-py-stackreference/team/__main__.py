0d1ec972624eff4754a1b8ee9d470e591eb9485f
stack72 <public@paulstack.co.uk>
2019-09-24 16:28:15 +0300

    Add aws-py-stackreference

---
+++ aws-py-stackreference/team/__main__.py
@@ -0,0 +1,32 @@
+from pulumi import StackReference, Config, export
+from pulumi_aws import get_ami, ec2
+
+config = Config()
+company_stack = StackReference(config.require("companyStack"))
+department_stack = StackReference(config.require("departmentStack"))
+
+combines_tags = {
+    "department": department_stack.get_output("departmentName"),
+    "company": company_stack.get_output("companyName"),
+    "team": config.require("teamName"),
+    "Managed By": "Pulumi",
+}
+
+ami_id = get_ami(
+    most_recent="true",
+    owners=["099720109477"],
+    filters=[
+        {
+            "name":"name",
+            "values":["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+        }]
+).id
+
+instance = ec2.Instance(
+    "tagged",
+    instance_type="t2.medium",
+    ami=ami_id,
+    tags=combines_tags)
+
+export("instance_id", instance.id)
+export("instance_tags", instance.tags)
