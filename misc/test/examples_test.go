69ab20279093865f216cf88cfc038eb0f54dea0a
stack72 <public@paulstack.co.uk>
2019-10-14 21:36:59 +0300

    change t.Error() to f.FailNow()

--- misc/test/examples_test.go
+++ misc/test/examples_test.go
@@ -1005,7 +1005,7 @@ func getGkeVersion() string {
 func getCwd(t *testing.T) string {
 	cwd, err := os.Getwd()
 	if err != nil {
-		t.Error("expected a valid working directory", err)
+		t.FailNow()
 	}
 
 	return cwd
@@ -1014,7 +1014,7 @@ func getCwd(t *testing.T) string {
 func getBaseOptions(t *testing.T) integration.ProgramTestOptions {
 	overrides, err := integration.DecodeMapString(os.Getenv("PULUMI_TEST_NODE_OVERRIDES"))
 	if err != nil {
-		t.Error("expected valid override map:", err.Error())
+		t.FailNow()
 	}
 
 	base := integration.ProgramTestOptions{
