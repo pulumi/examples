cd542d404a0e2d1474bd4ebf6c7c7e794933fe31
clstokes <cameron@cameronstokes.com>
2019-10-21 16:11:57 -0700

    Add ComponentResource example for Python
    A copy of https://github.com/pulumi/examples/tree/25368010c25a4413e45796058b6851ecaf653a47/azure-py-webserver using [ComponentResource](https://www.pulumi.com/docs/intro/concepts/programming-model/#components).
    
    Closes #434 .

---
+++ azure-py-webserver-component/__main__.py
@@ -0,0 +1,39 @@
+import pulumi
+from pulumi import Config, ResourceOptions
+from pulumi_azure import core, network
+from webserver import WebServerArgs, WebServer
+
+config = Config()
+username = config.require_secret("username")
+password = config.require_secret("password")
+
+resource_group = core.ResourceGroup("server", location="West US")
+
+net = network.VirtualNetwork(
+    "server-network",
+    resource_group_name=resource_group.name,
+    location=resource_group.location,
+    address_spaces=["10.0.0.0/16"],
+    subnets=[{"name": "default", "address_prefix": "10.0.1.0/24"}],
+    opts=ResourceOptions(parent=resource_group),
+)
+
+subnet = network.Subnet(
+    "server-subnet",
+    resource_group_name=resource_group.name,
+    virtual_network_name=net.name,
+    address_prefix="10.0.2.0/24",
+    opts=ResourceOptions(parent=net),
+)
+
+web_server = WebServer(
+    "server",
+    WebServerArgs(
+        resource_group=resource_group,
+        subnet=subnet,
+        username=username,
+        password=password,
+    ),
+)
+
+pulumi.export("public_ip", web_server.public_ip_addr)
