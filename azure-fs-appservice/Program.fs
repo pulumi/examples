fab09cb6b6b2bb2d8aa9796cb3ffbc2f7984b4f9
Mikhail Shilkov <shilkov@gmail.com>
2019-11-06 00:07:05 +0000

    Change the references version pattern

--- azure-fs-appservice/Program.fs
+++ azure-fs-appservice/Program.fs
@@ -8,33 +8,63 @@ open Pulumi.Azure.Core
 open Pulumi.Azure.Sql
 open Pulumi.Azure.Storage
 open Pulumi.Azure.Storage.Inputs
-open System.Collections.Immutable
+
+module Outputs =
+    
+    let apply<'a, 'b> (f: 'a -> 'b) (output: Output<'a>) : Output<'b> =
+        output.Apply f
+
+    let applyAsync<'a, 'b> (f: 'a -> Async<'b>) (output: Output<'a>) : Output<'b> =
+        output.Apply<'b> (f >> Async.StartAsTask)
+
+    let bind<'a, 'b> (f: 'a -> Output<'b>) (output: Output<'a>) : Output<'b> =
+        output.Apply<'b> f
+    
+    let pair<'a, 'b> (a: Output<'a>) (b: Output<'b>) =
+        Output.Tuple (a, b) 
+        |> apply (fun struct (a, b) -> (a, b))
+
+    let pair3<'a, 'b, 'c> (a: Output<'a>) (b: Output<'b>) (c: Output<'c>) =
+        Output.Tuple (io a, io b, io c) 
+        |> apply (fun struct (a,b,c) -> (a,b,c))
+
+    let pair4<'a, 'b, 'c, 'd> (a: Output<'a>) (b: Output<'b>) (c: Output<'c>) (d: Output<'d>) =
+        pair (pair a b) (pair c d)
+        |> apply(fun ((e, f), (g, h)) -> e, f, g, h)
+
+    let all<'a> (values: List<Output<'a>>) : Output<List<'a>> = 
+        Output.All (values |> List.map io |> List.toArray)
+        |> apply List.ofSeq
+
+let inputList<'a> (items: seq<Input<'a>>) =
+    let result = new InputList<'a> ()
+    for item in items do result.Add item
+    result
+
+let inputMap<'a> (items: seq<string * Input<'a>>) =
+    let result = new InputMap<'a> ()
+    for item in items do result.Add item
+    result
 
 let signedBlobReadUrl(blob: ZipBlob) (account: Account): Output<string> =
-    let getSasToken (values: ImmutableArray<string>) =
-        async {
-            let accountName = values.[0]
-            let connectionString = values.[1]
-            let containerName = values.[2]
-            let blobName = values.[3]
-            let permissions = 
-                new GetAccountBlobContainerSASPermissionsArgs
-                   (Read = input true, Write = input false, Delete = input false,
-                    List = input false, Add = input false, Create = input false)
-            let args = 
-                new GetAccountBlobContainerSASArgs
-                   (ConnectionString = input connectionString,
-                    ContainerName = input containerName,
-                    Start = input "2019-01-01",
-                    Expiry = input "2100-01-01",
-                    Permissions = input permissions)
-            let! sas = Invokes.GetAccountBlobContainerSAS args |> Async.AwaitTask
-            return sprintf "https://%s.blob.core.windows.net/%s/%s%s" accountName containerName blobName sas.Sas
-        }
-        |> Async.StartAsTask
+    let getSasToken (accountName, connectionString, containerName, blobName) = async {
+        let permissions = 
+            new GetAccountBlobContainerSASPermissionsArgs
+                (Read = input true, Write = input false, Delete = input false,
+                List = input false, Add = input false, Create = input false)
+        let args = 
+            new GetAccountBlobContainerSASArgs
+                (ConnectionString = input connectionString,
+                ContainerName = input containerName,
+                Start = input "2019-01-01",
+                Expiry = input "2100-01-01",
+                Permissions = input permissions)
+        let! sas = Invokes.GetAccountBlobContainerSAS args |> Async.AwaitTask
+        return sprintf "https://%s.blob.core.windows.net/%s/%s%s" accountName containerName blobName sas.Sas
+    }
 
-    Output.All(io account.Name, io account.PrimaryConnectionString, io blob.StorageContainerName, io blob.Name)
-          .Apply<string> getSasToken
+    Outputs.pair4 account.Name account.PrimaryConnectionString blob.StorageContainerName blob.Name
+    |> Outputs.applyAsync getSasToken
 
 let infra () =
     let resourceGroup = new ResourceGroup "appservice-rg"
@@ -90,13 +120,11 @@ let infra () =
                 RequestedServiceObjectiveName = input "S0"))
 
     let connectionString = 
-        Output
-            .Tuple<string, string, string>(io sqlServer.Name, io database.Name, io password)
-            .Apply<string>(
-                fun struct(server, database, pwd) -> 
-                    sprintf
-                        "Server= tcp:%s.database.windows.net;initial catalog=%s;userID=%s;password=%s;Min Pool Size=0;Max Pool Size=30;Persist Security Info=true;"
-                        server database username pwd)
+        Outputs.pair3 sqlServer.Name database.Name password
+        |> Outputs.apply(fun (server, database, pwd) -> 
+            sprintf
+                "Server= tcp:%s.database.windows.net;initial catalog=%s;userID=%s;password=%s;Min Pool Size=0;Max Pool Size=30;Persist Security Info=true;"
+                server database username pwd)
 
     let connectionStringSetting =
         new AppServiceConnectionStringsArgs
@@ -104,17 +132,13 @@ let infra () =
             Type = input "SQLAzure",
             Value = io connectionString)
 
-    let appSettings = new InputMap<string> ()
-    appSettings.Add("WEBSITE_RUN_FROM_PACKAGE", io codeBlobUrl)
-    let connectionStrings = new InputList<AppServiceConnectionStringsArgs> ()
-    connectionStrings.Add (input connectionStringSetting)
     let app = 
         new AppService("app", 
             new AppServiceArgs
                (ResourceGroupName = io resourceGroup.Name,
                 AppServicePlanId = io appServicePlan.Id,
-                AppSettings = appSettings,
-                ConnectionStrings = connectionStrings))
+                AppSettings = inputMap ["WEBSITE_RUN_FROM_PACKAGE", io codeBlobUrl],
+                ConnectionStrings = inputList [input connectionStringSetting]))
 
     dict [("endpoint", app.DefaultSiteHostname :> obj)]
 
