54da577bd317839e8affcea47ddc3a951c3b32e5
evanboyle <evan@pulumi.com>
2019-11-07 17:24:51 -0800

    improve readme, cleanup

--- aws-cs-lambda/README.md
+++ aws-cs-lambda/README.md
@@ -1,16 +1,38 @@
-# AWS C# Lambda Example
-Creates a lambda that does a simple `.toUpper` on the string input and returns it. 
+# AWS C# Lambda
+This example creates a lambda that does a simple `.toUpper` on the string input and returns it. 
 
+## Deploying the App
 
-## Instructions
+To deploy your infrastructure, follow the below steps.
 
-From this directory:
+### Prerequisites
+
+1. [Install Pulumi](https://www.pulumi.com/docs/get-started/install/)
+2. [Configure AWS Credentials](https://www.pulumi.com/docs/intro/cloud-providers/aws/setup/)
+
+### Steps
+
+After cloning this repo, from this working directory, run these commands:
+
+1. Build and publish the lambda function, making the output available to our Pulumi program. 
+
 ```bash
-# build publish the lambda function
 cd dotnetLambda/src/dotnetLambda/ && dotnet restore && dotnet build && dotnet publish && cd ../../../
 ```
 
+2. Execute our Pulumi program to archive our published function output, and create our lambda. 
 ```bash
-# execute the pulumi program to deploy the lambda
 cd ./pulumi/ && pulumi up && cd ../
+```
+
+3. Call our lambda function from the aws cli.
+```bash
+aws lambda invoke \
+--function-name $(pulumi stack output lambda) \
+--region $(pulumi config get aws:region) \
+--payload '"foo"' \
+output.json
+
+cat output.json
+# "FOO"
 ```
--- aws-cs-lambda/pulumi/Program.cs
+++ aws-cs-lambda/pulumi/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 using Pulumi;
 using Pulumi.Aws.Lambda;
@@ -16,36 +17,38 @@ class LambdaUtil
     {
         var lambdaRole = new Role("lambdaRole", new RoleArgs
         {
-            AssumeRolePolicy = @"{
-        ""Version"": ""2012-10-17"",
-        ""Statement"": [
-            {
-                ""Action"": ""sts:AssumeRole"",
-                ""Principal"": {
-                    ""Service"": ""lambda.amazonaws.com""
-                },
-                ""Effect"": ""Allow"",
-                ""Sid"": """"
-            }
-        ]
-    }",
+            AssumeRolePolicy =
+            @"{
+                ""Version"": ""2012-10-17"",
+                ""Statement"": [
+                    {
+                        ""Action"": ""sts:AssumeRole"",
+                        ""Principal"": {
+                            ""Service"": ""lambda.amazonaws.com""
+                        },
+                        ""Effect"": ""Allow"",
+                        ""Sid"": """"
+                    }
+                ]
+            }",
         });
 
         var logPolicy = new RolePolicy("lambdaLogPolicy", new RolePolicyArgs
         {
             Role = lambdaRole.Id,
-            Policy = @"{
-        ""Version"": ""2012-10-17"",
-        ""Statement"": [{
-            ""Effect"": ""Allow"",
-            ""Action"": [
-                ""logs:CreateLogGroup"",
-                ""logs:CreateLogStream"",
-                ""logs:PutLogEvents""
-            ],
-            ""Resource"": ""arn:aws:logs:*:*:*""
-        }]
-    }"
+            Policy =
+            @"{
+                ""Version"": ""2012-10-17"",
+                ""Statement"": [{
+                    ""Effect"": ""Allow"",
+                    ""Action"": [
+                        ""logs:CreateLogGroup"",
+                        ""logs:CreateLogStream"",
+                        ""logs:PutLogEvents""
+                    ],
+                    ""Resource"": ""arn:aws:logs:*:*:*""
+                }]
+            }"
         });
 
         return lambdaRole;
@@ -58,14 +61,17 @@ class Program
     {
         return Deployment.RunAsync(() =>
         {
-            var hnLambda = new Function("HN-Lambda", new FunctionArgs
+            var lambda = new Function("basicLambda", new FunctionArgs
             {
                 Runtime = "dotnetcore2.1",
                 Code = LambdaUtil.buildArchive("../dotnetLambda/src/dotnetLambda//bin/Debug/netcoreapp2.1/publish"),
                 Handler = "dotnetLambda::dotnetLambda.Function::FunctionHandler",
                 Role = LambdaUtil.createLambdaRole().Arn
             });
+
+            return new Dictionary<string, object>{
+                {"lambda", lambda.Arn}
+            };
         });
     }
 }
-
