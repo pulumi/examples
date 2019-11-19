7064b023508336e6a42645fabefd0766b2c98823
Pat Gavlin <pat@pulumi.com>
2019-11-19 15:37:01 -0800

    Temporarily disable Packet tests. (#468)
    We are having trouble with authorization failures in these tests.

--- misc/test/examples_test.go
+++ misc/test/examples_test.go
@@ -944,6 +944,8 @@ func TestAccGcpTsServerlessRaw(t *testing.T) {
 }
 
 func TestAccPacketPyWebserver(t *testing.T) {
+	t.Skipf("skipping temporarily due to Packet auth failures")
+
 	test := getBaseOptions(t).
 		With(integration.ProgramTestOptions{
 			Dir: path.Join(getCwd(t), "..", "..", "packet-py-webserver"),
@@ -953,6 +955,8 @@ func TestAccPacketPyWebserver(t *testing.T) {
 }
 
 func TestAccPacketTsWebserver(t *testing.T) {
+	t.Skipf("skipping temporarily due to Packet auth failures")
+
 	test := getBaseOptions(t).
 		With(integration.ProgramTestOptions{
 			Dir: path.Join(getCwd(t), "..", "..", "packet-ts-webserver"),
