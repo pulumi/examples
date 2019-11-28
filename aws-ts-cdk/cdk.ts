1267b70f4550091ca38c94bb2fe9846d83469131
Luke Hoban <luke@pulumi.com>
2019-11-27 16:41:59 -0800

    Use a component

--- aws-ts-cdk/cdk.ts
+++ aws-ts-cdk/cdk.ts
@@ -1,69 +1,55 @@
 // Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
+import * as cdk from "@aws-cdk/core";
+import * as cx from "@aws-cdk/cx-api";
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
 
-import * as cdk from "@aws-cdk/core";
-import * as cx from "@aws-cdk/cx-api";
+export class CDKStack extends pulumi.ComponentResource {
+    cloudformationStack: aws.cloudformation.Stack;
+    constructor(name: string, stackClass: typeof cdk.Stack, opts?: pulumi.ComponentResourceOptions) {
+        const app = new cdk.App({ outdir: "./cdk.out" });
+        const stack = new stackClass(app, name);
+        const assembly = app.synth();
+        const synthesizedStack = assembly.getStack(name);
 
-export function createStack(name: string, stackClass: typeof cdk.Stack): aws.cloudformation.Stack {
-    const app = new cdk.App({outdir: "./cdk.out"});
-    const stack = new stackClass(app, name);
-    const assembly = app.synth();
+        super("pulumicdk:app:stack", name, {
+            template: synthesizedStack.template,
+        }, opts);
 
-    const synthesizedStack = assembly.getStack(name);
-    const parameters: any = {};
-    if (synthesizedStack.assets.length !== 0) {
-        const s3Assets = <cx.FileAssetMetadataEntry[]>synthesizedStack.assets.filter(asset => asset.packaging !== "container-image");
-        const containerAssets = <cx.ContainerImageAssetMetadataEntry[]>synthesizedStack.assets.filter(asset => asset.packaging === "container-image");
+        const parameters: any = {};
+        if (synthesizedStack.assets.length !== 0) {
+            const s3Assets = <cx.FileAssetMetadataEntry[]>synthesizedStack.assets.filter(asset => asset.packaging !== "container-image");
+            const containerAssets = <cx.ContainerImageAssetMetadataEntry[]>synthesizedStack.assets.filter(asset => asset.packaging === "container-image");
 
-        if (containerAssets.length !== 0) {
-            throw new Error("NYI: container assets");
+            if (s3Assets.length !== 0) {
+                const bucket = new aws.s3.Bucket(`${name}-assets`, {}, { parent: this });
+                for (const asset of s3Assets) {
+                    const source = asset.packaging === "zip"
+                        ? new pulumi.asset.FileArchive("./cdk.out/${asset.path}")
+                        : new pulumi.asset.FileAsset("./cdk.out/${asset.path}");
+                    const _ = new aws.s3.BucketObject(`${name}-assets-${asset.s3KeyParameter}`, {
+                        bucket: bucket,
+                        key: asset.sourceHash,
+                        source: source,
+                    }, { parent: this });
+                    parameters[asset.s3BucketParameter] = bucket.id;
+                    parameters[asset.s3KeyParameter] = asset.sourceHash;
+                    parameters[asset.artifactHashParameter] = asset.sourceHash;
+                }
+            }
+
+            if (containerAssets.length !== 0) {
+                throw new Error("NYI: container assets");
+            }
         }
 
-        if (s3Assets.length !== 0) {
-            throw new Error("NYI: container assets");
-            // // Create an S3 bucket to hold the stack's assets.
-            // const bucket = new cloudformation.s3.Bucket(`${name}assets`);
-            // const uploader = bucket.id.apply(async bucketId => {
-            //     const s3 = new aws.S3({region: cloudformation.region});
-            //     const uploads = s3Assets.map(async asset => {
-            //         try {
-            //             let content: Buffer;
-            //             const path = `./cdk.out/${asset.path}`;
-            //             if (asset.packaging === "zip") {
-            //                 const archive = new zip();
-            //                 archive.addLocalFolder(path, "/");
-            //                 content = archive.toBuffer();
-            //             } else {
-            //                 content = fs.readFileSync(path);
-            //             }
-            //             await s3.upload({
-            //                 Bucket: bucketId,
-            //                 Key: asset.sourceHash,
-            //                 Body: content,
-            //             }).promise();
-            //         } catch (e) {
-            //             console.log(`upload failed: ${e}`);
-            //         }
-            //     });
-            //     await Promise.all(uploads);
-            //     return bucketId;
-            // });
+        this.cloudformationStack = new aws.cloudformation.Stack(name, {
+            templateBody: JSON.stringify(synthesizedStack.template),
+            disableRollback: true,
+            capabilities: ["CAPABILITY_IAM"],
+        }, { parent: this });
 
-            // for (const asset of s3Assets) {
-            //     parameters[asset.s3BucketParameter] = uploader;
-            //     parameters[asset.s3KeyParameter] = asset.sourceHash;
-            //     parameters[asset.artifactHashParameter] = asset.sourceHash;
-            // }
-        }
+        this.registerOutputs({});
     }
-
-    const cloudformationStack = new aws.cloudformation.Stack("s", {
-        templateBody: JSON.stringify(synthesizedStack.template),
-        disableRollback: true,
-        capabilities: ["CAPABILITY_IAM"],
-    });
-
-    return cloudformationStack;
 }
--- aws-ts-cdk/index.ts
+++ aws-ts-cdk/index.ts
@@ -1,9 +1,32 @@
 // Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-import * as aws from "@pulumi/aws";
+import events = require("@aws-cdk/aws-events");
+import targets = require("@aws-cdk/aws-events-targets");
+import lambda = require("@aws-cdk/aws-lambda");
+import cdk = require("@aws-cdk/core");
+import fs = require("fs");
+import { CDKStack } from "./cdk";
 
-import * as pulumi from "@pulumi/pulumi";
-import * as cdk from "./cdk";
-import { LambdaCronStack } from "./lambdaCron";
+// A CDK Stack which can use any resources from the CDK Construct library.
+class LambdaCronStack extends cdk.Stack {
+    constructor(app: cdk.App, id: string) {
+        super(app, id);
 
-cdk.createStack("foo", LambdaCronStack);
+        const lambdaFn = new lambda.Function(this, "Singleton", {
+            code: new lambda.InlineCode(fs.readFileSync("./app/lambda-handler.py", { encoding: "utf-8" })),
+            handler: "index.main",
+            timeout: cdk.Duration.seconds(300),
+            runtime: lambda.Runtime.PYTHON_3_6,
+        });
+
+        const rule = new events.Rule(this, "Rule", {
+            schedule: events.Schedule.expression("cron(0 18 ? * MON-FRI *)"),
+        });
+
+        rule.addTarget(new targets.LambdaFunction(lambdaFn));
+    }
+}
+
+// Instantiate the CDK Stack as a Pulumi resource.
+const stack = new CDKStack("lambdaCron", LambdaCronStack);
+export const stackId = stack.cloudformationStack.id;
