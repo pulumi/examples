903531f20128e3f8ff4fe4cad165cd4da857537b
Erin Krengel <ekrengel@pulumi.com>
2019-12-01 20:59:47 -0800

    improve K8s examples plus some cleanup (#485)

--- policy-packs/kubernetes/index.ts
+++ policy-packs/kubernetes/index.ts
@@ -13,22 +13,64 @@
 // limitations under the License.
 
 import * as k8s from "@pulumi/kubernetes";
-import { PolicyPack, validateTypedResource } from "@pulumi/policy";
+import { EnforcementLevel, PolicyPack, ResourceValidationPolicy, validateTypedResource } from "@pulumi/policy";
 
 const policies = new PolicyPack("kubernetes", {
     policies: [
-        {
-            name: "no-public-services",
-            description: "Kubernetes Services should be cluster-private",
-            enforcementLevel: "mandatory",
-            validateResource: validateTypedResource(k8s.core.v1.Service, (svc, args, reportViolation) => {
-                if (svc.spec && svc.spec.type === "LoadBalancer") {
-                    reportViolation(`Kubernetes Services that have .type === "LoadBalancer" are exposed to ` +
-                        `anything that can reach the Kubernetes cluster, likely including the ` +
-                        `public Internet. The security team has disallowed this to prevent ` +
-                        `unauthorized access.`);
-                }
-            }),
-        },
+        noPublicServices("mandatory"),
+        minimumReplicaCount("mandatory"),
+        podsAreProhibited("advisory"),
     ],
 });
+
+function noPublicServices(enforcementLevel: EnforcementLevel): ResourceValidationPolicy {
+    return {
+        name: "no-public-services",
+        description: "Kubernetes Services should be cluster-private",
+        enforcementLevel: enforcementLevel,
+        validateResource: validateTypedResource(k8s.core.v1.Service, (svc, _, reportViolation) => {
+            if (svc.spec && svc.spec.type === "LoadBalancer") {
+                reportViolation(`Kubernetes Services that have .type === "LoadBalancer" are exposed to ` +
+                    `anything that can reach the Kubernetes cluster, likely including the ` +
+                    `public Internet. The security team has disallowed this to prevent ` +
+                    `unauthorized access.`);
+            }
+        }),
+    };
+}
+
+function minimumReplicaCount(enforcementLevel: EnforcementLevel): ResourceValidationPolicy {
+    return {
+        name: "minimum-replica-count",
+        description: "Checks that Kubernetes Deployments and ReplicaSets have at least three replicas.",
+        enforcementLevel: enforcementLevel,
+        validateResource: [
+            validateTypedResource(k8s.apps.v1.Deployment, (deployment, _, reportViolation) => {
+                if (deployment.spec === undefined
+                    || deployment.spec.replicas === undefined
+                    || deployment.spec.replicas < 3) {
+                    reportViolation("Kubernetes Deployments should have at least three replicas.");
+                }
+            }),
+            validateTypedResource(k8s.apps.v1.ReplicaSet, (replicaSet, _, reportViolation) => {
+                if (replicaSet.spec === undefined
+                    || replicaSet.spec.replicas === undefined
+                    || replicaSet.spec.replicas < 3) {
+                    reportViolation("Kubernetes ReplicaSet should have at least three replicas.");
+                }
+            }),
+        ],
+    };
+}
+
+function podsAreProhibited(enforcementLevel: EnforcementLevel): ResourceValidationPolicy {
+    return {
+        name: "pods-are-prohibited",
+        description: "Checks that Kubernetes Pods are not being used directly.",
+        enforcementLevel: enforcementLevel,
+        validateResource: validateTypedResource(k8s.core.v1.Pod, (_, __, reportViolation) => {
+            reportViolation("Kubernetes Pods should not be used directly. " +
+                "Instead, you may want to use a Deployment, ReplicaSet or Job.");
+        }),
+    };
+}
