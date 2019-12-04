c1f3aec950fb019e1b1650a5a7ab2b247416fa38
Pat Gavlin <pat@pulumi.com>
2019-12-04 09:46:43 -0800

    Port two Azure examples to Go.
    And fix the Gopkg.toml files for AWS examples.

---
+++ azure-go-appservice/main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pulumi/pulumi-azure/sdk/go/azure/appinsights"
+	"github.com/pulumi/pulumi-azure/sdk/go/azure/appservice"
+	"github.com/pulumi/pulumi-azure/sdk/go/azure/core"
+	"github.com/pulumi/pulumi-azure/sdk/go/azure/sql"
+	"github.com/pulumi/pulumi-azure/sdk/go/azure/storage"
+	"github.com/pulumi/pulumi/sdk/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/go/pulumi/config"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// use first 10 characters of the stackname as prefix for resource names
+		prefix := ctx.Stack()
+		if len(prefix) > 10 {
+			prefix = prefix[:10]
+		}
+
+		resourceGroup, err := core.NewResourceGroup(ctx, prefix+"-rg", nil)
+		if err != nil {
+			return err
+		}
+
+		storageAccountName := strings.Replace(strings.ToLower(prefix), "-", "", -1) + "sa"
+		storageAccount, err := storage.NewAccount(ctx, storageAccountName, &storage.AccountArgs{
+			ResourceGroupName:      resourceGroup.Name,
+			Location:               resourceGroup.Location,
+			AccountKind:            pulumi.String("StorageV2"),
+			AccountTier:            pulumi.String("Standard"),
+			AccountReplicationType: pulumi.String("LRS"),
+		})
+		if err != nil {
+			return err
+		}
+
+		appServicePlan, err := appservice.NewPlan(ctx, prefix+"-asp", &appservice.PlanArgs{
+			ResourceGroupName: resourceGroup.Name,
+			Location:          resourceGroup.Location,
+			Kind:              pulumi.String("App"),
+			Sku: appservice.PlanSkuInputs{
+				Tier: pulumi.String("Basic"),
+				Size: pulumi.String("B1"),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		storageContainer, err := storage.NewContainer(ctx, prefix+"-c", &storage.ContainerArgs{
+			StorageAccountName:  storageAccount.Name,
+			ContainerAccessType: pulumi.String("private"),
+		})
+		if err != nil {
+			return err
+		}
+
+		blob, err := storage.NewZipBlob(ctx, prefix+"-b", &storage.ZipBlobArgs{
+			StorageAccountName:   storageAccount.Name,
+			StorageContainerName: storageContainer.Name,
+			Type:                 pulumi.String("block"),
+			Content:              pulumi.NewFileArchive("wwwroot"),
+		})
+		if err != nil {
+			return err
+		}
+
+		codeBlobURL := signedBlobReadURL(ctx, blob, storageAccount)
+
+		appInsights, err := appinsights.NewInsights(ctx, prefix+"-i", &appinsights.InsightsArgs{
+			ResourceGroupName: resourceGroup.Name,
+			Location:          resourceGroup.Location,
+			ApplicationType:   pulumi.String("Web"),
+		})
+		if err != nil {
+			return err
+		}
+
+		username := "pulumi"
+
+		// Get the password to use for SQL from config.
+		passwd := config.New(ctx, "").Require("sqlPassword")
+
+		sqlServer, err := sql.NewSqlServer(ctx, prefix+"-sql", &sql.SqlServerArgs{
+			ResourceGroupName:          resourceGroup.Name,
+			Location:                   resourceGroup.Location,
+			AdministratorLogin:         pulumi.String(username),
+			AdministratorLoginPassword: pulumi.String(passwd),
+			Version:                    pulumi.String("12.0"),
+		})
+		if err != nil {
+			return err
+		}
+
+		database, err := sql.NewDatabase(ctx, prefix+"-db", &sql.DatabaseArgs{
+			ResourceGroupName:             resourceGroup.Name,
+			Location:                      resourceGroup.Location,
+			ServerName:                    sqlServer.Name,
+			RequestedServiceObjectiveName: pulumi.String("S0"),
+		})
+		if err != nil {
+			return err
+		}
+
+		app, err := appservice.NewAppService(ctx, prefix+"-as", &appservice.AppServiceArgs{
+			ResourceGroupName: resourceGroup.Name,
+			Location:          resourceGroup.Location,
+			AppServicePlanId:  appServicePlan.ID(),
+			AppSettings: pulumi.StringMap{
+				"WEBSITE_RUN_FROM_ZIP":                   codeBlobURL,
+				"ApplicationInsights:InstrumentationKey": appInsights.InstrumentationKey,
+				"APPINSIGHTS_INSTRUMENTATIONKEY":         appInsights.InstrumentationKey,
+			},
+			ConnectionStrings: appservice.AppServiceConnectionStringsArray{
+				appservice.AppServiceConnectionStringsInputs{
+					Name: pulumi.String("db"),
+					Value: pulumi.Sprintf("Server=tcp:%s.database.windows.net;initial catalog=%s;user ID=%s;password=%s;Min Pool Size=0;Max Pool Size=30;Persist Security Info=true;",
+						sqlServer.Name, database.Name, username, passwd),
+					Type: pulumi.String("SQLAzure"),
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("endpoint", pulumi.Sprintf("https://%s", app.DefaultSiteHostname))
+		return nil
+	})
+}
+
+func signedBlobReadURL(ctx *pulumi.Context, blob *storage.ZipBlob, account *storage.Account) pulumi.StringOutput {
+	// Choose a fixed, far-future expiration date for signed blob URLs. The shared access signature
+	// (SAS) we generate for the Azure storage blob must remain valid for as long as the Function
+	// App is deployed, since new instances will download the code on startup. By using a fixed
+	// date, rather than (e.g.) "today plus ten years", the signing operation is idempotent.
+	const signatureExpiration = "2100-01-01"
+
+	return pulumi.All(account.Name, account.PrimaryConnectionString, blob.StorageContainerName, blob.Name).
+		ApplyT(func(args []interface{}) (string, error) {
+			accountName := args[0].(string)
+			connectionString := args[1].(string)
+			containerName := args[2].(string)
+			blobName := args[3].(string)
+
+			sas, err := storage.LookupAccountBlobContainerSAS(ctx, &storage.GetAccountBlobContainerSASArgs{
+				ConnectionString: connectionString,
+				ContainerName:    containerName,
+				Start:            "2019-01-01",
+				Expiry:           signatureExpiration,
+				Permissions:      storage.GetAccountBlobContainerSASPermissionsArgs{Read: true},
+			})
+			if err != nil {
+				return "", err
+			}
+			return fmt.Sprintf("https://%s.blob.core.windows.net/%s/%s%s", accountName, containerName, blobName, sas.Sas), nil
+		}).(pulumi.StringOutput)
+}
---
+++ azure-go-webserver-component/README.md
@@ -0,0 +1,63 @@
+[![Deploy](https://get.pulumi.com/new/button.svg)](https://app.pulumi.com/new)
+
+# Web Server Component Using Azure Virtual Machine
+
+This example provisions a configurable number of Linux web servers in an Azure Virtual Machine, and returns the
+resulting public IP addresses. This example uses a reusable [Pulumi component](
+https://www.pulumi.com/docs/intro/concepts/programming-model/#components) to simplify the creation of new virtual machines. By
+defining a `WebServer` class, we can hide many details (see [here](./webserver.ts) for its definition).
+
+## Prerequisites
+
+- [Node.js](https://nodejs.org/en/download/)
+- [Download and install the Pulumi CLI](https://www.pulumi.com/docs/get-started/install/)
+- [Connect Pulumi with your Azure account](https://www.pulumi.com/docs/intro/cloud-providers/azure/setup/) (if your `az` CLI is
+      configured, this will just work)
+
+## Running the App
+
+1.  Create a new stack:
+
+    ```
+    $ pulumi stack init dev
+    ```
+
+1.  Configure the deployment. The username and password here will be used to configure the Virtual Machine. The
+    password must adhere to the [Azure restrictions on VM passwords](
+    https://docs.microsoft.com/en-us/azure/virtual-machines/windows/faq#what-are-the-password-requirements-when-creating-a-vm).
+
+    ```
+    $ pulumi config set azure:location westus  # any valid Azure region will do
+    $ pulumi config set username webmaster
+    $ pulumi config set password <your-password> --secret
+    $ pulumi config set count 5                # optional -- will default to 2 if left out
+    ```
+
+    Note that `--secret` ensures your password is encrypted safely.
+
+1.  Login to Azure CLI (you will be prompted to do this during deployment if you forget this step):
+
+    ```
+    $ az login
+    ```
+
+1.  Run `pulumi up` to preview and deploy changes:
+
+    ``` 
+    $ pulumi up
+    Previewing changes:
+    ...
+
+    Performing changes:
+    ...
+    info: 15 changes performed:
+        + 15 resources created
+    Update duration: 4m27s
+    ```
+
+1.  Check the resulting IP addresses:
+
+    ```
+    $ pulumi stack output ipAddresses
+    [ 40.112.181.239, ..., 40.112.181.240 ]
+    ```
