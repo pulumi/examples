83e29e10469c90cd51e324983d5c8c0c46a568ea
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-01-13 18:31:13 -0800

    Add new file

---
+++ aws-ts-ec2-provisioners/util.ts
@@ -0,0 +1,14 @@
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+
+import * as crypto from "crypto";
+import * as fs from "fs";
+import * as path from "path";
+
+/**
+ * Calculates a hash for all of the files under the scripts directory.
+ */
+export function getFileHash(filename: string): string {
+	const data = fs.readFileSync(path.join(__dirname, filename), {encoding: "utf8"});
+	const hash = crypto.createHash("md5").update(data, "utf8");
+	return hash.digest("hex");
+}
