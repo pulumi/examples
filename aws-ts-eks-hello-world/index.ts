7472b947d6fd91603da6f5ac3c57e04311d521a9
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2020-01-15 23:10:42 -0800

    Update aws-ts-eks-hello-world/index.ts

--- aws-ts-eks-hello-world/index.ts
+++ aws-ts-eks-hello-world/index.ts
@@ -8,7 +8,7 @@ import * as pulumi from "@pulumi/pulumi";
 const name = "helloworld";
 
 // Create an EKS cluster with non-default configuration
-const vpc = new awsx.ec2.Vpc("vpc");
+const vpc = new awsx.ec2.Vpc("vpc", { numberOfAvailabilityZones: 2 });
 
 const cluster = new eks.Cluster(name, {
     vpcId: vpc.vpcId,
