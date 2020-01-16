d27bf1a5f62c78d8d766795c9d13c05d06acabaf
CyrusNajmabadi <cyrus.najmabadi@gmail.com>
2020-01-15 23:10:55 -0800

    Update aws-ts-eks/index.ts

--- aws-ts-eks/index.ts
+++ aws-ts-eks/index.ts
@@ -4,7 +4,7 @@ import * as awsx from "@pulumi/awsx";
 import * as eks from "@pulumi/eks";
 
 // Create a VPC for our cluster.
-const vpc = new awsx.ec2.Vpc("vpc");
+const vpc = new awsx.ec2.Vpc("vpc", { numberOfAvailabilityZones: 2 });
 
 // Create the EKS cluster itself, including a "gp2"-backed StorageClass and a deployment of the Kubernetes dashboard.
 const cluster = new eks.Cluster("cluster", {
