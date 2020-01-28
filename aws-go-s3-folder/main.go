bc2df62e58afd837faab1351adc608be7d3d9f08
Pat Gavlin <pat@pulumi.com>
2020-01-27 19:47:08 -0800

    Simplify

--- aws-go-s3-folder/main.go
+++ aws-go-s3-folder/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"mime"
 	"path"
@@ -33,20 +31,33 @@ func main() {
 		}
 		for _, item := range files {
 			name := item.Name()
-			filePath := filepath.Join(siteDir, name)
 			if _, err := s3.NewBucketObject(ctx, name, &s3.BucketObjectArgs{
 				Bucket:      siteBucket.ID(),                                     // reference to the s3.Bucket object
-				Source:      pulumi.NewFileAsset(filePath),                       // use FileAsset to point to a file
+				Source:      pulumi.NewFileAsset(filepath.Join(siteDir, name)),   // use FileAsset to point to a file
 				ContentType: pulumi.String(mime.TypeByExtension(path.Ext(name))), // set the MIME type of the file
 			}); err != nil {
 				return err
 			}
 		}
 
-		// Set the access policy for the bucket so all objects are readable
+		// Set the access policy for the bucket so all objects are readable.
 		if _, err := s3.NewBucketPolicy(ctx, "bucketPolicy", &s3.BucketPolicyArgs{
-			Bucket: siteBucket.ID(),                                        // refer to the bucket created earlier
-			Policy: siteBucket.ID().ApplyString(publicReadPolicyForBucket), // use output property `siteBucket.bucket`
+			Bucket: siteBucket.ID(), // refer to the bucket created earlier
+			Policy: pulumi.Any(map[string]interface{}{
+				"Version": "2012-10-17",
+				"Statement": []map[string]interface{}{
+					{
+						"Effect":    "Allow",
+						"Principal": "*",
+						"Action": []interface{}{
+							"s3:GetObject",
+						},
+						"Resource": []interface{}{
+							pulumi.Sprintf("arn:aws:s3:::%s/*", siteBucket.ID()), // policy refers to bucket name explicitly
+						},
+					},
+				},
+			}),
 		}); err != nil {
 			return err
 		}
@@ -56,24 +67,4 @@ func main() {
 		ctx.Export("websiteUrl", siteBucket.WebsiteEndpoint)
 		return nil
 	})
-}
-
-// Create an S3 Bucket Policy to allow public read of all objects in bucket.
-func publicReadPolicyForBucket(bucketName pulumi.ID) string {
-	policy, _ := json.Marshal(map[string]interface{}{
-		"Version": "2012-10-17",
-		"Statement": []map[string]interface{}{
-			{
-				"Effect":    "Allow",
-				"Principal": "*",
-				"Action": []string{
-					"s3:GetObject",
-				},
-				"Resource": []string{
-					fmt.Sprintf("arn:aws:s3:::%s/*", bucketName), // policy refers to bucket name explicitly
-				},
-			},
-		},
-	})
-	return string(policy)
 }
