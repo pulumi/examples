b88f385a8951f81fe41cb4f40eb940b074e9263b
Pat Gavlin <pat@pulumi.com>
2020-01-30 08:01:27 -0800

    Update Go examples for v1.9.1. (#527)
    * Update Go examples for v1.9.1.
    
    * Simplify

--- aws-go-s3-folder/main.go
+++ aws-go-s3-folder/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"mime"
 	"path"
@@ -10,15 +8,14 @@ import (
 
 	"github.com/pulumi/pulumi-aws/sdk/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/go/pulumi/asset"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create a bucket and expose a website index document
 		siteBucket, err := s3.NewBucket(ctx, "s3-website-bucket", &s3.BucketArgs{
-			Website: map[string]interface{}{
-				"indexDocument": "index.html",
+			Website: s3.BucketWebsiteArgs{
+				IndexDocument: pulumi.String("index.html"),
 			},
 		})
 		if err != nil {
@@ -34,47 +31,40 @@ func main() {
 		}
 		for _, item := range files {
 			name := item.Name()
-			filePath := filepath.Join(siteDir, name)
 			if _, err := s3.NewBucketObject(ctx, name, &s3.BucketObjectArgs{
-				Bucket:      siteBucket.ID(),                      // reference to the s3.Bucket object
-				Source:      asset.NewFileAsset(filePath),         // use FileAsset to point to a file
-				ContentType: mime.TypeByExtension(path.Ext(name)), // set the MIME type of the file
+				Bucket:      siteBucket.ID(),                                     // reference to the s3.Bucket object
+				Source:      pulumi.NewFileAsset(filepath.Join(siteDir, name)),   // use FileAsset to point to a file
+				ContentType: pulumi.String(mime.TypeByExtension(path.Ext(name))), // set the MIME type of the file
 			}); err != nil {
 				return err
 			}
 		}
 
-		// Set the access policy for the bucket so all objects are readable
+		// Set the access policy for the bucket so all objects are readable.
 		if _, err := s3.NewBucketPolicy(ctx, "bucketPolicy", &s3.BucketPolicyArgs{
-			Bucket: siteBucket.ID(),                                  // refer to the bucket created earlier
-			Policy: siteBucket.ID().Apply(publicReadPolicyForBucket), // use output property `siteBucket.bucket`
+			Bucket: siteBucket.ID(), // refer to the bucket created earlier
+			Policy: pulumi.Any(map[string]interface{}{
+				"Version": "2012-10-17",
+				"Statement": []map[string]interface{}{
+					{
+						"Effect":    "Allow",
+						"Principal": "*",
+						"Action": []interface{}{
+							"s3:GetObject",
+						},
+						"Resource": []interface{}{
+							pulumi.Sprintf("arn:aws:s3:::%s/*", siteBucket.ID()), // policy refers to bucket name explicitly
+						},
+					},
+				},
+			}),
 		}); err != nil {
 			return err
 		}
 
 		// Stack exports
 		ctx.Export("bucketName", siteBucket.ID())
-		ctx.Export("websiteUrl", siteBucket.WebsiteEndpoint())
+		ctx.Export("websiteUrl", siteBucket.WebsiteEndpoint)
 		return nil
 	})
-}
-
-// Create an S3 Bucket Policy to allow public read of all objects in bucket.
-func publicReadPolicyForBucket(bucketName pulumi.ID) (interface{}, error) {
-	policy, _ := json.Marshal(map[string]interface{}{
-		"Version": "2012-10-17",
-		"Statement": []map[string]interface{}{
-			{
-				"Effect":    "Allow",
-				"Principal": "*",
-				"Action": []string{
-					"s3:GetObject",
-				},
-				"Resource": []string{
-					fmt.Sprintf("arn:aws:s3:::%s/*", bucketName), // policy refers to bucket name explicitly
-				},
-			},
-		},
-	})
-	return string(policy), nil
 }
