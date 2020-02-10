7e669ceba1cc2be901a9a3ca92c7a2a93f7c244b
Mark Lambert <marklam@spunow.co.uk>
2020-02-10 21:11:50 +0000

    Creating AWS API Gateway entry for a lambda in F# (#525)
    Creating AWS API Gateway entry for a lambda in F#

---
+++ aws-fs-lambda-webserver/README.md
@@ -0,0 +1,28 @@
+# AWS F# Lambda Web Server
+This example creates a web server in AWS lambda using the Giraffe web server
+
+## Deploying the App
+
+To deploy your infrastructure, follow the below steps.
+
+### Prerequisites
+
+1. [Install Pulumi](https://www.pulumi.com/docs/get-started/install/)
+2. [Configure AWS Credentials](https://www.pulumi.com/docs/intro/cloud-providers/aws/setup/)
+
+### Steps
+
+After cloning this repo, from this working directory, run these commands:
+
+1. Build and publish the lambda function, making the output available to our Pulumi program. 
+
+```bash
+dotnet publish ./LambdaWebService
+```
+
+2. Execute our Pulumi program to archive our published function output, and create our lambda. 
+```bash
+pulumi up -C ./pulumi
+```
+
+3. In a browser, navigate to the URL for `websiteUrl`. You should see the welcome message.
---
+++ aws-fs-lambda-webserver/pulumi/Program.fs
@@ -0,0 +1,147 @@
+﻿open Pulumi
+open Pulumi.FSharp
+open Pulumi.Aws
+open Pulumi.Aws.ApiGateway
+open Pulumi.Aws.Iam
+open Pulumi.Aws.Lambda
+
+module ManagedPolicies =
+    let AWSLambdaBasicExecutionRole = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+    let AWSLambdaFullAccess         = "arn:aws:iam::aws:policy/AWSLambdaFullAccess"
+
+let openApiSpec (name, arn) =
+    let quotedTitle = "\"" + name + "api\""
+    let quotedUri   = sprintf "\"arn:aws:apigateway:%s:lambda:path/2015-03-31/functions/%s/invocations\"" Pulumi.Aws.Config.Region arn
+    """{
+  "openapi": "3.0.1",
+  "info": {
+    "title": """ + quotedTitle + """,
+    "version": "1.0"
+  },
+  "paths": {
+    "/{proxy+}": {
+      "x-amazon-apigateway-any-method": {
+        "x-amazon-apigateway-integration": {
+          "uri": """ + quotedUri + """,
+          "passthroughBehavior": "when_no_match",
+          "httpMethod": "POST",
+          "type": "aws_proxy"
+        }
+      }
+    },
+    "/": {
+      "x-amazon-apigateway-any-method": {
+        "x-amazon-apigateway-integration": {
+          "uri": """ + quotedUri + """,
+          "passthroughBehavior": "when_no_match",
+          "httpMethod": "POST",
+          "type": "aws_proxy"
+        }
+      }
+    }
+  },
+  "components": {}
+}"""
+
+let restApiArgs lambdaName lambdaArn =
+    let openApiSpec =
+        Outputs.pair lambdaName lambdaArn
+        |> Outputs.apply openApiSpec
+
+    RestApiArgs(Body = io openApiSpec)
+
+let addInvokePermission name accountId functionArn executionArn =
+    Permission(
+        name,
+        PermissionArgs(
+            Action = input "lambda:InvokeFunction",
+            Function = functionArn,
+            Principal = input "apigateway.amazonaws.com",
+            SourceAccount = accountId,
+            SourceArn = executionArn,
+            StatementIdPrefix = input "lambdaPermission"
+        )
+    )
+    |> ignore
+
+let infra () =
+    let configSection = Pulumi.Config()
+    let accountId = configSection.Get "awsAccount"
+
+    let lambdaRole =
+        Role (
+            "lambdaRole",
+            RoleArgs(
+                AssumeRolePolicy = input
+                    """{
+                        "Version": "2012-10-17",
+                        "Statement": [
+                            {
+                                "Action": "sts:AssumeRole",
+                                "Principal": {
+                                    "Service": "lambda.amazonaws.com"
+                                },
+                                "Effect": "Allow",
+                                "Sid": ""
+                            }
+                        ]
+                    }"""
+                )
+            )
+
+    RolePolicyAttachment("lambdaS3ReadOnlyAccess", RolePolicyAttachmentArgs(Role = io lambdaRole.Id, PolicyArn = input ManagedPolicies.AWSLambdaFullAccess))         |> ignore
+    RolePolicyAttachment("lambdaBasicExecution",   RolePolicyAttachmentArgs(Role = io lambdaRole.Id, PolicyArn = input ManagedPolicies.AWSLambdaBasicExecutionRole)) |> ignore
+
+    let lambda =
+        Function(
+            "basicLambda",
+            FunctionArgs(
+                Runtime = input "dotnetcore2.1",
+                Code    = input (FileArchive "../LambdaWebServer/bin/Debug/netcoreapp2.1/publish" :> Archive),
+                Handler = input "LambdaWebServer::Setup+LambdaEntryPoint::FunctionHandlerAsync",
+                Role    = io lambdaRole.Arn,
+                Timeout = input 30
+            )
+        )
+
+    let restApi =
+        RestApi(
+            "websiteApi",
+            (restApiArgs lambda.Name lambda.Arn)
+        )
+
+    let deployment =
+        Deployment(
+            "websiteApiDeployment",
+            DeploymentArgs(
+                RestApi   = io restApi.Id,
+                StageName = input ""
+            )
+        )
+
+    let prodStage =
+        Stage(
+            "websiteApiProd",
+            StageArgs(
+                RestApi    = io restApi.Id,
+                Deployment = io deployment.Id,
+                StageName  = input "Prod"
+            )
+        )
+
+    addInvokePermission "lambdaPermission1" (input accountId) (io lambda.Arn) (deployment.ExecutionArn |> Outputs.apply (fun arn -> arn + "*/*/")  |> io)
+    addInvokePermission "lambdaPermission2" (input accountId) (io lambda.Arn) (deployment.ExecutionArn |> Outputs.apply (fun arn -> arn + "*/*/*") |> io)
+
+    let urlWithTrailingSlash =
+        prodStage.InvokeUrl
+        |> Outputs.apply (fun url -> url + "/")
+
+    dict [
+        ("account",    accountId :> obj);
+        ("websiteUrl", urlWithTrailingSlash :> obj)
+    ]
+
+[<EntryPoint>]
+let main _argv =
+    Deployment.run infra
+
