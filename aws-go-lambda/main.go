1aa6acfe43ca4254aae3e26608d33e51f0d673b3
Tasia Halim <tasia@pulumi.com>
2020-02-12 16:53:32 -0800

    Created example for AWS Lambda in Go (#553)

---
+++ aws-go-lambda/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-aws/sdk/go/aws/iam"
+	"github.com/pulumi/pulumi-aws/sdk/go/aws/lambda"
+	"github.com/pulumi/pulumi/sdk/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create an IAM role.
+		role, err := iam.NewRole(ctx, "task-exec-role", &iam.RoleArgs{
+			AssumeRolePolicy: pulumi.String(`{
+				"Version": "2012-10-17",
+				"Statement": [{
+					"Sid": "",
+					"Effect": "Allow",
+					"Principal": {
+						"Service": "lambda.amazonaws.com"
+					},
+					"Action": "sts:AssumeRole"
+				}]
+			}`),
+		})
+		if err != nil {
+			return err
+		}
+
+		// Set arguments for constructing the function resource.
+		args := &lambda.FunctionArgs{
+			Handler: pulumi.String("handler"),
+			Role:    role.Arn,
+			Runtime: pulumi.String("go1.x"),
+			Code:    pulumi.NewFileArchive("./handler/handler.zip"),
+		}
+
+		// Create the lambda using the args.
+		function, err := lambda.NewFunction(ctx, "basicLambda", args)
+		if err != nil {
+			return err
+		}
+
+		// Export the lambda ARN.
+		ctx.Export("lambda", function.Arn)
+
+		return nil
+	})
+}
