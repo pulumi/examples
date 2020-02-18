ee51d32d09883f280902f506c8e016e5318c3243
Tasia Halim <tasia@pulumi.com>
2020-02-17 16:00:22 -0800

    Added gcp-go-functions (#563)
    * added gcp-go-functions
    
    * updated Gopkg.toml

---
+++ gcp-go-functions/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-gcp/sdk/go/gcp/cloudfunctions"
+	"github.com/pulumi/pulumi-gcp/sdk/go/gcp/storage"
+	"github.com/pulumi/pulumi/sdk/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create a bucket.
+		bucket, err := storage.NewBucket(ctx, "bucket", nil)
+		if err != nil {
+			return err
+		}
+
+		// Create an object in our bucket with our function.
+		bucketObjectArgs := &storage.BucketObjectArgs{
+			Bucket: bucket.Name,
+			Source: pulumi.NewFileArchive("gofunc"),
+		}
+		bucketObject, err := storage.NewBucketObject(ctx, "go-zip", bucketObjectArgs)
+		if err != nil {
+			return err
+		}
+
+		// Set arguments for creating the function resource.
+		args := &cloudfunctions.FunctionArgs{
+			SourceArchiveBucket: bucket.Name,
+			Runtime:             pulumi.String("go111"),
+			SourceArchiveObject: bucketObject.Name,
+			EntryPoint:          pulumi.String("Handler"),
+			TriggerHttp:         pulumi.Bool(true),
+			AvailableMemoryMb:   pulumi.Int(128),
+		}
+
+		// Create the function using the args.
+		function, err := cloudfunctions.NewFunction(ctx, "basicFunction", args)
+		if err != nil {
+			return err
+		}
+
+		// Export the tigger URL.
+		ctx.Export("function", function.HttpsTriggerUrl)
+		return nil
+	})
+}
