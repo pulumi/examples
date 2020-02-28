6555d706854871b9be4316b6bb750d90d8d05eb3
Tasia Halim <tasiahalim@Tasias-MBP.lan>
2020-02-28 13:48:58 -0800

    started azure-go-aci

---
+++ azure-go-aci/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-azure/sdk/go/azure/containerservice"
+	"github.com/pulumi/pulumi-azure/sdk/go/azure/core"
+	"github.com/pulumi/pulumi/sdk/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create a resource group.
+		resourceGroup, err := core.NewResourceGroup(ctx, "aci-rg", nil)
+		if err != nil {
+			return err
+		}
+
+		// Create a registry.
+		registryArgs := containerservice.RegistryArgs{
+			ResourceGroupName: resourceGroup.Name,
+			AdminEnabled:      pulumi.Bool(true),
+			Sku:               pulumi.String("Premium"),
+		}
+		registry, err := containerservice.NewRegistry(ctx, "registry", &registryArgs)
+		if err != nil {
+			return err
+		}
+
+		// Create the docker image.
+
+		// Create a group.
+		credentialArgs := containerservice.GroupImageRegistryCredentialArgs{
+			Server:   registry.LoginServer,
+			Username: registry.AdminUsername,
+			Password: registry.AdminPassword,
+		}
+		portArgs := containerservice.GroupContainerPortArgs{
+			Port:     pulumi.Int(80),
+			Protocol: pulumi.String("TCP"),
+		}
+		containerArgs := containerservice.GroupContainerArgs{
+			Cpu:    pulumi.Float64(0.5),
+			Memory: pulumi.Float64(1.5),
+			Name:   pulumi.String("hello-world"),
+			Ports:  containerservice.GroupContainerPortArray([]containerservice.GroupContainerPortInput{portArgs}),
+		}
+		groupArgs := containerservice.GroupArgs{
+			ResourceGroupName:        resourceGroup.Name,
+			ImageRegistryCredentials: containerservice.GroupImageRegistryCredentialArray([]containerservice.GroupImageRegistryCredentialInput{credentialArgs}),
+			OsType:                   pulumi.String("Linux"),
+			Containers:               containerservice.GroupContainerArray([]containerservice.GroupContainerInput{containerArgs}),
+			IpAddressType:            pulumi.String("public"),
+			DnsNameLabel:             pulumi.String("acigo"),
+		}
+		group, err := containerservice.NewGroup(ctx, "aci", &groupArgs)
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("endpoint", group.Fqdn)
+		return nil
+	})
+}
