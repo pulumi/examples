9b1d5b504ecc4572fe48c95981b98759b04fabbc
clstokes <cameron@cameronstokes.com>
2020-03-09 21:35:16 -0700

    Update `aws-go-webserver` to use `VpcSecurityGroupIds`, not `SecurityGroups`.
    `SecurityGroups` doesn't allow for updates while `VpcSecurityGroupIds` does.
    
    See https://github.com/pulumi/pulumi-aws/issues/852 for more context.

--- aws-go-webserver/main.go
+++ aws-go-webserver/main.go
@@ -41,10 +41,10 @@ func main() {
 
 		// Create a simple web server using the startup script for the instance.
 		srv, err := ec2.NewInstance(ctx, "web-server-www", &ec2.InstanceArgs{
-			Tags:           pulumi.Map{"Name": pulumi.String("web-server-www")},
-			InstanceType:   pulumi.String("t2.micro"), // t2.micro is available in the AWS free tier.
-			SecurityGroups: pulumi.StringArray{group.Name},
-			Ami:            pulumi.String(ami.Id),
+			Tags:                pulumi.Map{"Name": pulumi.String("web-server-www")},
+			InstanceType:        pulumi.String("t2.micro"), // t2.micro is available in the AWS free tier.
+			VpcSecurityGroupIds: pulumi.StringArray{group.ID()},
+			Ami:                 pulumi.String(ami.Id),
 			UserData: pulumi.String(`#!/bin/bash
 echo "Hello, World!" > index.html
 nohup python -m SimpleHTTPServer 80 &`),
