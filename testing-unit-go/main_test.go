c0f25023171afe364d311eae23a7ed2cf67cf116
Mikhail Shilkov <github@mikhail.io>
2020-04-01 18:03:49 +0200

    Update the Go unit test example

--- testing-unit-go/main_test.go
+++ testing-unit-go/main_test.go
@@ -1,63 +1,78 @@
 package main
 
 import (
+	"sync"
 	"testing"
 
+	"github.com/pulumi/pulumi-aws/sdk/go/aws/ec2"
 	"github.com/pulumi/pulumi/sdk/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/go/pulumi"
 	"github.com/stretchr/testify/assert"
 )
 
-type testMonitor struct {
-	CallF        func(tok string, args resource.PropertyMap, provider string) (resource.PropertyMap, error)
-	NewResourceF func(typeToken, name string, inputs resource.PropertyMap,
-		provider, id string) (string, resource.PropertyMap, error)
+type mocks int
+
+// Create the mock.
+func (mocks) NewResource(typeToken, name string, inputs resource.PropertyMap, provider, id string) (string, resource.PropertyMap, error) {
+	return name + "_id", inputs, nil
 }
 
-func (m *testMonitor) Call(tok string, args resource.PropertyMap, provider string) (resource.PropertyMap, error) {
-	if m.CallF == nil {
-		return resource.PropertyMap{}, nil
-	}
-	return m.CallF(tok, args, provider)
+func (mocks) Call(token string, args resource.PropertyMap, provider string) (resource.PropertyMap, error) {
+	return args, nil
 }
 
-func (m *testMonitor) NewResource(typeToken, name string, inputs resource.PropertyMap,
-	provider, id string) (string, resource.PropertyMap, error) {
-
-	if m.NewResourceF == nil {
-		return name, resource.PropertyMap{}, nil
-	}
-	return m.NewResourceF(typeToken, name, inputs, provider, id)
-}
-
-func TestCreateBucket(t *testing.T) {
-	bucketName := "my-bucket-unique-name"
-
-	mocks := &testMonitor{
-		NewResourceF: func(typeToken, name string, inputs resource.PropertyMap,
-			provider, id string) (string, resource.PropertyMap, error) {
-			assert.Equal(t, "aws:s3/bucket:Bucket", typeToken)
-			return id, resource.NewPropertyMapFromMap(map[string]interface{}{
-				"bucket": bucketName,
-			}), nil
-		},
-	}
-
+// Applying unit tests.
+func TestInfrastructure(t *testing.T) {
 	err := pulumi.RunErr(func(ctx *pulumi.Context) error {
-		bucket, err := CreateBucket(ctx)
+		infra, err := createInfrastructure(ctx)
 		assert.NoError(t, err)
 
-		resultChan := make(chan string)
+		var wg sync.WaitGroup
+		wg.Add(3)
 
-		bucket.Bucket.ApplyT(func(val string) (string, error) {
-			resultChan <- val
-			return val, nil
+		// Test if the service has tags and a name tag.
+		pulumi.All(infra.server.URN(), infra.server.Tags).ApplyT(func(all []interface{}) error {
+			urn := all[0].(pulumi.URN)
+			tags := all[1].(map[string]interface{})
+
+			assert.Containsf(t, tags, "Name", "missing a Name tag on server %v", urn)
+			wg.Done()
+			return nil
 		})
 
-		actual := <-resultChan
-		assert.Equal(t, bucketName, actual)
+		// Test if the instance is configured with user_data.
+		pulumi.All(infra.server.URN(), infra.server.UserData).ApplyT(func(all []interface{}) error {
+			urn := all[0].(pulumi.URN)
+			userData := all[1].(*string)
 
+			assert.Nilf(t, userData, "illegal use of userData on server %v", urn)
+			wg.Done()
+			return nil
+		})
+
+		// Test if port 22 for ssh is exposed.
+		pulumi.All(infra.group.URN(), infra.group.Ingress).ApplyT(func(all []interface{}) error {
+			urn := all[0].(pulumi.URN)
+			ingress := all[1].([]ec2.SecurityGroupIngress)
+
+			for _, i := range ingress {
+				openToInternet := false
+				for _, b := range i.CidrBlocks {
+					if b == "0.0.0.0/0" {
+						openToInternet = true
+						break
+					}
+				}
+
+				assert.Falsef(t, i.FromPort == 22 && openToInternet, "illegal SSH port 22 open to the Internet (CIDR 0.0.0.0/0) on group %v", urn)
+			}
+
+			wg.Done()
+			return nil
+		})
+
+		wg.Wait()
 		return nil
-	}, pulumi.WithMocks("project", "stack", mocks))
+	}, pulumi.WithMocks("project", "stack", mocks(0)))
 	assert.NoError(t, err)
 }
