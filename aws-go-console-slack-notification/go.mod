910ce61bcee71de888598eb2318776cafbf126ed
clstokes <cameron@cameronstokes.com>
2020-04-20 20:41:03 -0700

    Update for Pulumi 2.0.

--- aws-go-console-slack-notification/go.mod
+++ aws-go-console-slack-notification/go.mod
@@ -3,10 +3,8 @@ module github.com/pulumi/examples/aws-go-console-slack-notification
 go 1.14
 
 require (
-	github.com/aws/aws-lambda-go v1.16.0
+	github.com/aws/aws-lambda-go v1.16.0 // indirect
 	github.com/aws/aws-sdk-go v1.30.8
-	github.com/pulumi/pulumi v1.14.1
-	github.com/pulumi/pulumi-aws v1.31.0
-	github.com/pulumi/pulumi-aws/sdk v1.31.0
-	github.com/pulumi/pulumi/sdk v1.14.1
+	github.com/pulumi/pulumi-aws/sdk/v2 v2.1.0
+	github.com/pulumi/pulumi/sdk/v2 v2.0.0
 )
--- aws-go-console-slack-notification/main.go
+++ aws-go-console-slack-notification/main.go
@@ -6,14 +6,15 @@ import (
 	AWS "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/pulumi/pulumi-aws/sdk/go/aws"
-	"github.com/pulumi/pulumi-aws/sdk/go/aws/cloudtrail"
-	"github.com/pulumi/pulumi-aws/sdk/go/aws/cloudwatch"
-	"github.com/pulumi/pulumi-aws/sdk/go/aws/iam"
-	"github.com/pulumi/pulumi-aws/sdk/go/aws/lambda"
-	"github.com/pulumi/pulumi-aws/sdk/go/aws/s3"
-	"github.com/pulumi/pulumi/sdk/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/go/pulumi/config"
+
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws"
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/cloudtrail"
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/cloudwatch"
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/iam"
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/lambda"
+	"github.com/pulumi/pulumi-aws/sdk/v2/go/aws/s3"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
 )
 
 func main() {
