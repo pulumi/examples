86909795f7999f198f73fe1b2f63b8e6b8d1d6b2
Ant Seddon <anthony@vividcloud.co.uk>
2020-05-04 00:46:42 +0100

    Don't specify a storage class - AWS creates one (#674)

--- aws-ts-eks/index.ts
+++ aws-ts-eks/index.ts
@@ -6,7 +6,7 @@ import * as eks from "@pulumi/eks";
 // Create a VPC for our cluster.
 const vpc = new awsx.ec2.Vpc("vpc", { numberOfAvailabilityZones: 2 });
 
-// Create the EKS cluster itself, including a "gp2"-backed StorageClass and a deployment of the Kubernetes dashboard.
+// Create the EKS cluster itself and a deployment of the Kubernetes dashboard.
 const cluster = new eks.Cluster("cluster", {
     vpcId: vpc.id,
     subnetIds: vpc.publicSubnetIds,
@@ -14,7 +14,6 @@ const cluster = new eks.Cluster("cluster", {
     desiredCapacity: 2,
     minSize: 1,
     maxSize: 2,
-    storageClasses: "gp2",
     deployDashboard: true,
 });
 
