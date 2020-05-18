27630a1a572a092ea1d0044e1b474e1b35a2ca02
Mikhail Shilkov <github@mikhail.io>
2020-05-18 14:48:33 +0200

    Formatting

--- testing-unit-cs-mocks/WebsiteStackTests.cs
+++ testing-unit-cs-mocks/WebsiteStackTests.cs
@@ -1,3 +1,5 @@
+// Copyright 2016-2020, Pulumi Corporation
+
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
--- testing-unit-fs-mocks/Testing.fs
+++ testing-unit-fs-mocks/Testing.fs
@@ -1,3 +1,5 @@
+// Copyright 2016-2020, Pulumi Corporation
+
 namespace UnitTesting
 
 open System
