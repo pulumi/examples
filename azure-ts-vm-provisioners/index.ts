c4e53ed9953815da2d62744e0fab2411ca104fbf
evanboyle <evan@pulumi.com>
2020-06-11 11:50:59 -0700

    copyright

--- azure-ts-vm-provisioners/index.ts
+++ azure-ts-vm-provisioners/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
 import * as azure from "@pulumi/azure";
 import * as pulumi from "@pulumi/pulumi";
