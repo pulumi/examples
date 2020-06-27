d8016c65804ff2ecffa739c8fddfa9309adedd57
XUANHE ZHOU <xuanhezhou@zephyrs-pulumi-workbook.local>
2020-06-26 18:39:57 -0700

    Upgrade the version of netcoreapp to 3.1 and converted the aws-ts-serverless-raw to a python project

---
+++ aws-py-serverless-raw/app/Functions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
+using Newtonsoft.Json;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace app
+{
+    public class Counter
+    {
+        public string Id { get; set; }
+        public Int32 Count { get; set; }
+    }
+
+    public class Functions
+    {
+        // This const is the name of the environment variable that the serverless.template will use to set
+        // the name of the DynamoDB table used to store blog posts.
+        const string TABLENAME_ENVIRONMENT_VARIABLE_LOOKUP = "COUNTER_TABLE";
+
+        IDynamoDBContext DDBContext { get; set; }
+
+        /// <summary>
+        /// Default constructor that Lambda will invoke.
+        /// </summary>
+        public Functions()
+        {
+            // Check to see if a table name was passed in through environment variables and if so 
+            // add the table mapping.
+            var tableName = System.Environment.GetEnvironmentVariable(TABLENAME_ENVIRONMENT_VARIABLE_LOOKUP);
+            if(!string.IsNullOrEmpty(tableName))
+            {
+                AWSConfigsDynamoDB.Context.TypeMappings[typeof(Counter)] = new Amazon.Util.TypeMapping(typeof(Counter), tableName);
+            }
+
+            var config = new DynamoDBContextConfig { Conversion = DynamoDBEntryConversion.V2 };
+            this.DDBContext = new DynamoDBContext(new AmazonDynamoDBClient(), config);
+        }
+
+        /// <summary>
+        /// A Lambda function that returns a simple JSON object.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public async Task<APIGatewayProxyResponse> GetAsync(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            context.Logger.LogLine($"Getting count for '{request.Path}'");
+
+            var counter = await DDBContext.LoadAsync<Counter>(request.Path);
+            if (counter == null) {
+                counter = new Counter { Id = request.Path, Count = 1 };
+            }
+            var count = counter.Count++;
+            await DDBContext.SaveAsync<Counter>(counter);
+
+            context.Logger.LogLine($"Got count {count} for '{request.Path}'");
+
+            var response = new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Body = JsonConvert.SerializeObject(new { Path = request.Path, Count = count }),
+                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+            };
+
+            return response;
+        }
+
+    }
+}
