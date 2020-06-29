b521abc7656a275fc8e487594502607fd9dc3786
Levi Blackstone <levi@pulumi.com>
2020-06-29 16:42:24 -0600

    Fix TS PVC

--- kubernetes-ts-jenkins/jenkins.ts
+++ kubernetes-ts-jenkins/jenkins.ts
@@ -134,9 +134,6 @@ export class Instance extends pulumi.ComponentResource {
         const pvc = new k8s.core.v1.PersistentVolumeClaim(`${args.name}-pvc`, {
             metadata: {
                 name: args.name,
-                annotations: {
-                    "volume.beta.kubernetes.io/storage-class": `${args.storageClass || "standard" }`,
-                },
             },
             spec: {
                 accessModes: ["ReadWriteOnce"],
