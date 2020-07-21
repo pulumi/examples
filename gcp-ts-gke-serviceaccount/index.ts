f2ac0a6249ea2806cd8dbe5a9d5f42e429d383c1
Lee Briggs <lee@leebriggs.co.uk>
2020-07-21 16:31:42 -0700

    add a gke + service account example

---
+++ gcp-ts-gke-serviceaccount/index.ts
@@ -0,0 +1,153 @@
+// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+import * as gcp from "@pulumi/gcp";
+import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
+
+const config = new pulumi.Config();
+
+const name = config.get("name") ||
+    "gke-serviceaccount-example";
+export const masterVersion = config.get("masterVersion") ||
+    gcp.container.getEngineVersions().then(it => it.latestMasterVersion);
+const machineType = "n1-standard-1" || config.get("machineType")
+
+// Create a service account
+const serviceAccount = new gcp.serviceAccount.Account("serviceAccount", {
+    accountId: name,
+    displayName: "A service account for a GKE application",
+});
+
+const serviceAccountIAM = new gcp.projects.IAMBinding("serviceAccount-pub", {
+    role: "roles/pubsub.subscriber",
+    members: [pulumi.interpolate`serviceAccount:${serviceAccount.email}`]
+}, {parent: serviceAccount})
+
+const serviceAccountKey = new gcp.serviceAccount.Key("serviceAccount-key", {
+    serviceAccountId: serviceAccount.name,
+    publicKeyType: "TYPE_X509_PEM_FILE",
+}, {parent: serviceAccount, additionalSecretOutputs: ['privateKey']})
+
+// Create a GKE cluster
+const cluster = new gcp.container.Cluster(name, {
+    initialNodeCount: 2,
+    minMasterVersion: masterVersion,
+    nodeVersion: masterVersion,
+    nodeConfig: {
+        machineType: machineType,
+        oauthScopes: [
+            "https://www.googleapis.com/auth/compute",
+            "https://www.googleapis.com/auth/devstorage.read_only",
+            "https://www.googleapis.com/auth/logging.write",
+            "https://www.googleapis.com/auth/monitoring",
+        ],
+    },
+});
+
+// Export the Cluster name
+export const clusterName = cluster.name;
+
+// Manufacture a GKE-style kubeconfig. Note that this is slightly "different"
+// because of the way GKE requires gcloud to be in the picture for cluster
+// authentication (rather than using the client cert/key directly).
+export const kubeconfig = pulumi.all([cluster.name, cluster.endpoint, cluster.masterAuth]).apply(([name, endpoint, masterAuth]) => {
+    const context = `${gcp.config.project}_${gcp.config.zone}_${name}`;
+    return `apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: ${masterAuth.clusterCaCertificate}
+    server: https://${endpoint}
+  name: ${context}
+contexts:
+- context:
+    cluster: ${context}
+    user: ${context}
+  name: ${context}
+current-context: ${context}
+kind: Config
+preferences: {}
+users:
+- name: ${context}
+  user:
+    auth-provider:
+      config:
+        cmd-args: config config-helper --format=json
+        cmd-path: gcloud
+        expiry-key: '{.credential.token_expiry}'
+        token-key: '{.credential.access_token}'
+      name: gcp
+`;
+});
+
+// Create a Kubernetes provider instance that uses our cluster from above.
+const clusterProvider = new k8s.Provider(name, {
+    kubeconfig: kubeconfig,
+});
+
+const appLabels = {appClass: "pubsub"};
+
+// Create a Kubernetes Namespace
+const ns = new k8s.core.v1.Namespace("pubsub-ns", {
+    metadata: {
+        name: "pubsub",
+        labels: appLabels,
+    }
+}, {provider: clusterProvider});
+
+
+const gcpCredentials = new k8s.core.v1.Secret('gcp-credentials', {
+    metadata: {
+        namespace: ns.metadata.name,
+        labels: appLabels,
+    },
+    type: 'Opaque',
+    stringData: {
+        'gcp-credentials.json': serviceAccountKey.privateKey.apply((x) => Buffer.from(x, 'base64').toString('utf8')),
+    },
+}, {provider: clusterProvider, parent: ns});
+
+const deployment = new k8s.apps.v1.Deployment("pubsub",
+    {
+        metadata: {
+            namespace: ns.metadata.name,
+            labels: appLabels,
+        },
+        spec: {
+            replicas: 1,
+            selector: {matchLabels: appLabels},
+            template: {
+                metadata: {
+                    labels: appLabels,
+                },
+                spec: {
+                    volumes: [
+                        {
+                            name: 'google-cloud-key',
+                            secret: {
+                                secretName: gcpCredentials.metadata.name,
+                            }
+                        }
+                    ],
+                    containers: [
+                        {
+                            name: "pubsub-example",
+                            image: "gcr.io/google-samples/pubsub-sample:v1",
+                            volumeMounts: [
+                                {
+                                    name: "google-cloud-key",
+                                    mountPath: "/var/secrets/google"
+                                }
+                            ],
+                            env: [{
+                                name: "GOOGLE_APPLICATION_CREDENTIALS",
+                                value: "/var/secrets/google/gcp-credentials.json"
+                            }]
+                        },
+                    ],
+                },
+            },
+        },
+    },
+    {
+        provider: clusterProvider, parent: ns
+    },
+);
