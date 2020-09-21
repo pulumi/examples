5f209605b95b75fa7899be48315aad5a401a9f05
Tushar Shah <tushar.shah1@gmail.com>
2020-09-21 15:42:25 -0400

    fixed name tag

--- aws-ts-vpc-with-ecs-fargate-py/vpc-crosswalk-ts/index.ts
+++ aws-ts-vpc-with-ecs-fargate-py/vpc-crosswalk-ts/index.ts
@@ -8,10 +8,10 @@ const vpc_name = config.require("vpc_name");
 const zone_number = config.requireNumber("zone_number");
 const vpc_cidr = config.require("vpc_cidr");
 const number_of_nat_gateways = config.requireNumber("number_of_nat_gateways");
-const config_name = "Pulumi." + `${env}` + ".yaml";
 
 const baseTags = {
-    Name: `${vpc_name}`,
+    //Name: `${vpc_name}`,
+    "Name": `${vpc_name}`,
     "availability_zones_used": `${zone_number}`,
     "cidr_block": `${vpc_cidr}`,
     "crosswalk":"yes",
@@ -20,7 +20,6 @@ const baseTags = {
     "pulumi:Project": pulumi.getProject(),
     "pulumi:Stack": pulumi.getStack(),
     "cost_center": "1234",
-    "pulumi:Configs": config_name,
 }
 
 // Allocate a new VPC with the CIDR range from config file:
