3340633421dc775568c5a76adf8b97b681501953
Piers Karsenbarg <piers@pulumi.com>
2020-12-21 16:55:10 +0000

    Fixed linting

--- aws-ts-apigatewayv2-quickcreate/index.ts
+++ aws-ts-apigatewayv2-quickcreate/index.ts
@@ -1,49 +1,53 @@
-import * as pulumi from "@pulumi/pulumi";
+// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
+
 import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi/pulumi";
 
 // Create the role for the Lambda to assume
 const lambdaRole = new aws.iam.Role("lambdaRole", {
-    assumeRolePolicy: {
-       Version: "2012-10-17",
-       Statement: [{
-          Action: "sts:AssumeRole",
-          Principal: {
-             Service: "lambda.amazonaws.com",
-          },
-          Effect: "Allow",
-          Sid: "",
-       }],
-    },
- });
+  assumeRolePolicy: {
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Action: "sts:AssumeRole",
+        Principal: {
+          Service: "lambda.amazonaws.com",
+        },
+        Effect: "Allow",
+        Sid: "",
+      },
+    ],
+  },
+});
 
- // Attach the fullaccess policy to the Lambda role created above
- new aws.iam.RolePolicyAttachment("lambdaRoleAttachment", {
-    role: lambdaRole,
-    policyArn: aws.iam.ManagedPolicies.AWSLambdaFullAccess,
- });
+// Attach the fullaccess policy to the Lambda role created above
+new aws.iam.RolePolicyAttachment("lambdaRoleAttachment", {
+  role: lambdaRole,
+  policyArn: aws.iam.ManagedPolicies.AWSLambdaFullAccess,
+});
 
-// Create the Lambda to execute 
+// Create the Lambda to execute
 const lambda = new aws.lambda.Function("lambdaFunction", {
-    code: new pulumi.asset.AssetArchive({
-        ".": new pulumi.asset.FileArchive("./app"),
-     }),
-     runtime: "nodejs12.x",
-     role: lambdaRole.arn,
-     handler: "index.handler",
+  code: new pulumi.asset.AssetArchive({
+    ".": new pulumi.asset.FileArchive("./app"),
+  }),
+  runtime: "nodejs12.x",
+  role: lambdaRole.arn,
+  handler: "index.handler",
 });
 
 // Give API Gateway permissions to invoke the Lambda
-const lambdaPermission = new aws.lambda.Permission("lambdapermission", {
-    action: "lambda:InvokeFunction",
-    principal: "apigateway.amazonaws.com",
-    function: lambda,
+new aws.lambda.Permission("lambdapermission", {
+  action: "lambda:InvokeFunction",
+  principal: "apigateway.amazonaws.com",
+  function: lambda,
 });
 
 // Set up the API Gateway
 const apigw = new aws.apigatewayv2.Api("httpApiGateway", {
-    protocolType: "HTTP",
-    routeKey: "GET /",
-    target: lambda.invokeArn,
+  protocolType: "HTTP",
+  routeKey: "GET /",
+  target: lambda.invokeArn,
 });
 
 export const endpoint = apigw.apiEndpoint;
