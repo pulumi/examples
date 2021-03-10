1dc1e131f8d739d347d945dbde106e268c4accdf
Anton Tayanovskyy <anton@pulumi.com>
2021-03-10 18:37:42 -0500

    Fix getResourceProperty call

--- classic-azure-ts-aks-helm/index.ts
+++ classic-azure-ts-aks-helm/index.ts
@@ -19,5 +19,5 @@ const apache = new k8s.helm.v3.Chart(
 export let cluster = k8sCluster.name;
 export let kubeConfig = k8sCluster.kubeConfigRaw;
 export let serviceIP = apache
-    .getResourceProperty("v1/Service", "apache-apache", "status")
+    .getResourceProperty("v1/Service", "apache", "status")
     .apply(status => status.loadBalancer.ingress[0].ip);
