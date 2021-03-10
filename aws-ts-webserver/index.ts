aefea1b3f261acd6fe85564b8a7cd8c0ecb5b133
Dave Wrede <dave@pulumi.com>
2021-03-10 08:48:22 -0800

    Updated example to use enum

--- aws-ts-webserver/index.ts
+++ aws-ts-webserver/index.ts
@@ -3,8 +3,6 @@
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
 
-const size = "t2.micro";    // t2.micro is available in the AWS free tier
-
 // Get the id for the latest Amazon Linux AMI
 const ami = aws.ec2.getAmi({
     filters: [
@@ -29,7 +27,7 @@ nohup python -m SimpleHTTPServer 80 &`;
 
 const server = new aws.ec2.Instance("web-server-www", {
     tags: { "Name": "web-server-www" },
-    instanceType: size,
+    instanceType: aws.ec2.InstanceType.T2_Micro, // t2.micro is available in the AWS free tier
     vpcSecurityGroupIds: [ group.id ], // reference the group object above
     ami: ami,
     userData: userData,              // start a simple web server
