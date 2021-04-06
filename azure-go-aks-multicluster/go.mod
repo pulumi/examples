1cdce7caff35fdbb83edacfa09400de1bdf8402f
David Wrede <dave@pulumi.com>
2021-04-06 15:54:21 -0700

    Ported and created Azure Native AKS multi-cluster examples (#959)
    * Ported and created new Azure Native AKS multi-cluster examples
    
    * Apply suggestions from code review
    
    Co-authored-by: Vivek Lakshmanan <vivek@pulumi.com>
    
    * Update READMEs to reflect correct files and update go.sum
    
    Co-authored-by: Vivek Lakshmanan <vivek@pulumi.com>

---
+++ azure-go-aks-multicluster/go.mod
@@ -0,0 +1,9 @@
+module azure-go-aks-multicluster
+
+go 1.16
+
+require (
+	github.com/pulumi/pulumi-azure-native/sdk v0.8.0
+	github.com/pulumi/pulumi-azuread/sdk/v3 v3.5.1
+	github.com/pulumi/pulumi/sdk/v2 v2.24.1
+)
---
+++ azure-py-aks-multicluster/__main__.py
@@ -0,0 +1,82 @@
+# Copyright 2016-2021, Pulumi Corporation.  All rights reserved.
+
+import pulumi
+from pulumi_azure_native import resources, containerservice
+import pulumi_azuread as ad
+
+
+config = pulumi.Config()
+password = config.get_secret("password")
+ssh_public_key = config.require("sshPublicKey")
+location = config.get("location") or "eastus"
+
+resource_group=resources.ResourceGroup(
+    "aks",
+    location=location
+)
+
+ad_app = ad.Application(
+    "aks",
+    display_name="my-aks-multicluster",
+)
+
+ad_sp = ad.ServicePrincipal(
+    "aksSp",
+    application_id=ad_app.application_id
+)
+
+ad_sp_password = ad.ServicePrincipalPassword(
+    "aksSpPassword",
+    service_principal_id=ad_sp.id,
+    value=password,
+    end_date="2099-01-01T00:00:00Z"
+)
+
+aks_cluster_configs = [
+    {
+        "name": "east",
+        "location": "eastus",
+        "node_count": 2,
+        "node_size": containerservice.ContainerServiceVMSizeTypes.STANDARD_D2_V2
+    },
+    {
+        "name": "west",
+        "location": "westus",
+        "node_count": 2,
+        "node_size": containerservice.ContainerServiceVMSizeTypes.STANDARD_D2_V2
+    },
+]
+
+cluster_names = []
+for cluster_config in aks_cluster_configs:
+    cluster = containerservice.ManagedCluster(
+        "aksCluster-{}".format(cluster_config["name"]),
+        resource_group_name=resource_group.name,
+        linux_profile=containerservice.ContainerServiceLinuxProfileArgs(
+            admin_username="aksuser",
+            ssh=containerservice.ContainerServiceSshConfigurationArgs(
+                public_keys=[
+                    containerservice.ContainerServiceSshPublicKeyArgs(
+                        key_data=ssh_public_key,
+                    )
+                ],
+            ),
+        ),
+        service_principal_profile=containerservice.ManagedClusterServicePrincipalProfileArgs(
+            client_id=ad_app.application_id,
+            secret=ad_sp_password.value
+        ),
+        location=cluster_config["location"],
+        agent_pool_profiles=[containerservice.ManagedClusterAgentPoolProfileArgs(
+            name="aksagentpool",
+            mode=containerservice.AgentPoolMode.SYSTEM,
+            count=cluster_config["node_count"],
+            vm_size=cluster_config["node_size"],
+        )],
+        dns_prefix="{}-kube".format(pulumi.get_stack()),
+        kubernetes_version="1.18.14"
+    )
+    cluster_names.append(cluster.name)
+
+pulumi.export("aks_cluster_names", pulumi.Output.all(cluster_names))
+
