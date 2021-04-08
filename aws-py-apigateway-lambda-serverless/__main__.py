cd21369a4009b0658c378b8d07902cfc6c31459c
ajhool <ajhoolachan21@gmail.com>
2021-04-08 19:30:13 -0400

    Lambda-backed REST and HTTP apis

---
+++ aws-py-apigateway-lambda-serverless/__main__.py
@@ -0,0 +1,127 @@
+"""An AWS Python Pulumi program"""
+
+import pulumi
+import pulumi_aws as aws
+import iam
+import json
+
+region = aws.config.region
+
+custom_stage_name = 'example'
+
+##################
+## Lambda Function
+##################
+
+# Create a Lambda function, using code from the `./app` folder.
+
+lambda_func = aws.lambda_.Function("mylambda",
+    role=iam.lambda_role.arn,
+    runtime="python3.7",
+    handler="hello.handler",
+    code=pulumi.AssetArchive({
+        '.': pulumi.FileArchive('./hello_lambda')
+    })
+)
+
+
+####################################################################
+##
+## API Gateway REST API (API Gateway V1 / original)
+##    /{proxy+} - passes all requests through to the lambda function
+##
+####################################################################
+
+# Create a single Swagger spec route handler for a Lambda function.
+def swagger_route_handler(arn):
+    return ({
+        "x-amazon-apigateway-any-method": {
+            "x-amazon-apigateway-integration": {
+                "uri": f'arn:aws:apigateway:{region}:lambda:path/2015-03-31/functions/{arn}/invocations',
+                "passthroughBehavior": "when_no_match",
+                "httpMethod": "POST",
+                "type": "aws_proxy",
+            },
+        },
+    })
+
+# Create the API Gateway Rest API, using a swagger spec.
+rest_api = aws.apigateway.RestApi("api",
+    body=lambda_func.arn.apply(lambda arn: json.dumps({
+        "swagger": "2.0",
+        "info": {"title": "api", "version": "1.0"},
+        "paths": {
+            "/{proxy+}": swagger_route_handler(arn),
+        },
+    })))
+
+# Create a deployment of the Rest API.
+deployment = aws.apigateway.Deployment("api-deployment",
+    rest_api=rest_api.id,
+    # Note: Set to empty to avoid creating an implicit stage, we'll create it
+    # explicitly below instead.
+    stage_name="",
+)
+
+# Create a stage, which is an addressable instance of the Rest API. Set it to point at the latest deployment.
+stage = aws.apigateway.Stage("api-stage",
+    rest_api=rest_api.id,
+    deployment=deployment.id,
+    stage_name=custom_stage_name,
+)
+
+# Give permissions from API Gateway to invoke the Lambda
+invoke_permission = aws.lambda_.Permission("api-lambda-permission",
+    action="lambda:invokeFunction",
+    function=lambda_func.name,
+    principal="apigateway.amazonaws.com",
+    source_arn=deployment.execution_arn.apply(lambda arn: arn + "*/*"),
+)
+
+#########################################################################
+# Create an HTTP API and attach the lambda function to it
+#########################################################################
+
+http_endpoint = aws.apigatewayv2.Api("http-api-pulumi-example",
+    protocol_type="HTTP"
+)
+
+# Note to self: example provided her ew as incorrect: https://www.pulumi.com/docs/reference/pkg/aws/apigatewayv2/integration/
+http_lambda_backend = aws.apigatewayv2.Integration("example",
+    api_id= http_endpoint.id,
+    integration_type="AWS_PROXY",
+    connection_type="INTERNET",
+    description="Lambda example",
+    integration_method="POST",
+    integration_uri=lambda_func.arn,
+    passthrough_behavior="WHEN_NO_MATCH"
+)
+
+url = http_lambda_backend.integration_uri.apply(lambda l: "" + l)
+
+http_route = aws.apigatewayv2.Route("example-route",
+    api_id=http_endpoint.id,
+    route_key="ANY /example-route/{proxy+}",
+    target=http_lambda_backend.id.apply(lambda targetUrl: "integrations/" + targetUrl)
+)
+
+http_stage = aws.apigatewayv2.Stage("example-stage",
+    api_id=http_endpoint.id,
+    route_settings= [
+        {
+            "route_key": http_route.route_key.apply(lambda route_key: "" + route_key)
+        }
+    ],
+    auto_deploy=True
+)
+
+# http_deployment = aws.apigatewayv2.Deployment("example-deployment",
+#     api_id=http_endpoint.id,
+#     description="Example deployment"
+# )
+
+# pulumi.export("http-endpoint", http_endpoint.api_endpoint)
+# Export the https endpoint of the running Rest API
+pulumi.export("apigateway-rest-endpoint", deployment.invoke_url.apply(lambda url: url + custom_stage_name))
+# See "Outputs" for (Inputs and Outputs)[https://www.pulumi.com/docs/intro/concepts/inputs-outputs/] the usage of the pulumi.Output.all function to do string concatenation
+pulumi.export("apigatewayv2-http-endpoint", pulumi.Output.all(http_endpoint.api_endpoint, http_stage.name).apply(lambda values: values[0] + '/' + values[1]))
