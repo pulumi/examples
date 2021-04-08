62a31039e842da6fb07ec0dd565864f4818e132e
ajhool <ajhoolachan21@gmail.com>
2021-04-08 19:30:13 -0400

    Lambda-backed REST and HTTP apis

---
+++ aws-py-apigateway-lambda-serverless/iam.py
@@ -0,0 +1,35 @@
+# Copyright 2016-2018, Pulumi Corporation.  All rights reserved.
+
+from pulumi_aws import iam
+
+lambda_role = iam.Role('lambdaRole',
+    assume_role_policy="""{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "sts:AssumeRole",
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                },
+                "Effect": "Allow",
+                "Sid": ""
+            }
+        ]
+    }"""
+)
+
+lambda_role_policy = iam.RolePolicy('lambdaRolePolicy',
+    role=lambda_role.id,
+    policy="""{
+        "Version": "2012-10-17",
+        "Statement": [{
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "arn:aws:logs:*:*:*"
+        }]
+    }"""
+)
