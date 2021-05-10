3c61f1c48e52b76101cdd6475abf555c9fd2a8ae
stack72 <public@paulstack.co.uk>
2021-05-10 11:42:49 +0100

    Linting aws-ts-apigateway-lambda-serverless

--- aws-ts-apigateway-lambda-serverless/handler.ts
+++ aws-ts-apigateway-lambda-serverless/handler.ts
@@ -1,28 +1,28 @@
-'use strict'
+// Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import * as aws from '@pulumi/aws';
+import * as aws from "@pulumi/aws";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
 /**
  * A simple function that returns the request.
- * 
- * @param {APIGatewayProxyEvent} event - 
- * @returns returns a confirmation to the message to the 
+ *
+ * @param {APIGatewayProxyEvent} event -
+ * @returns returns a confirmation to the message to the
  */
 export const handler: aws.lambda.EventHandler<APIGatewayProxyEvent, APIGatewayProxyResult> = async (event) => {
   const route = event.pathParameters!["route"];
   const body = event.body ? JSON.parse(event.body) : null;
 
-  console.log('Received body: ', body);
+  console.log("Received body: ", body);
 
   return {
     statusCode: 200,
     body: JSON.stringify({
       route,
       affirmation: "Nice job, you've done it! :D",
-      requestBodyEcho: body
-    })
-  }
-}
+      requestBodyEcho: body,
+    }),
+  };
+};
 
 export default handler;
