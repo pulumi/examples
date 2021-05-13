836bb92e91fdbd36e88ec37749101af39a6abc14
Anton Tayanovskyy <anton@pulumi.com>
2021-05-13 17:45:05 -0400

    Min example of pulumi hang

--- aws-py-dynamicresource/mysql_dynamic_provider.py
+++ aws-py-dynamicresource/mysql_dynamic_provider.py
@@ -1,18 +1,23 @@
 # Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-import mysql.connector as connector
-from mysql.connector import errorcode
+# import mysql.connector as connector
+# from mysql.connector import errorcode
 from pulumi import Input, Output, ResourceOptions
 from pulumi.dynamic import *
 from typing import Any, Optional
+import typing
 import binascii
 import os
 
-# A class representing the arguments that the dynamic provider needs. Each argument 
+
+class Creator:
+    pass
+
+# A class representing the arguments that the dynamic provider needs. Each argument
 # will automatically be converted from Input[T] to T before being passed to the
 # functions in the provider
 class SchemaInputs(object):
-    creator_name: Input[str]
+    creator_name: Input[typing.Sequence[Creator]]
     creator_password: Input[str]
     server_address: Input[str]
     database_name: Input[str]
@@ -26,50 +31,57 @@ class SchemaInputs(object):
         self.creation_script = creation_script
         self.deletion_script = deletion_script
 
-# The code for the dynamic provider that gives us our custom resource. It handles 
-# all the create, read, update, and delete operations the resource needs. 
+# The code for the dynamic provider that gives us our custom resource. It handles
+# all the create, read, update, and delete operations the resource needs.
 class SchemaProvider(ResourceProvider):
-    
+
     # The function that is called when a new resource needs to be created
     def create(self, args):
         # A connection is created to the MySQL database, and the script is run
-        connection = connector.connect(user=args["creator_name"], 
-        password=args["creator_password"],
-        host=args["server_address"],
-        database=args["database_name"])
-        cursor = connection.cursor()
-        cursor.execute(args["creation_script"])
+        connection = dict(user=args["creator_name"],
+                          password=args["creator_password"],
+                          host=args["server_address"],
+                          database=args["database_name"])
+        # cursor = connection.cursor()
+        # cursor.execute(args["creation_script"])
+        print('executing creation_script')
         # The creation process is finished. We assign a unique ID to this resource,
         # and return all the outputs required by the resource (in this case
         # outputs are identical to the inputs)
-        return CreateResult("schema-"+binascii.b2a_hex(os.urandom(16)).decode("utf-8"), outs=args)
-    
+
+        outs = {**args}
+        outs['creator_name'] = {'boo': 'hoo'}
+        return CreateResult("schema-"+binascii.b2a_hex(os.urandom(16)).decode("utf-8"), outs=outs)
+
     # The function that is called when an existing resource needs to be deleted
     def delete(self, id, args):
+        print('delete')
         # A connection is created to the MySQL database, and the script is run
-        connection = connector.connect(user=args["creator_name"],
-        password=args["creator_password"],
-        host=args["server_address"],
-        database=args["database_name"])
-        cursor = connection.cursor()
-        cursor.execute(args["deletion_script"])
-    
-    # The function that determines if an existing resource whose inputs were 
+        connection = dict(user=args["creator_name"],
+                          password=args["creator_password"],
+                          host=args["server_address"],
+                          database=args["database_name"])
+        #cursor = connection.cursor()
+        #cursor.execute(args["deletion_script"])
+        print('executing deletion_script')
+
+    # The function that determines if an existing resource whose inputs were
     # modified needs to be updated or entirely replaced
     def diff(self, id, oldInputs, newInputs):
+        print('diff')
         # server_address, database_name, and creation_script are critical inputs
         # that require the resource to be entirely replaced if they are modified.
-        # Changes in other inputs mean the resource can be safely updated without 
+        # Changes in other inputs mean the resource can be safely updated without
         # recreating it
         replaces = []
         if (oldInputs["server_address"] != newInputs["server_address"]): replaces.append("server_address")
         if (oldInputs["database_name"] != newInputs["database_name"]): replaces.append("database_name")
-        if (oldInputs["creation_script"] != newInputs["creation_script"]): replaces.append("creation_script") 
-        
+        if (oldInputs["creation_script"] != newInputs["creation_script"]): replaces.append("creation_script")
+
         return DiffResult(
             # If the old and new inputs don't match, the resource needs to be updated/replaced
             changes=oldInputs != newInputs,
-            # If the replaces[] list is empty, nothing important was changed, and we do not have to 
+            # If the replaces[] list is empty, nothing important was changed, and we do not have to
             # replace the resource
             replaces=replaces,
             # An optional list of inputs that are always constant
@@ -80,13 +92,16 @@ class SchemaProvider(ResourceProvider):
     # The function that updates an existing resource without deleting and
     # recreating it from scratch
     def update(self, id, oldInputs, newInputs):
+        print('update')
         # The old existing inputs are discarded and the new inputs are used
-        return UpdateResult(outs={**newInputs})
+        outs = {**newInputs}
+        outs['creator_name'] = {'boo': 'hoo'}
+        return UpdateResult(outs=outs)
 
 # The main Schema resource that we instantiate in our infrastructure code
 class Schema(Resource):
-    # The inputs used by the dynamic provider are made implicitly availible as outputs 
-    creator_name: Output[str]
+    # The inputs used by the dynamic provider are made implicitly availible as outputs
+    creator_name: Output[typing.Sequence[Creator]]
     creator_password: Output[str]
     server_address: Output[str]
     database_name: Output[str]
