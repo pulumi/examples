f0cb297439b733b2e1a160a6796e626524e7383c
Anton Tayanovskyy <anton@pulumi.com>
2021-07-09 10:43:02 -0400

    Python version

---
+++ py-many-resources/__main__.py
@@ -0,0 +1,12 @@
+import os
+import pulumi
+
+from dummy import Dummy
+
+resource_count = int(os.environ.get('RESOURCE_COUNT', 64))
+resource_payload_bytes = int(os.environ.get('RESOURCE_PAYLOAD_BYTES', 1024))
+
+for i in range(0, resource_count):
+    deadweight = '{:08}'.format(i) * int(resource_payload_bytes / 8)
+    dummy = Dummy(f'dummy-{i}', deadweight=deadweight)
+    pulumi.export(f'output-{i}', dummy.deadweight)
