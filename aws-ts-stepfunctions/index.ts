764d81e5e11c215da2d98e286e0ff633194b70e0
Phillip Edwards <phillip@pulumi.com>
2021-07-13 16:08:50 -0700

    removed deprecated aws.serverless.lambda in favor aws.lambda.CallbackFunction for lambda functions

--- aws-ts-stepfunctions/index.ts
+++ aws-ts-stepfunctions/index.ts
@@ -6,63 +6,80 @@ import * as pulumi from "@pulumi/pulumi";
 const region = aws.config.requireRegion();
 
 const lambdaRole = new aws.iam.Role("lambdaRole", {
-    assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({ Service: "lambda.amazonaws.com" }),
+  assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({ Service: "lambda.amazonaws.com" }),
 });
 
 const lambdaRolePolicy = new aws.iam.RolePolicy("lambdaRolePolicy", {
-    role: lambdaRole.id,
-    policy: {
-        Version: "2012-10-17",
-        Statement: [{
-            Effect: "Allow",
-            Action: [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents",
-            ],
-            Resource: "arn:aws:logs:*:*:*",
-        }],
-    },
+  role: lambdaRole.id,
+  policy: {
+    Version: "2012-10-17",
+    Statement: [{
+      Effect: "Allow",
+      Action: [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+      ],
+      Resource: "arn:aws:logs:*:*:*",
+    }],
+  },
 });
 
 const sfnRole = new aws.iam.Role("sfnRole", {
-    assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({ Service: `states.${region}.amazonaws.com` }),
+  assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({ Service: `states.${region}.amazonaws.com` }),
 });
 
 const sfnRolePolicy = new aws.iam.RolePolicy("sfnRolePolicy", {
-    role: sfnRole.id,
-    policy: {
-        Version: "2012-10-17",
-        Statement: [{
-            Effect: "Allow",
-            Action: [
-                "lambda:InvokeFunction",
-            ],
-            Resource: "*",
-        }],
-    },
+  role: sfnRole.id,
+  policy: {
+    Version: "2012-10-17",
+    Statement: [{
+      Effect: "Allow",
+      Action: [
+        "lambda:InvokeFunction",
+      ],
+      Resource: "*",
+    }],
+  },
 });
 
-const helloFunction = new aws.serverless.Function(
-    "helloFunction",
-    { role: lambdaRole },
-    (event, context, callback) => {
-        callback(null, "Hello");
-    },
-);
+const helloFunction = new aws.lambda.CallbackFunction(
+  "helloFunction", {
+  role: lambdaRole,
+  callback: (event, context, callback) => {
+    callback(null, "Hello");
+  }
+});
 
-const worldFunction = new aws.serverless.Function(
-  "worldFunction",
-  {role: lambdaRole},
-  (event, context, callback) => {
-    callback(null, `${event} World!`);
-  },
-);
+// const helloFunction = new aws.serverless.Function(
+//     "helloFunction",
+//     { role: lambdaRole },
+//     (event, context, callback) => {
+//         callback(null, "Hello");
+//     },
+// );
+
+const worldFunction = new aws.lambda.CallbackFunction(
+  "worldFunction", {
+    role: lambdaRole,
+    callback: (event, context, callback) => {
+      callback(null, `${event} World!`)
+    }
+  }
+)
+
+// const worldFunction = new aws.serverless.Function(
+//   "worldFunction",
+//   { role: lambdaRole },
+//   (event, context, callback) => {
+//     callback(null, `${event} World!`);
+//   },
+// );
 
 const stateMachine = new aws.sfn.StateMachine("stateMachine", {
-    roleArn: sfnRole.arn,
-    definition: pulumi.all([helloFunction.lambda.arn, worldFunction.lambda.arn])
-      .apply(([helloArn, worldArn]) => {
+  roleArn: sfnRole.arn,
+  definition: pulumi.all([helloFunction.arn, worldFunction.arn])
+    .apply(([helloArn, worldArn]) => {
       return JSON.stringify({
         "Comment": "A Hello World example of the Amazon States Language using two AWS Lambda Functions",
         "StartAt": "Hello",
