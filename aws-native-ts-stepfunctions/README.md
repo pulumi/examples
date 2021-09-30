566d79e4e2c26b8f142c82585b3c0af91b8ca047
Levi Blackstone <levi@pulumi.com>
2021-09-30 17:10:15 -0600

    cleanup

--- aws-native-ts-stepfunctions/README.md
+++ aws-native-ts-stepfunctions/README.md
@@ -4,20 +4,90 @@
 
 A basic example that demonstrates using AWS Step Functions with a Lambda function using the AWS Native provider.
 
+Note: Some resources are not yet supported by the Native AWS provider, so we are using both the Native
+and Classic provider in this example. The resources will be updated to use native resources as they are
+available in AWS's Cloud Control API.
+
+## Known error
+
+On the first update, you may see the following error message:
+
 ```
-# Create and configure a new stack
-$ pulumi stack init stepfunctions-dev
-$ export AWS_REGION=us-east-2
-
-# Install dependencies
-$ npm install
-
-# Preview and run the deployment
-$ pulumi up
-
-# Start execution using the AWS CLI (or from the console at https://console.aws.amazon.com/states)
-$ aws stepfunctions start-sync-execution --state-machine-arn $(pulumi stack output stateMachineArn)
-
-# Remove the app
-$ pulumi destroy
+error: operation CREATE failed with "InvalidRequest": The role defined for the function cannot be assumed by Lambda. (Service: Lambda, Status Code: 400, Request ID: c33fdd39-59d4-4ba8-8ad6-29f6c04d79eb, Extended Request ID: null)
 ```
+
+Re-running the update should succeed. This issue is tracked [here](https://github.com/pulumi/pulumi-aws-native/issues/148)
+
+## Deploying and running the program
+
+Note: some values in this example will be different from run to run.  These values are indicated
+with `***`.
+
+1. Create a new stack:
+
+    ```bash
+    $ pulumi stack init dev
+    ```
+
+1. Set the AWS region:
+
+    Either using an environment variable
+    ```bash
+    $ export AWS_REGION=us-west-2
+    ```
+
+    Or with the stack config
+    ```bash
+    $ pulumi config set aws:region us-west-2
+    $ pulumi config set aws-native:region us-west-2
+    ```
+
+1. Restore NPM modules via `npm install` or `yarn install`.
+
+1. Run `pulumi up` to preview and deploy changes.  After the preview is shown you will be
+    prompted if you want to continue or not.
+
+    ```bash
+    $ pulumi up
+    Previewing update (dev)
+    ...
+
+    Updating (dev)
+
+    View Live: https://app.pulumi.com/***/stepfunctions/dev/updates/1
+
+         Type                                      Name               Status
+    +   pulumi:pulumi:Stack                       stepfunctions-dev  created     
+    +   ├─ aws:iam:Role                           sfnRole            created
+    +   ├─ aws:iam:Role                           lambdaRole         created
+    +   ├─ aws:iam:RolePolicy                     sfnRolePolicy      created
+    +   ├─ aws:iam:RolePolicy                     lambdaRolePolicy   created
+    +   ├─ aws-native:lambda:Function             helloFunction      created
+    +   ├─ aws-native:lambda:Function             worldFunction      created
+    +   └─ aws-native:stepfunctions:StateMachine  stateMachine       created
+   
+    Outputs:
+    + stateMachineArn: "arn:aws:states:us-west-2:***:stateMachine:***"
+
+    Resources:
+    + 10 created
+
+    Duration: ***
+    ```
+
+1. To see the resources that were created, run `pulumi stack output`:
+
+    ```bash
+    $ pulumi stack output
+    Current stack outputs (1):
+    OUTPUT           VALUE
+    stateMachineArn  arn:aws:states:us-west-2:***:stateMachine:***
+    ```
+
+1. Start execution using the AWS CLI (or from the console at https://console.aws.amazon.com/states)
+
+    ```bash
+    $ aws stepfunctions start-sync-execution --state-machine-arn $(pulumi stack output stateMachineArn)
+    ```
+
+1.  To clean up resources, run `pulumi destroy` and answer the confirmation question at the prompt.
--- aws-native-ts-stepfunctions/index.ts
+++ aws-native-ts-stepfunctions/index.ts
@@ -7,93 +7,93 @@ import * as pulumi from "@pulumi/pulumi";
 const region = aws.config.requireRegion();
 
 const lambdaRole = new aws.iam.Role("lambdaRole", {
-  assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({ Service: "lambda.amazonaws.com" }),
+    assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({Service: "lambda.amazonaws.com"}),
 });
 
 const lambdaRolePolicy = new aws.iam.RolePolicy("lambdaRolePolicy", {
-  role: lambdaRole.id,
-  policy: {
-    Version: "2012-10-17",
-    Statement: [{
-      Effect: "Allow",
-      Action: [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents",
-      ],
-      Resource: "arn:aws:logs:*:*:*",
-    }],
-  },
+    role: lambdaRole.id,
+    policy: {
+        Version: "2012-10-17",
+        Statement: [{
+            Effect: "Allow",
+            Action: [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents",
+            ],
+            Resource: "arn:aws:logs:*:*:*",
+        }],
+    },
 });
 
 const helloFunction = new awsnative.lambda.Function('helloFunction',
-  {
-    role: lambdaRole.arn,
-    runtime: "nodejs14.x",
-    handler: "index.handler",
-    code: {
-      zipFile: `exports.handler = function(event, context, callback){
+    {
+        role: lambdaRole.arn,
+        runtime: "nodejs14.x",
+        handler: "index.handler",
+        code: {
+            zipFile: `exports.handler = function(event, context, callback){
                                           const response = {"response": "Hello "};
                                           callback(null, response);
                                         };`,
-    },
-  }, { dependsOn: lambdaRolePolicy });
+        },
+    }, {dependsOn: lambdaRolePolicy});
 
 const worldFunction = new awsnative.lambda.Function('worldFunction',
-  {
-    role: lambdaRole.arn,
-    runtime: "nodejs14.x",
-    handler: "index.handler",
-    code: {
-      zipFile: `exports.handler = function(event, context, callback){
+    {
+        role: lambdaRole.arn,
+        runtime: "nodejs14.x",
+        handler: "index.handler",
+        code: {
+            zipFile: `exports.handler = function(event, context, callback){
                                           var response = event.response;
                                           const updated = { "response": response + "World!" };
                                           callback(null, updated);
                                         };`,
-    },
-  }, {dependsOn: lambdaRolePolicy});
+        },
+    }, {dependsOn: lambdaRolePolicy});
 
 
 const sfnRole = new aws.iam.Role("sfnRole", {
-  assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({ Service: `states.${region}.amazonaws.com` }),
+    assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({Service: `states.${region}.amazonaws.com`}),
 });
 
 const sfnRolePolicy = new aws.iam.RolePolicy("sfnRolePolicy", {
-  role: sfnRole.id,
-  policy: {
-    Version: "2012-10-17",
-    Statement: [{
-      Effect: "Allow",
-      Action: [
-        "lambda:InvokeFunction",
-      ],
-      Resource: "*",
-    }],
-  },
+    role: sfnRole.id,
+    policy: {
+        Version: "2012-10-17",
+        Statement: [{
+            Effect: "Allow",
+            Action: [
+                "lambda:InvokeFunction",
+            ],
+            Resource: "*",
+        }],
+    },
 });
 
 const stateMachine = new awsnative.stepfunctions.StateMachine("stateMachine", {
-  roleArn: sfnRole.arn,
-  stateMachineType: 'EXPRESS',
-  definitionString: pulumi.all([helloFunction.arn, worldFunction.arn])
-    .apply(([helloArn, worldArn]) => {
-      return JSON.stringify({
-        "Comment": "A Hello World example of the Amazon States Language using two AWS Lambda Functions",
-        "StartAt": "Hello",
-        "States": {
-          "Hello": {
-            "Type": "Task",
-            "Resource": helloArn,
-            "Next": "World",
-          },
-          "World": {
-            "Type": "Task",
-            "Resource": worldArn,
-            "End": true,
-          },
-        },
-      });
-    }),
-}, { dependsOn: sfnRolePolicy });
+    roleArn: sfnRole.arn,
+    stateMachineType: 'EXPRESS',
+    definitionString: pulumi.all([helloFunction.arn, worldFunction.arn])
+        .apply(([helloArn, worldArn]) => {
+            return JSON.stringify({
+                "Comment": "A Hello World example of the Amazon States Language using two AWS Lambda Functions",
+                "StartAt": "Hello",
+                "States": {
+                    "Hello": {
+                        "Type": "Task",
+                        "Resource": helloArn,
+                        "Next": "World",
+                    },
+                    "World": {
+                        "Type": "Task",
+                        "Resource": worldArn,
+                        "End": true,
+                    },
+                },
+            });
+        }),
+}, {dependsOn: sfnRolePolicy});
 
 export const stateMachineArn = stateMachine.id;
