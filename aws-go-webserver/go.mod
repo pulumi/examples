768b129ffdc4798fdeb001ff3e8ee5f59cfb772d
Anton Tayanovskyy <anton@pulumi.com>
2021-10-07 12:00:10 -0400

    Update aws-go-webserver, todo discuss if this makes sense

--- aws-go-webserver/go.mod
+++ aws-go-webserver/go.mod
@@ -3,6 +3,6 @@ module github.com/pulumi/examples/aws-go-webserver
 go 1.13
 
 require (
-	github.com/pulumi/pulumi-aws/sdk/v4 v4.0.0
-	github.com/pulumi/pulumi/sdk/v3 v3.0.0
+	github.com/pulumi/pulumi-aws/sdk/v4 v4.23.0
+	github.com/pulumi/pulumi/sdk/v3 v3.11.0
 )
--- aws-go-webserver/main.go
+++ aws-go-webserver/main.go
@@ -24,26 +24,23 @@ func main() {
 
 		// Get the ID for the latest Amazon Linux AMI.
 		mostRecent := true
-		ami, err := ec2.LookupAmi(ctx, &ec2.LookupAmiArgs{
-			Filters: []ec2.GetAmiFilter{
-				{
-					Name:   "name",
-					Values: []string{"amzn-ami-hvm-*-x86_64-ebs"},
+		ami := ec2.LookupAmiOutput(ctx, ec2.LookupAmiOutputArgs{ // here
+			Filters: ec2.GetAmiFilterArray{
+				ec2.GetAmiFilterArgs{
+					Name:   pulumi.String("name"),
+					Values: pulumi.StringArray{pulumi.String("amzn-ami-hvm-*-x86_64-ebs")},
 				},
 			},
-			Owners:     []string{"137112412989"},
-			MostRecent: &mostRecent,
+			Owners:     pulumi.StringArray{pulumi.String("137112412989")},
+			MostRecent: pulumi.Bool(mostRecent),
 		})
-		if err != nil {
-			return err
-		}
 
 		// Create a simple web server using the startup script for the instance.
 		srv, err := ec2.NewInstance(ctx, "web-server-www", &ec2.InstanceArgs{
 			Tags:                pulumi.StringMap{"Name": pulumi.String("web-server-www")},
 			InstanceType:        pulumi.String("t2.micro"), // t2.micro is available in the AWS free tier.
 			VpcSecurityGroupIds: pulumi.StringArray{group.ID()},
-			Ami:                 pulumi.String(ami.Id),
+			Ami:                 ami.Id(),
 			UserData: pulumi.String(`#!/bin/bash
 echo "Hello, World!" > index.html
 nohup python -m SimpleHTTPServer 80 &`),
