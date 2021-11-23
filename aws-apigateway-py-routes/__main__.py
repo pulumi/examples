050169138a034bbb12d0639fecac79a3973d0c3c
Daniel Bradley <daniel.dbphoto@gmail.com>
2021-11-23 14:27:17 +0000

    Fix a bunch of python issues

--- aws-apigateway-py-routes/__main__.py
+++ aws-apigateway-py-routes/__main__.py
@@ -1,7 +1,7 @@
 import pulumi
 import pulumi_aws as aws
-import pulumi_apigateway as apigateway
-import lambdas
+import pulumi_aws_apigateway as apigateway
+import lambdas_
 
 # Create a Cognito User Pool of authorized users
 user_pool = aws.cognito.UserPool("user-pool")
@@ -12,30 +12,30 @@ api = apigateway.RestAPI('api', routes=[
     apigateway.RouteArgs(path="static", local_path="www"),
     # Invoke our Lambda to handle a single route
     apigateway.RouteArgs(path="lambda", method="GET",
-                         event_handler=hello_handler),
+                         event_handler=lambdas_.hello_handler),
     # Proxy requests to another service
     apigateway.RouteArgs(path="proxy", target=apigateway.TargetArgs(
         uri="https://www.google.com", type="http_proxy")),
     # Authorize requests using Cognito
     apigateway.RouteArgs(
         path="cognito-authorized",
-        event_handler=hello_handler,
+        event_handler=lambdas_.hello_handler,
         # Define an authorizer which uses Cognito to validate the token from the Authorization header
         authorizers=[apigateway.AuthorizerArgs(
-            parameterName="Authorization",
-            identitySource=["method.request.header.Authorization"],
-            providerARNs=[user_pool.arn]
+            parameter_name="Authorization",
+            identity_source=["method.request.header.Authorization"],
+            provider_arns=[user_pool.arn]
         )]
     ),
     # Authorize requests using a Lambda function
-    apigateway.RouteArgs(path="lambda-authorized", method="GET", event_handler=hello_handler,
+    apigateway.RouteArgs(path="lambda-authorized", method="GET", event_handler=lambdas_.hello_handler,
                          authorizers=[apigateway.AuthorizerArgs(
                              auth_type="custom",
                              parameter_name="Authorization",
                              type="request",
                              identity_source=[
                                  "method.request.header.Authorization"],
-                             handler=auth_lambda
+                             handler=lambdas_.auth_lambda
                          )]),
 ])
 
--- aws-apigateway-py-routes/lambdas_.py
+++ aws-apigateway-py-routes/lambdas_.py
@@ -1,7 +1,7 @@
 import pulumi
 import pulumi_aws as aws
 
-auth_lambda_role = aws.iam.Role('auth-lambda-role',
+lambda_role = aws.iam.Role('auth-lambda-role',
                        assume_role_policy="""{
         "Version": "2012-10-17",
         "Statement": [
@@ -17,8 +17,8 @@ auth_lambda_role = aws.iam.Role('auth-lambda-role',
     }"""
                        )
 
-auth_lambda_role_policy = aws.iam.RolePolicy('auth-lambda-role-policy',
-                                    role=auth_lambda_role.id,
+lambda_role_policy = aws.iam.RolePolicy('auth-lambda-role-policy',
+                                    role=lambda_role.id,
                                     policy="""{
         "Version": "2012-10-17",
         "Statement": [{
@@ -35,7 +35,7 @@ auth_lambda_role_policy = aws.iam.RolePolicy('auth-lambda-role-policy',
 
 # Create a Lambda function to validate request authorization
 auth_lambda = aws.lambda_.Function("auth-lambda",
-                                  role=auth_lambda_role.arn,
+                                  role=lambda_role.arn,
                                   runtime=aws.lambda_.Runtime.PYTHON3D8,
                                   code=pulumi.AssetArchive({
                                       ".": pulumi.FileArchive("./authorizer"),
@@ -45,6 +45,7 @@ auth_lambda = aws.lambda_.Function("auth-lambda",
 
 # Create a Lambda function to respond to HTTP requests
 hello_handler = aws.lambda_.Function("hello-handler",
+                                    role=lambda_role,
                                     runtime=aws.lambda_.Runtime.PYTHON3D8,
                                     code=pulumi.AssetArchive({
                                         ".": pulumi.FileArchive("./handler"),
