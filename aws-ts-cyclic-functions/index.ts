cd6add66dfd315891e37cc1f702a0631135e2d79
Daniel Bradley <daniel@pulumi.com>
2021-12-15 10:51:00 +0000

    Use azure-native

--- aws-ts-cyclic-functions/index.ts
+++ aws-ts-cyclic-functions/index.ts
@@ -1,101 +1,15 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
-import * as aws from "@pulumi/aws";
-import * as awsnative from "@pulumi/aws-native";
+import * as resources from "@pulumi/azure-native/resources";
 
-// const handlerFactory = () => {
-//   const lambda = new aws.sdk.Lambda();
-//   return (ev: unknown): void => {
-//     const opponentFunction = process.env.OPPONENT_FN_NAME;
-//     if (!opponentFunction) {
-//       return console.log("No one to play against");
-//     }
-//     if (Math.random() > 0.5) {
-//       lambda.invoke({
-//         FunctionName: opponentFunction,
-//         InvocationType: "Event",
-//       });
-//       console.log(`Returned to ${opponentFunction}`);
-//     } else {
-//       console.log(`Missed! ${opponentFunction} wins!`);
-//     }
-//   };
-// };
+const initialA = new resources.ResourceGroup(
+  "a",
+  {},
+  { ignoreChanges: ["tags"] }
+);
 
-const role = new aws.iam.Role("lambda-role", {
-  assumeRolePolicy: {
-    Version: "2012-10-17",
-    Statement: [
-      {
-        Sid: "AllowAssumeRole",
-        Effect: "Allow",
-        Principal: {
-          Service: "lambda.amazonaws.com",
-        },
-        Action: "sts:AssumeRole",
-      },
-    ],
-  },
-  inlinePolicies: [
-    {
-      policy: JSON.stringify({
-        Statement: [
-          {
-            Action: [
-              "logs:CreateLogGroup",
-              "logs:CreateLogStream",
-              "logs:PutLogEvents",
-            ],
-            Effect: "Allow",
-            Resource: "arn:aws:logs:*:*:*",
-          },
-        ],
-        Version: "2012-10-17",
-      }),
-    },
-  ],
-});
+const b = new resources.ResourceGroup("b", { tags: { a: initialA.name } });
+const finalA = new resources.ResourceGroup("a", { tags: { b: b.name } });
 
-const functionArgs = {
-  role: role.arn,
-  runtime: "nodejs14.x",
-  handler: "index.handler",
-  code: {
-      zipFile: `
-      exports.handler = function(event, context, callback){
-        callback(null, "TODO");
-      };`,
-    },
-};
-
-const initialPing = new awsnative.lambda.Function("ping", {
-  ...functionArgs,
-  functionName: "ping-name",
-  environment: {
-    variables: {
-      OPPONENT_FN_NAME: "",
-    },
-  },
-}, { ignoreChanges: ["environment"] } );
-
-const pong = new awsnative.lambda.Function("pong", {
-  ...functionArgs,
-  environment: {
-    variables: {
-      OPPONENT_FN_NAME: initialPing.functionName,
-    },
-  },
-});
-
-const ping = new awsnative.lambda.Function("ping", {
-  ...functionArgs,
-  functionName: "ping-name",
-  environment: {
-    variables: {
-      OPPONENT_FN_NAME: pong.functionName,
-    },
-  },
-});
-
-// Export the name of the function
-export const pongName = pong.functionName;
+export const aTags = finalA.tags;
+export const bTags = b.tags;
