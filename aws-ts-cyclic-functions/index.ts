2311fca52a3671de729ff19cf59ea4f987a964aa
Daniel Bradley <daniel@pulumi.com>
2021-12-15 14:51:40 +0000

    Add parallel AWS and Azure setup

--- aws-ts-cyclic-functions/index.ts
+++ aws-ts-cyclic-functions/index.ts
@@ -1,15 +1,30 @@
 // Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
 
+import * as awsnative from "@pulumi/aws-native";
 import * as resources from "@pulumi/azure-native/resources";
 
-const initialA = new resources.ResourceGroup(
+const rgInitialA = new resources.ResourceGroup(
   "a",
   {},
   { ignoreChanges: ["tags"] }
 );
 
-const b = new resources.ResourceGroup("b", { tags: { a: initialA.name } });
-const finalA = new resources.ResourceGroup("a", { tags: { b: b.name } });
+const rgB = new resources.ResourceGroup("b", { tags: { a: rgInitialA.name } });
+const rgFinalA = new resources.ResourceGroup("a", { tags: { b: rgB.name } });
 
-export const aTags = finalA.tags;
-export const bTags = b.tags;
+const s3initialA = new awsnative.s3.Bucket(
+  "a",
+  {},
+  { ignoreChanges: ["tags"] }
+);
+const s3b = new awsnative.s3.Bucket("b", {
+  tags: [{ key: "a", value: s3initialA.bucketName.apply((n) => n ?? "") }],
+});
+const s3finalA = new awsnative.s3.Bucket("a", {
+  tags: [{ key: "b", value: s3b.bucketName.apply((n) => n ?? "") }],
+});
+
+export const rgATags = rgFinalA.tags;
+export const rgBTags = rgB.tags;
+export const s3aTags = s3finalA.tags;
+export const s3bTags = s3b.tags;
