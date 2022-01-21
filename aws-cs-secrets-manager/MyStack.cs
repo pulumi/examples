f07a6d70968a6bcf7a0caf9afc31d004c03ab389
Piers Karsenbarg <piers@pulumi.com>
2022-01-21 10:46:13 +0000

    Added csharp version of secrets manager example

---
+++ aws-cs-secrets-manager/MyStack.cs
@@ -0,0 +1,23 @@
+using Pulumi;
+using Pulumi.Aws.SecretsManager;
+
+class MyStack : Stack
+{
+    public MyStack()
+    {
+        // Create secret
+        var secret = new Secret("secret");
+
+        // Create secret version
+        var secretVersion = new SecretVersion("secretVersion", new SecretVersionArgs
+        {
+            SecretId = secret.Id,
+            SecretString = "mysecret"
+        });
+
+        this.SecretId = secret.Id;
+    }
+
+    [Output]
+    public Output<string> SecretId { get; set; }
+}
