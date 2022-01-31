c8850c2b77247fb4c534f5ba4cd62a5f3ce824c5
Piers Karsenbarg <piers@pulumi.com>
2022-01-31 15:30:52 +0000

    Updated policy example to use azure native

--- policy-packs/azure-ts/index.ts
+++ policy-packs/azure-ts/index.ts
@@ -1,6 +1,6 @@
 // Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
-import * as azure from "@pulumi/azure";
+import * as azure from "@pulumi/azure-native";
 import { PolicyPack, validateResourceOfType } from "@pulumi/policy";
 
 const policies = new PolicyPack("azure", {
@@ -10,7 +10,7 @@ const policies = new PolicyPack("azure", {
             description: "Associating public IP addresses is discouraged.",
             enforcementLevel: "advisory",
             validateResource: validateResourceOfType(azure.network.NetworkInterface, (ni, args, reportViolation) => {
-                if (ni.ipConfigurations.some(cfg => cfg.publicIpAddressId)) {
+                if (ni.ipConfigurations.some(cfg => cfg.publicIPAddress.id)) {
                     reportViolation("Associating public IP addresses is discouraged.");
                 }
             }),
@@ -19,19 +19,19 @@ const policies = new PolicyPack("azure", {
             name: "prohibited-public-internet",
             description: "Inbound rules with public internet access are prohibited.",
             enforcementLevel: "mandatory",
-            validateResource: validateResourceOfType(azure.network.NetworkSecurityRule, (securityRule, args, reportViolation) => {
+            validateResource: validateResourceOfType(azure.network.SecurityRule, (securityRule, args, reportViolation) => {
                 if (securityRule.sourceAddressPrefix === "*") {
                     reportViolation("Inbound rules with public internet access are prohibited.");
                 }
             }),
         },
         {
-            name: "prohibited-iot",
-            description: "Use of IOT services is prohibited.",
+            name: "prohibited-powerbi",
+            description: "Use of PowerBI services is prohibited.",
             enforcementLevel: "mandatory",
             validateResource: (args, reportViolation) => {
-                if (args.type.startsWith("azure:iot")) {
-                    reportViolation("Use of IOT services is prohibited.");
+                if (args.type.startsWith("azure-native:powerbi")) {
+                    reportViolation("Use of PowerBI services is prohibited.");
                 }
             },
         },
