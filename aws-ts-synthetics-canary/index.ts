61ad2c8026fba084498195ce3fe9cc82a1cad0ce
Mitch Gerdisch <mitch@pulumi.com>
2022-04-22 09:28:35 -0500

    Mitchgerdisch/canary add bucketversioning (#1207)
    * working on stuff
    
    * working version
    
    * fix linting errors

--- aws-ts-synthetics-canary/index.ts
+++ aws-ts-synthetics-canary/index.ts
@@ -7,9 +7,7 @@ import { generateCanaryPolicy } from "./canaryPolicy";
 // Used for naming convention
 const baseName = "canary";
 
-// Bucket for storing canary scripts
-const canaryScriptsBucket = new aws.s3.BucketV2(`${baseName}-scripts`);
-// Bucket for storing canary results
+// Bucket for storing canary RESULTS
 const canaryResultsS3Bucket = new aws.s3.BucketV2(`${baseName}-results`, {
   // This allows the bucket to be destroyed even if it contains canary results.
   forceDestroy: true,
@@ -39,24 +37,36 @@ const canaryExecutionPolicy = new aws.iam.RolePolicy(`${baseName}-exec-policy`, 
   policy: canaryResultsS3Bucket.arn.apply(arn => generateCanaryPolicy(arn)),
 });
 
-// zip up, upload and deploy the "simple canary"
-const simpleCanaryScriptArchive = new pulumi.asset.FileArchive("./canaries/simple-canary/");
-const simpleCanaryScriptObject = new aws.s3.BucketObjectv2(`${baseName}-simple-canary`, {
+// Bucket for storing the canary SCRIPTS
+const canaryScriptsBucket = new aws.s3.BucketV2(`${baseName}-scripts`);
+// Enable versioning so that as new scripts are uploaded the canary will be updated as well.
+const canaryScriptBucketVersioning = new aws.s3.BucketVersioningV2(`${baseName}-scripts-versioning`, {
   bucket: canaryScriptsBucket.id,
-  source: simpleCanaryScriptArchive,
+  versioningConfiguration: {
+    status: "Enabled",
+  },
 });
-const simpleCanary = new aws.synthetics.Canary(`${baseName}-simple`, {
-    artifactS3Location: pulumi.interpolate`s3://${canaryResultsS3Bucket.id}`,
-    executionRoleArn: canaryExecutionRole.arn,
-    handler: "exports.handler",
-    runtimeVersion: "syn-nodejs-puppeteer-3.5",
-    schedule: {
-        expression: "rate(1 minute)",
-    },
-    s3Bucket: canaryScriptsBucket.id,
-    s3Key: simpleCanaryScriptObject.id,
-    startCanary: true,
+// zip up, upload and deploy the "simple canary"
+const canaryScriptArchive = new pulumi.asset.FileArchive("./canaries/simple-canary/");
+const canaryScriptObject = new aws.s3.BucketObjectv2(`${baseName}-script`, {
+  bucket: canaryScriptBucketVersioning.bucket,
+  source: canaryScriptArchive,
 });
 
-export const canaryName = simpleCanary.name;
-export const canaryNameArn = simpleCanary.arn;
+// Create the canary.
+const canary = new aws.synthetics.Canary(`${baseName}-simple`, {
+  artifactS3Location: pulumi.interpolate`s3://${canaryResultsS3Bucket.id}`,
+  executionRoleArn: canaryExecutionRole.arn,
+  handler: "exports.handler",
+  runtimeVersion: "syn-nodejs-puppeteer-3.5",
+  schedule: {
+      expression: "rate(1 minute)",
+  },
+  s3Bucket: canaryScriptsBucket.id,
+  s3Key: canaryScriptObject.id,
+  s3Version: canaryScriptObject.versionId,
+  startCanary: true,
+});
+
+export const simpleCanaryName = canary.name;
+export const simpleCanaryArn = canary.arn;
