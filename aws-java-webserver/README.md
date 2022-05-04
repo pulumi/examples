25ed997fb6593e1d228f5a94f4a1c7767ff93151
Anton Tayanovskyy <anton@pulumi.com>
2022-05-04 10:29:21 -0400

    Add aws-java-webserver example

---
+++ aws-java-webserver/README.md
@@ -0,0 +1,60 @@
+# Web Server Using Amazon EC2
+
+This example deploys a simple AWS EC2 virtual machine running a Python web server.
+
+## Deploying the App
+
+To deploy your infrastructure, follow the below steps.
+
+### Prerequisites
+
+1. [Install Pulumi](https://www.pulumi.com/docs/get-started/install/)
+2. [Configure AWS Credentials](https://www.pulumi.com/docs/intro/cloud-providers/aws/setup/)
+
+### Steps
+
+After cloning this repo, from this working directory, run these commands:
+
+1. Create a new stack, which is an isolated deployment target for this example:
+
+    ```bash
+    $ pulumi stack init
+    ```
+
+2. Set the required configuration variables for this program:
+
+    ```bash
+    $ pulumi config set aws:region us-east-1
+    ```
+
+3. Stand up the VM, which will also boot up your Python web server on port 80:
+
+    ```bash
+    $ pulumi up
+    ```
+
+4. After a couple minutes, your VM will be ready, and two stack outputs are printed:
+
+    ```bash
+    $ pulumi stack output
+    Current stack outputs (2):
+    OUTPUT          VALUE
+    publicHostName  ec2-53-40-227-82.compute-1.amazonaws.com
+    publicIp        53.40.227.82
+    ```
+
+5. Thanks to the security group making port 80 accessible to the 0.0.0.0/0 CIDR block (all addresses), we can curl it:
+
+    ```bash
+    $ curl $(pulumi stack output publicIp)
+    Hello, World!
+    ```
+
+6. From there, feel free to experiment. Simply making edits and running `pulumi up` will incrementally update your VM.
+
+7. Afterwards, destroy your stack and remove it:
+
+    ```bash
+    $ pulumi destroy --yes
+    $ pulumi stack rm --yes
+    ```
---
+++ aws-java-webserver/app/src/main/java/webserver/App.java
@@ -0,0 +1,65 @@
+package webserver;
+
+import com.pulumi.Context;
+import com.pulumi.Pulumi;
+import com.pulumi.aws.ec2.Ec2Functions;
+import com.pulumi.aws.ec2.Instance;
+import com.pulumi.aws.ec2.InstanceArgs;
+import com.pulumi.aws.ec2.SecurityGroup;
+import com.pulumi.aws.ec2.SecurityGroupArgs;
+import com.pulumi.aws.ec2.inputs.GetAmiArgs;
+import com.pulumi.aws.ec2.inputs.GetAmiFilter;
+import com.pulumi.aws.ec2.inputs.SecurityGroupIngressArgs;
+import com.pulumi.aws.ec2.outputs.GetAmiResult;
+import com.pulumi.core.Output;
+
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) {
+        Pulumi.run(App::stack);
+    }
+
+    public static void stack(Context ctx) {
+        final var ami = Ec2Functions.getAmi(GetAmiArgs.builder()
+                .filters(GetAmiFilter.builder()
+                        .name("name")
+                        .values("amzn-ami-hvm-*-x86_64-ebs")
+                        .build())
+                .owners("137112412989")
+                .mostRecent(true)
+                .build()
+        ).thenApply(GetAmiResult::id);
+
+        final var group = new SecurityGroup("web-secgrp", SecurityGroupArgs.builder()
+                .ingress(SecurityGroupIngressArgs.builder()
+                        .protocol("tcp")
+                        .fromPort(80)
+                        .toPort(80)
+                        .cidrBlocks("0.0.0.0/0")
+                        .build())
+                .build()
+        );
+
+        // (optional) create a simple web server using the startup
+        // script for the instance
+
+        final var userData =
+                "#!/bin/bash\n" +
+                        "echo \"Hello, World!\" > index.html\n" +
+                        "nohup python -m SimpleHTTPServer 80 &";
+
+        final var server = new Instance("web-server-www", InstanceArgs.builder()
+                .tags(Map.of("Name", "web-server-www"))
+                .instanceType(Output.ofRight(com.pulumi.aws.ec2.enums.InstanceType.T2_Micro))
+                .vpcSecurityGroupIds(group.getId().applyValue(List::of))
+                .ami(Output.of(ami))
+                .userData(userData)
+                .build()
+        );
+
+        ctx.export("publicIp", server.publicIp());
+        ctx.export("publicHostName", server.publicDns());
+    }
+}
