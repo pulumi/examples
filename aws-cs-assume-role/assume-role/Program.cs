46bce722981987308a7bd031e1fc227d23a79d7c
Zaid Ajaj <zaid.naom@gmail.com>
2022-06-13 20:23:36 +0200

    Update aws-cs-assume-role: add solution, use JSON nodes to define policies, remove redundant classes

--- aws-cs-assume-role/assume-role/Program.cs
+++ aws-cs-assume-role/assume-role/Program.cs
@@ -1,7 +1,36 @@
-﻿using System.Threading.Tasks;
+﻿// Copyright 2016-2022, Pulumi Corporation.
+using System.Collections.Generic;
 using Pulumi;
+using Pulumi.Aws;
+using Pulumi.Aws.Inputs;
+using Pulumi.Aws.S3;
 
-class Program
+await Deployment.RunAsync(() =>
 {
-    static Task<int> Main() => Deployment.RunAsync<AssumeRoleStack>();
-}
+    var awsConfig = new Pulumi.Config("aws");
+    var config = new Pulumi.Config();
+    var roleToAssumeArn = config.Require("roleToAssumeARN");
+    var provider = new Provider("privileged", new ProviderArgs
+    {
+        Region = awsConfig.Require("region"),
+        AssumeRole = new ProviderAssumeRoleArgs
+        {
+            RoleArn = roleToAssumeArn,
+            SessionName = "PulumiSession",
+            ExternalId = "PulumiApplication"
+        }
+    });
+
+    var emptyBucketArgs = new BucketArgs();
+    
+    var bucket = new Bucket(
+        name: "myBucket",  
+        args: emptyBucketArgs, 
+        options: new CustomResourceOptions { Provider = provider }
+    );
+
+    return new Dictionary<string, object?>
+    {
+        ["bucketName"] = bucket.BucketName
+    };
+});
