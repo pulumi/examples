4be43116f64d60f02b046a13b138cd19fa6b9c82
Harry Liuson <hliuson@Hotmail.com>
2022-06-15 10:01:24 -0700

    fixed linting

--- azure-ts-containerapps/index.ts
+++ azure-ts-containerapps/index.ts
@@ -3,10 +3,10 @@
 import * as docker from "@pulumi/docker";
 import * as pulumi from "@pulumi/pulumi";
 
+import * as app from "@pulumi/azure-native/app";
 import * as containerregistry from "@pulumi/azure-native/containerregistry";
 import * as operationalinsights from "@pulumi/azure-native/operationalinsights";
 import * as resources from "@pulumi/azure-native/resources";
-import * as app from "@pulumi/azure-native/app";
 
 const resourceGroup = new resources.ResourceGroup("rg");
 
