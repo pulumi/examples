1cf4546bcd54275169cfd0a3431221903d75648a
Myles Haynes <myles@pulumi.com>
2022-06-17 11:37:31 -0700

    render readme to string instead of byte array

--- stack-readme-ts/index.ts
+++ stack-readme-ts/index.ts
@@ -3,4 +3,4 @@ import { readFileSync } from "fs";
 export const strVar = "foo";
 export const arrVar = ["fizz", "buzz"];
 // add readme to stack outputs
-export const readme = readFileSync("./Pulumi.README.md");
+export const readme = readFileSync("./Pulumi.README.md").toString();
