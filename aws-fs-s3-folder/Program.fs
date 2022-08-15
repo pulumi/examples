ae4d6080779a802edbb44f3df18f748b8b6d72bc
Zaid Ajaj <zaid.naom@gmail.com>
2022-08-15 22:24:26 +0200

    update and simplify aws-fs-s3-folder to make it more readable

--- aws-fs-s3-folder/Program.fs
+++ aws-fs-s3-folder/Program.fs
@@ -5,35 +5,33 @@ open Pulumi
 open Pulumi.FSharp
 open Pulumi.Aws.S3.Inputs
 open Pulumi.Aws.S3
+open MimeTypes
 
 let infra () =
-
     // Create an AWS resource (S3 Bucket)
-    let bucket =
-        Bucket("my-bucket",
-               BucketArgs (Website = input (BucketWebsiteArgs (IndexDocument = input "index.html"))))
-        
-    // For each file in wwwroot ...
+    let bucket = Bucket("my-bucket", BucketArgs (Website = BucketWebsiteArgs (IndexDocument =  "index.html")))
+    // For each file in wwwroot, create a bucket object
+    let bucketObjects = ResizeArray()
     let files = Directory.GetFiles "wwwroot"
-    let bucketObjects =
-        files
-        |> Array.map(fun file ->
-            let name = file.Substring 8
-            let contentType = if name.EndsWith ".png" then "image/png" else "text/html" 
-    
-            // ... create a bucket object
-            BucketObject(name,
-                         BucketObjectArgs
-                            (Acl = input "public-read",
-                             Bucket = io bucket.BucketName,
-                             ContentType = input contentType,
-                             Source = input (FileAsset file :> AssetOrArchive)),
-                         CustomResourceOptions (Parent = bucket)))
-    
+    for file in files do
+        let fileName = Path.GetFileName file
+        let extension = Path.GetExtension file
+        let contentType = MimeTypeMap.GetMimeType extension
+
+        let bucketArgs = BucketObjectArgs()
+        bucketArgs.Acl <- "public-read"
+        bucketArgs.Bucket <- bucket.BucketName
+        bucketArgs.ContentType <- contentType
+        bucketArgs.Source <- input (FileAsset file :> AssetOrArchive)
+
+        let resourceOptions = CustomResourceOptions (Parent = bucket)
+        // create a bucket object
+        let bucketObject = BucketObject(fileName, bucketArgs, resourceOptions)
+        bucketObjects.Add bucketObject
+
     // Export the name of the bucket
     let endpoint = bucket.WebsiteEndpoint.Apply (sprintf "http://%s")
     dict [("endpoint", endpoint :> obj)]
 
 [<EntryPoint>]
-let main _ =
-  Deployment.run infra
+let main (args: string[]) = Deployment.run infra
