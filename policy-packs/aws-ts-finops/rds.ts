7219f945aeb40f038b4f1f430f8f02fdb6281e71
Richard Shade <richard@pulumi.com>
2022-09-06 13:57:02 -0500

    Cleanup, and 4 spaces

--- policy-packs/aws-ts-finops/rds.ts
+++ policy-packs/aws-ts-finops/rds.ts
@@ -1,115 +1,109 @@
+// Copyright 2016-2022, Pulumi Corporation.  All rights reserved.
+
 import * as aws from "@pulumi/aws";
 import {
-  ReportViolation,
-  ResourceValidationArgs,
-  ResourceValidationPolicy,
-  StackValidationPolicy,
-  EnforcementLevel,
-  validateResourceOfType,
+    ReportViolation,
+    ResourceValidationArgs,
+    ResourceValidationPolicy,
+    StackValidationPolicy,
+    EnforcementLevel,
+    validateResourceOfType,
 } from "@pulumi/policy";
 import * as utils from "./utils";
 
-//Burstable
 export function requireRdsInstanceType(
-  name: string,
-  instanceTypes: aws.rds.InstanceType | Iterable<aws.rds.InstanceType>,
-  enforcementLevel: EnforcementLevel = "advisory"
+    name: string,
+    instanceTypes: aws.rds.InstanceType | Iterable<aws.rds.InstanceType>,
+    enforcementLevel: EnforcementLevel = "advisory"
 ): ResourceValidationPolicy {
-  const types = utils.toStringSet(instanceTypes);
+    const types = utils.toStringSet(instanceTypes);
 
-  return {
-    name: name,
-    description: "RDS instances should use approved instance types.",
-    enforcementLevel: enforcementLevel,
-    validateResource: [
-      validateResourceOfType(
-        aws.rds.Instance,
-        (instance, args, reportViolation) => {
-          if (
-            instance.instanceClass !== undefined &&
-            !types.has(instance.instanceClass)
-          ) {
-            reportViolation(
-              "RDS Instance should use the approved instance types."
-            );
-          }
-        }
-      ),
-      validateResourceOfType(
-        aws.rds.ClusterInstance,
-        (ci, args, reportViolation) => {
-          if (!types.has(ci.instanceClass)) {
-            reportViolation(
-              "ClusterInstance should use the approved instance types."
-            );
-          }
-        }
-      ),
-      //   validateResourceOfType(
-      //     aws.rds.Cluster,
-      //     (cluster, args, reportViolation) => {
-      //       if (!cluster.dbClusterInstanceClass || !types.has(lt.instanceType)) {
-      //         reportViolation(
-      //           "EC2 LaunchTemplate should use the approved instance types."
-      //         );
-      //       }
-      //     }
-      //   ),
-    ],
-  };
+    return {
+        name: name,
+        description: "RDS instances should use approved instance types.",
+        enforcementLevel: enforcementLevel,
+        validateResource: [
+            validateResourceOfType(
+                aws.rds.Instance,
+                (instance, args, reportViolation) => {
+                    if (
+                        instance.instanceClass !== undefined &&
+                        !types.has(instance.instanceClass)
+                    ) {
+                        reportViolation(
+                            "RDS Instance should use the approved instance types."
+                        );
+                    }
+                }
+            ),
+            validateResourceOfType(
+                aws.rds.ClusterInstance,
+                (ci, args, reportViolation) => {
+                    if (!types.has(ci.instanceClass)) {
+                        reportViolation(
+                            "ClusterInstance should use the approved instance types."
+                        );
+                    }
+                }
+            ),
+        ],
+    };
 }
 
-//GP3
 export function requireRdsVolumesGp2(
-  name: string,
-  enforcementLevel: EnforcementLevel
+    name: string,
+    enforcementLevel: EnforcementLevel
 ): ResourceValidationPolicy {
-  return {
-    name: name,
-    description: "RDS StorageType Should be gp2",
-    enforcementLevel: enforcementLevel,
-    validateResource: [
-      validateResourceOfType(
-        aws.rds.Instance,
-        (instance, args, reportViolation) => {
-          if (
-            instance.storageType !== undefined &&
-            instance.storageType != "gp2"
-          ) {
-            reportViolation("RDS Instance should use gp2 storage type");
-          }
-        }
-      ),
-    ],
-  };
+    return {
+        name: name,
+        description: "RDS StorageType Should be gp2",
+        enforcementLevel: enforcementLevel,
+        validateResource: [
+            validateResourceOfType(
+                aws.rds.Instance,
+                (instance, args, reportViolation) => {
+                    if (
+                        instance.storageType !== undefined &&
+                        instance.storageType != "gp2"
+                    ) {
+                        reportViolation(
+                            "RDS Instance should use gp2 storage type"
+                        );
+                    }
+                }
+            ),
+        ],
+    };
 }
 
 export function requireRdsLicenseModel(
-  name: string,
-  licenseModel: Iterable<
-    "license-included" | "bring-your-own-license" | "general-public-license"
-  >,
-  enforcementLevel: EnforcementLevel
+    name: string,
+    licenseModel: Iterable<
+        "license-included" | "bring-your-own-license" | "general-public-license"
+    >,
+    enforcementLevel: EnforcementLevel
 ): ResourceValidationPolicy {
-  const types = utils.toStringSet(licenseModel);
-  return {
-    name: name,
-    description: `RDS license type should be ${licenseModel}`,
-    enforcementLevel: enforcementLevel,
-    validateResource: [
-      validateResourceOfType(
-        aws.rds.Instance,
-        (instance, args, reportViolation) => {
-          if (
-            instance.licenseModel == undefined ||
-            (instance.licenseModel !== undefined &&
-              !types.has(instance.licenseModel))
-          ) {
-            reportViolation(`RDS license type should be ${licenseModel}`);
-          }
-        }
-      ),
-    ],
-  };
+    const types = utils.toStringSet(licenseModel);
+    return {
+        name: name,
+        description: `RDS license type should be ${licenseModel}`,
+        enforcementLevel: enforcementLevel,
+        validateResource: [
+            validateResourceOfType(
+                aws.rds.Instance,
+                (instance, args, reportViolation) => {
+                    if (
+                        instance.licenseModel == undefined ||
+                        (instance.licenseModel !== undefined &&
+                            !types.has(instance.licenseModel))
+                    ) {
+                        reportViolation(
+                            `RDS license type should be ${licenseModel}`
+                        );
+                    }
+                }
+            ),
+        ],
+    };
 }
 //Volume Size -- future
--- policy-packs/aws-ts-finops/vpc.ts
+++ policy-packs/aws-ts-finops/vpc.ts
@@ -1,36 +1,38 @@
+// Copyright 2016-2022, Pulumi Corporation.  All rights reserved.
+
 import * as aws from "@pulumi/aws";
 import {
-  ReportViolation,
-  ResourceValidationArgs,
-  ResourceValidationPolicy,
-  StackValidationPolicy,
-  EnforcementLevel,
-  validateResourceOfType,
+    ReportViolation,
+    ResourceValidationArgs,
+    ResourceValidationPolicy,
+    StackValidationPolicy,
+    EnforcementLevel,
+    validateResourceOfType,
 } from "@pulumi/policy";
 import * as utils from "./utils";
 
 export function requireSingleNatGateway(
-  name: string,
-  enforcementLevel: EnforcementLevel
+    name: string,
+    enforcementLevel: EnforcementLevel
 ): StackValidationPolicy {
-  return {
-    name: name,
-    description: "There should be a max of one Nat Gateway",
-    enforcementLevel: enforcementLevel,
-    validateStack: (stack, reportViolation) => {
-      let counter = 0;
-      for (const resource of stack.resources) {
-        if (resource.type != "aws:ec2/natGateway:NatGateway") {
-          return;
-        } else {
-          counter = counter + 1;
-        }
-      }
-      if (counter != 0) {
-        reportViolation("Stack can only have one Nat Gateway");
-      }
-    },
-  };
+    return {
+        name: name,
+        description: "There should be a max of one Nat Gateway",
+        enforcementLevel: enforcementLevel,
+        validateStack: (stack, reportViolation) => {
+            let counter = 0;
+            for (const resource of stack.resources) {
+                if (resource.type != "aws:ec2/natGateway:NatGateway") {
+                    return;
+                } else {
+                    counter = counter + 1;
+                }
+            }
+            if (counter != 0) {
+                reportViolation("Stack can only have one Nat Gateway");
+            }
+        },
+    };
 }
 
 // Todo
