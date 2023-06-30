932f82ee629a2e61f60c3463669bb2e964c54315
MitchellGerdisch <mitch@pulumi.com>
2023-06-30 09:40:54 -0500

    Address some errors related to old packages

--- aws-ts-eks-hello-world/index.ts
+++ aws-ts-eks-hello-world/index.ts
@@ -11,13 +11,14 @@ const name = "helloworld";
 const vpc = new awsx.ec2.Vpc("vpc", { numberOfAvailabilityZones: 2 });
 
 const cluster = new eks.Cluster(name, {
-    vpcId: vpc.id,
+    vpcId: vpc.vpcId,
     subnetIds: vpc.publicSubnetIds,
     desiredCapacity: 2,
     minSize: 1,
     maxSize: 2,
     storageClasses: "gp2",
     deployDashboard: false,
+    
 });
 
 // Export the clusters' kubeconfig.
