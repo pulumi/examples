a6871fe455407dc65c978beaad011de3b3b7e650
Phillip Edwards <phillip@pulumi.com>
2023-09-26 07:21:58 -0700

    added sql db to program

--- azure-cs-appservice-docker/Program.cs
+++ azure-cs-appservice-docker/Program.cs
@@ -3,8 +3,8 @@ using System;
 using Pulumi;
 using System.Linq;
 using Pulumi.AzureNative.ContainerRegistry;
-using Pulumi.AzureNative.ContainerRegistry.Inputs;
 using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Sql;
 using Pulumi.AzureNative.Web;
 using Pulumi.AzureNative.Web.Inputs;
 using Pulumi.Docker;
@@ -13,6 +13,28 @@ using System.Collections.Generic;
 return await Pulumi.Deployment.RunAsync(() =>
 {
     var resourceGroup = new ResourceGroup("appservice-docker-rg");
+
+    var config = new Pulumi.Config();
+    var username = config.Get("sqlUsername") ?? "sqladminuser";
+    var password = config.RequireSecret("sqlPassword");
+
+    var sqlServer = new Server("sqlserver", new ServerArgs
+    {
+        ResourceGroupName = resourceGroup.Name,
+        AdministratorLogin = username,
+        AdministratorLoginPassword = password,
+        Version = "12.0",
+    });
+
+    var database = new Database("db", new DatabaseArgs
+    {
+        ResourceGroupName = resourceGroup.Name,
+        ServerName = sqlServer.Name,
+        Sku = new Pulumi.AzureNative.Sql.Inputs.SkuArgs
+        {
+            Name = "S0"
+        }
+    });
 
     var plan = new AppServicePlan("linux-apps", new AppServicePlanArgs
     {
@@ -27,41 +49,17 @@ return await Pulumi.Deployment.RunAsync(() =>
     });
 
     //
-    // Scenario 1: deploying an image from Docker Hub.
-    // The example uses a HelloWorld application written in Go.
-    // Image: https://hub.docker.com/r/microsoft/azure-appservices-go-quickstart/
-    //
-    var imageInDockerHub = "microsoft/azure-appservices-go-quickstart";
-
-    var helloApp = new WebApp("hello-app", new WebAppArgs
-    {
-        ResourceGroupName = resourceGroup.Name,
-        ServerFarmId = plan.Id,
-        SiteConfig = new SiteConfigArgs
-        {
-            AppSettings = new[]
-            {
-                    new NameValuePairArgs
-                    {
-                        Name = "WEBSITES_ENABLE_APP_SERVICE_STORAGE",
-                        Value = "false"
-                    }
-                },
-            AlwaysOn = true,
-            LinuxFxVersion = $"DOCKER|{imageInDockerHub}"
-        },
-        HttpsOnly = true
-    });
-
-    //
-    // Scenario 2: deploying a custom image from Azure Container Registry.
+    // deploying a custom image from Azure Container Registry.
     //
     var customImage = "node-app";
 
     var registry = new Registry("myregistry", new RegistryArgs
     {
         ResourceGroupName = resourceGroup.Name,
-        Sku = new SkuArgs { Name = "Basic" },
+        Sku = new Pulumi.AzureNative.ContainerRegistry.Inputs.SkuArgs
+        {
+            Name = Pulumi.AzureNative.ContainerRegistry.SkuName.Basic,
+        },
         AdminUserEnabled = true
     });
 
@@ -72,9 +70,10 @@ return await Pulumi.Deployment.RunAsync(() =>
         ImageName = Output.Format($"{registry.LoginServer}/{customImage}:v1.0.0"),
         Build = new Pulumi.Docker.Inputs.DockerBuildArgs
         {
-            Context = $"./{customImage}"
+            Context = $"./{customImage}",
+            Platform = "linux/amd64"
         },
-        Registry = new Pulumi.Docker.Inputs.RegistryArgs 
+        Registry = new Pulumi.Docker.Inputs.RegistryArgs
         {
             Server = registry.LoginServer,
             Username = credentials.username,
@@ -90,32 +89,41 @@ return await Pulumi.Deployment.RunAsync(() =>
         {
             AppSettings = new[]
             {
-                    new NameValuePairArgs
-                    {
-                        Name = "WEBSITES_ENABLE_APP_SERVICE_STORAGE",
-                        Value = "false"
-                    },
-                    new NameValuePairArgs
-                    {
-                        Name = "DOCKER_REGISTRY_SERVER_URL",
-                        Value = Output.Format($"https://{registry.LoginServer}")
-                    },
-                    new NameValuePairArgs
-                    {
-                        Name = "DOCKER_REGISTRY_SERVER_USERNAME",
-                        Value = credentials.username,
-                    },
-                    new NameValuePairArgs
-                    {
-                        Name = "DOCKER_REGISTRY_SERVER_PASSWORD",
-                        Value = credentials.password,
-                    },
-                    new NameValuePairArgs
-                    {
-                        Name = "WEBSITES_PORT",
-                        Value = "80" // Our custom image exposes port 80. Adjust for your app as needed.
-                    }
+                new NameValuePairArgs
+                {
+                    Name = "WEBSITES_ENABLE_APP_SERVICE_STORAGE",
+                    Value = "false"
                 },
+                new NameValuePairArgs
+                {
+                    Name = "DOCKER_REGISTRY_SERVER_URL",
+                    Value = Output.Format($"https://{registry.LoginServer}")
+                },
+                new NameValuePairArgs
+                {
+                    Name = "DOCKER_REGISTRY_SERVER_USERNAME",
+                    Value = credentials.username,
+                },
+                new NameValuePairArgs
+                {
+                    Name = "DOCKER_REGISTRY_SERVER_PASSWORD",
+                    Value = credentials.password,
+                },
+                new NameValuePairArgs
+                {
+                    Name = "WEBSITES_PORT",
+                    Value = "80" // Our custom image exposes port 80. Adjust for your app as needed.
+                }
+            },
+            ConnectionStrings =
+            {
+                new ConnStringInfoArgs
+                {
+                    Name = "db",
+                    Type = ConnectionStringType.SQLAzure,
+                    ConnectionString = GetConnectionString(sqlServer.Name, database.Name, username, password),
+                },
+            },
             AlwaysOn = true,
             LinuxFxVersion = Output.Format($"DOCKER|{myImage.ImageName}")
         },
@@ -124,7 +132,6 @@ return await Pulumi.Deployment.RunAsync(() =>
 
     return new Dictionary<string, object?>
     {
-        ["HelloEndpoint"] = Output.Format($"https://{helloApp.DefaultHostName}/hello"),
         ["GetStartedEndpoint"] = Output.Format($"https://{getStartedApp.DefaultHostName}"),
     };
 });
@@ -140,4 +147,14 @@ return await Pulumi.Deployment.RunAsync(() =>
     var adminUsername = credentials.Apply(c => c.Username ?? "");
     var adminPassword = credentials.Apply(c => Output.CreateSecret(c.Passwords.First().Value ?? ""));
     return (adminUsername, adminPassword);
+}
+
+Output<string> GetConnectionString(Output<string> name, Output<string> dbName, string username, Output<string> password)
+{
+    return Output.Tuple(name, dbName, password).Apply(t =>
+    {
+        (string server, string database, string pwd) = t;
+        return
+            $"Server= tcp:{server}.database.windows.net;initial catalog={database};userID={username};password={pwd};Min Pool Size=0;Max Pool Size=30;Persist Security Info=true;";
+    });
 }
