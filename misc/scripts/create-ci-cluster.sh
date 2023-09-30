7691e2888e6e1284d4c61fab02cc2a4806fb037a
Christian Nunciato <c@nunciato.org>
2023-09-29 18:00:23 -0700

    Update create-ci-cluster.sh

--- misc/scripts/create-ci-cluster.sh
+++ misc/scripts/create-ci-cluster.sh
@@ -5,9 +5,6 @@ echo Creating ephemeral Kubernetes cluster for CI testing...
 
 pushd misc/scripts/testinfra/
 yarn
-pulumi whoami -v
-echo "${1}"
-pulumi login
 pulumi stack init --non-interactive ${1}
 pulumi up --skip-preview --yes
 
