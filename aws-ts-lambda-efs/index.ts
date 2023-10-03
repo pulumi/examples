49ee3a0a9fa9e46d6550e0f17499b5673f559135
Thomas Kappler <tkappler@pulumi.com>
2023-10-03 12:41:33 +0200

    Upgrade aws-ts-lambda-efs to aws v6

--- aws-ts-lambda-efs/index.ts
+++ aws-ts-lambda-efs/index.ts
@@ -1,6 +1,7 @@
 // Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
 import * as aws from "@pulumi/aws";
+import * as apigateway from "@pulumi/aws-apigateway";
 import * as awsx from "@pulumi/awsx";
 import * as cp from "child_process";
 import * as fs from "fs";
@@ -8,19 +9,20 @@ import * as fs from "fs";
 export = async () => {
 
     // VPC
-    const vpc = new awsx.ec2.Vpc("vpc", { subnets: [{ type: "private" }, { type: "public" }] });
-    const subnetIds = await vpc.publicSubnetIds;
+    const vpc = new awsx.ec2.Vpc("vpc", {}); // subnets default to one private, one public
 
     // EFS
     const filesystem = new aws.efs.FileSystem("filesystem");
-    const targets = [];
-    for (let i = 0; i < subnetIds.length; i++) {
-        targets.push(new aws.efs.MountTarget(`fs-mount-${i}`, {
-            fileSystemId: filesystem.id,
-            subnetId: subnetIds[i],
-            securityGroups: [vpc.vpc.defaultSecurityGroupId],
-        }));
-    }
+    const targets: aws.efs.MountTarget[] = [];
+    vpc.publicSubnetIds.apply(ids => {
+        for (let i = 0; i < ids.length; i++) {
+            targets.push(new aws.efs.MountTarget(`fs-mount-${i}`, {
+                fileSystemId: filesystem.id,
+                subnetId: ids[i],
+                securityGroups: [vpc.vpc.defaultSecurityGroupId],
+            }));
+        }
+    });
     const ap = new aws.efs.AccessPoint("ap", {
         fileSystemId: filesystem.id,
         posixUser: { uid: 1000, gid: 1000 },
@@ -28,7 +30,7 @@ export = async () => {
     }, { dependsOn: targets });
 
     // Lambda
-    function efsvpcCallback(name: string, f: aws.lambda.Callback<awsx.apigateway.Request, awsx.apigateway.Response>) {
+    function efsvpcCallback(name: string, f: aws.lambda.Callback<unknown, {statusCode: number, body: string}>) {
         return new aws.lambda.CallbackFunction(name, {
             policies: [aws.iam.ManagedPolicy.AWSLambdaVPCAccessExecutionRole, aws.iam.ManagedPolicy.LambdaFullAccess],
             vpcConfig: {
@@ -41,12 +43,13 @@ export = async () => {
     }
 
     // API Gateway
-    const api = new awsx.apigateway.API("api", {
+    const api = new apigateway.RestAPI("api", {
         routes: [
             {
                 method: "GET", path: "/files/{filename+}", eventHandler: efsvpcCallback("getHandler", async (ev, ctx) => {
                     try {
-                        const f = "/mnt/storage/" + ev.pathParameters!.filename;
+                        const event = <any>ev;
+                        const f = "/mnt/storage/" + event.pathParameters!.filename;
                         const data = fs.readFileSync(f);
                         return {
                             statusCode: 200,
@@ -60,8 +63,9 @@ export = async () => {
             {
                 method: "POST", path: "/files/{filename+}", eventHandler: efsvpcCallback("uploadHandler", async (ev, ctx) => {
                     try {
-                        const f = "/mnt/storage/" + ev.pathParameters!.filename;
-                        const data = new Buffer(ev.body!, "base64");
+                        const event = <any>ev;
+                        const f = "/mnt/storage/" + event.pathParameters!.filename;
+                        const data = Buffer.from(event.body!, "base64");
                         fs.writeFileSync(f, data);
                         return {
                             statusCode: 200,
@@ -74,7 +78,8 @@ export = async () => {
             },
             {
                 method: "POST", path: "/", eventHandler: efsvpcCallback("execHandler", async (ev, ctx) => {
-                    const cmd = new Buffer(ev.body!, "base64").toString();
+                    const event = <any>ev;
+                    const cmd = Buffer.from(event.body!, "base64").toString();
                     const buf = cp.execSync(cmd);
                     return {
                         statusCode: 200,
@@ -86,7 +91,7 @@ export = async () => {
     });
 
     // ECS Cluster
-    const cluster = new awsx.ecs.Cluster("cluster", { vpc: vpc });
+    const cluster = new aws.ecs.Cluster("cluster");
     const efsVolumeConfiguration: aws.types.input.ecs.TaskDefinitionVolumeEfsVolumeConfiguration = {
         fileSystemId: filesystem.id,
         authorizationConfig: { accessPointId: ap.id },
@@ -96,9 +101,10 @@ export = async () => {
 
     // Fargate Service
     const nginx = new awsx.ecs.FargateService("nginx", {
-        cluster: cluster,
+        cluster: cluster.arn,
         taskDefinitionArgs: {
             container: {
+                name: "nginx",
                 image: "nginx",
                 memory: 128,
                 portMappings: [{ containerPort: 80, hostPort: 80, protocol: "tcp" }],
@@ -106,8 +112,10 @@ export = async () => {
             },
             volumes: [{ name: "efs", efsVolumeConfiguration }],
         },
-        securityGroups: [vpc.vpc.defaultSecurityGroupId, ...cluster.securityGroups],
-        subnets: vpc.publicSubnetIds,
+        networkConfiguration: {
+            subnets: vpc.publicSubnetIds,
+            securityGroups: [vpc.vpc.defaultSecurityGroupId],
+        },
         platformVersion: "1.4.0",
     });
 
