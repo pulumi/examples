ba51d50131e2b59c29166929d797d22efdc56306
Thomas Kappler <tkappler@pulumi.com>
2023-10-05 08:44:57 +0200

    Fix aws-ts-apigateway compilation

--- aws-ts-apigateway/index.ts
+++ aws-ts-apigateway/index.ts
@@ -1,7 +1,10 @@
 // Copyright 2016-2019, Pulumi Corporation.  All rights reserved.
 
+import * as dynamoClient from "@aws-sdk/client-dynamodb";
+import * as dynamoLib from "@aws-sdk/lib-dynamodb";
+
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as apigateway from "@pulumi/aws-apigateway";
 
 // Create a mapping from 'route' to a count
 const counterTable = new aws.dynamodb.Table("counterTable", {
@@ -14,39 +17,46 @@ const counterTable = new aws.dynamodb.Table("counterTable", {
     writeCapacity: 5,
 });
 
+const getHandler = new aws.lambda.CallbackFunction("GET", {
+    policies: [aws.iam.ManagedPolicy.AWSLambdaVPCAccessExecutionRole, aws.iam.ManagedPolicy.LambdaFullAccess],
+    callback: async (ev, ctx) => {
+        const event = <any>ev;
+        const route = event.pathParameters!["route"];
+        console.log(`Getting count for '${route}'`);
+
+        const dynoClient = new dynamoClient.DynamoDBClient({});
+        const doc = dynamoLib.DynamoDBDocument.from(dynoClient);
+
+        // get previous value and increment
+        // reference outer `counterTable` object
+        const tableData = await doc.get({
+            TableName: counterTable.name.get(),
+            Key: { id: route },
+            ConsistentRead: true,
+        });
+
+        const value = tableData.Item;
+        let count = (value && value.count) || 0;
+
+        await doc.put({
+            TableName: counterTable.name.get(),
+            Item: { id: route, count: ++count },
+        });
+
+        console.log(`Got count ${count} for '${route}'`);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ route, count }),
+        };
+    },
+});
+
 // Create an API endpoint
-const endpoint = new awsx.apigateway.API("hello-world", {
+const endpoint = new apigateway.RestAPI("hello-world", {
     routes: [{
         path: "/{route+}",
         method: "GET",
-        eventHandler: async (event) => {
-            const route = event.pathParameters!["route"];
-            console.log(`Getting count for '${route}'`);
-
-            const client = new aws.sdk.DynamoDB.DocumentClient();
-
-            // get previous value and increment
-            // reference outer `counterTable` object
-            const tableData = await client.get({
-                TableName: counterTable.name.get(),
-                Key: { id: route },
-                ConsistentRead: true,
-            }).promise();
-
-            const value = tableData.Item;
-            let count = (value && value.count) || 0;
-
-            await client.put({
-                TableName: counterTable.name.get(),
-                Item: { id: route, count: ++count },
-            }).promise();
-
-            console.log(`Got count ${count} for '${route}'`);
-            return {
-                statusCode: 200,
-                body: JSON.stringify({ route, count }),
-            };
-        },
+        eventHandler: getHandler,
     }],
 });
 
