b50587639f22d2f81d367aae61eadd66bdcd2115
Bryce Lampe <bryce@pulumi.com>
2024-01-11 13:00:58 -0800

    pin it

--- aws-java-webserver/src/main/java/webserver/App.java
+++ aws-java-webserver/src/main/java/webserver/App.java
@@ -25,7 +25,7 @@ public class App {
         final var ami = Ec2Functions.getAmi(GetAmiArgs.builder()
                 .filters(GetAmiFilterArgs.builder()
                         .name("name")
-                        .values("amzn2-ami-hvm-*-x86_64-ebs")
+                        .values("amzn2-ami-hvm-2.0.20231218.0-x86_64-ebs")
                         .build())
                 .owners("137112412989")
                 .mostRecent(true)
