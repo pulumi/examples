01694daa3b9f80302c1ab49fd79b2d268836493f
diana esteves <diana@pulumi.com>
2024-05-07 16:19:03 -0500

    adds a docker build cloud example

---
+++ docker-ts-image-build/index.ts
@@ -0,0 +1,55 @@
+// Pulumi program to build a docker image with DBC
+
+// This Pulumi template is meant to be copied via:
+// $ pulumi new https://<full repo path to this folder>
+// Once copied to your machine, feel free to edit as needed.
+
+// How to run this program in your terminal:
+// $ pulumi up
+
+// Pre-requisites:
+// - Docker Build Cloud (DBC) 
+// - Docker Desktop / CLI
+// - Pulumi CLI (https://www.pulumi.com/docs/get-started/install/)
+// - *Recommended* Pulumi Cloud account (https://app.pulumi.com/signup)
+// - npm (https://www.npmjs.com/get-npm)
+
+// Import required libraries, update package.json if you add more.
+// (Recommended to run `npm update --save` after adding more libraries)
+import * as docker_build from "@pulumi/docker-build";
+import * as pulumi from "@pulumi/pulumi"; // Required for Config
+
+// Read the current stack configuration, see Pulumi.<STACK>.yaml file
+// Configuration values can be set with the pulumi config set command
+// OR by editing the Pulumi.<STACK>.yaml file.
+// OR during the pulumi new wizard.
+const config = new pulumi.Config();
+// Docker Build Cloud (DBC) builder name
+const builder = config.require("builder"); // Example, "cloud-pulumi-my-cool-builder"
+
+const image = new docker_build.Image("image", {
+    // Enable exec to run a custom docker-buildx binary with support
+    // for Docker Build Cloud (DBC). 
+    exec: true,
+    // Configures the name of your existing buildx builder to use.
+    builder: {
+        name: builder, // Example, "cloud-pulumi-my-cool-builder",
+    },
+    push: false,
+    // Silence warning: "No exports were specified so the build
+    // will only remain in the local build cache."
+    exports: [
+        {
+            cacheonly: {},
+        }
+    ],
+    //
+    // Other parameters
+    //
+    // Tag our image 
+    tags: [`nginx:latest`],
+    // The Dockerfile resides in the app directory for this example.
+    context: {
+        location: "app",
+    },
+});
