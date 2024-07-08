e7ba319402c38de473df3b385af27a2ac163e48e
Christian Nunciato <c@nunciato.org>
2024-07-08 10:46:47 -0700

    Do some cleanup (#1663)
    Does a bit of refactoring to make our GHA jobs much more maintainable:
    
    * Pulls common setup code into a composite action to reduce a _lot_ of
    duplication
    * Consolidates multiple workflows into a single workflow that responds
    to multiple events
    * Deletes two long-failing workflows that aren't used anymore
    * Simplifies and localizes the commands that run the unit tests
    * Deletes some unneeded files in the project root
    * Cleans up some naming for improved readability in the Actions list
    
    Also fixes a bug: apparently the Python unit tests haven't even been
    running! 🙀
    
    Before:
    
    ```
    Ran 0 tests in 0.000s
    ```
    
    Now:
    
    ```
    3 passed, 3 warnings in 0.36s
    ```
    
    Fixes #1661.

--- .github/workflows/README.md
+++
@@ -1,4 +0,0 @@
-## Please Note
-
-The GitHub Actions that are present in this repository are scaffolded from a central generator
-therefore, any manual changes will be overwritten on the next codegen
--- misc/scripts/create-ci-cluster.sh
+++ misc/scripts/create-ci-cluster.sh
@@ -4,8 +4,7 @@ set -o errexit -o pipefail
 echo Creating ephemeral Kubernetes cluster for CI testing...
 
 pushd misc/scripts/testinfra/
-yarn
-pulumi stack init --non-interactive ${1}
-pulumi up --skip-preview --yes
-
+    pulumi install
+    pulumi stack init "$1"
+    pulumi up --skip-preview
 popd
