e57f8b6d2011e2791faec289b336df6b95bdbc68
Daniel Bradley <daniel@pulumi.com>
2024-10-16 14:31:09 +0100

    Delete complex dotnet cosmos example
    Currently has a lot of build failures. The Typescript version is still available.

--- classic-azure-cs-cosmosapp-component/Extensions.cs
+++
@@ -1,8 +0,0 @@
-﻿public static class Extensions
-{
-    public static string Truncate(this string value, int maxLength)
-    {
-        if (string.IsNullOrEmpty(value)) return value;
-        return value.Length <= maxLength ? value : value.Substring(0, maxLength);
-    }
-}
