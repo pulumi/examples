34f07dd51d221e174ee6247f2c631f388c351e27
Richard Shade <richard@pulumi.com>
2025-01-09 16:18:22 -0600

    Converting to BucketV2, fixes #1716

--- testing-integration-py/resource_s3.py
+++ testing-integration-py/resource_s3.py
@@ -1,14 +1,14 @@
 import pulumi
 from pulumi_aws import s3
 
-BUCKET_NAME = 'pulumi-it-bucket'
-OUTPUT_KEY_BUCKET_NAME = 'bucket_name'
-OUTPUT_KEY_REGION = 'region'
+BUCKET_NAME = "pulumi-it-bucket"
+OUTPUT_KEY_BUCKET_NAME = "bucket_name"
+OUTPUT_KEY_REGION = "region"
 
 
 def create_s3_bucket():
     # Create an AWS resource (S3 Bucket)
-    bucket = s3.Bucket(BUCKET_NAME)
+    bucket = s3.BucketV2(BUCKET_NAME)
 
     # Export the value of the bucket
     pulumi.export(OUTPUT_KEY_BUCKET_NAME, bucket.bucket)
