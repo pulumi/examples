6b49ef4cacbf5474ec3eac3e2f030bd8dcebba5b
Richard Shade <richard@pulumi.com>
2025-01-10 12:01:36 -0600

    Reformatting

--- azure-py-functions/__main__.py
+++ azure-py-functions/__main__.py
@@ -8,25 +8,24 @@ from pulumi import Output
 resource_group = resources.ResourceGroup('resourcegroup_functions_py')
 
 # Create a Storage Account
-account = storage.StorageAccount('storageaccount',
+account = storage.StorageAccount(
+    'storageaccount',
     resource_group_name=resource_group.name,
-    sku=storage.SkuArgs(name=storage.SkuName.STANDARD_LRS,),
-    kind=storage.Kind.STORAGE_V2)
+    sku=storage.SkuArgs(
+        name=storage.SkuName.STANDARD_LRS,
+    ),
+    kind=storage.Kind.STORAGE_V2,
+)
 
 # Create a consumption plan
 # Consumption plan must be linux for python: https://docs.microsoft.com/en-us/azure/azure-functions/functions-scale#operating-systemruntime
-plan = web.AppServicePlan("consumption-plan",
+plan = web.AppServicePlan(
+    "consumption-plan",
     resource_group_name=resource_group.name,
     location=resource_group.location,
-    kind = "functionapp",
-    reserved=True, # This is an Azure Requirement for PYTHON. The function can only run on Linux. 
-    sku=web.SkuDescriptionArgs(
-        name="Y1",
-        tier="Dynamic",
-        size="Y1",
-        family="Y",
-        capacity=0
-    )
+    kind="functionapp",
+    reserved=True,  # This is an Azure Requirement for PYTHON. The function can only run on Linux.
+    sku=web.SkuDescriptionArgs(name="Y1", tier="Dynamic", size="Y1", family="Y", capacity=0),
 )
 
 # Export the Azure Resource Group
@@ -39,41 +38,53 @@ pulumi.export('storageaccount', account.name)
 pulumi.export('consumptionplan', plan.name)
 
 # List of storage account keys
-storageAccountKeys = pulumi.Output.all(resource_group.name, account.name).apply(lambda args:  storage.list_storage_account_keys(resource_group_name=args[0],account_name=args[1]))
+storageAccountKeys = pulumi.Output.all(resource_group.name, account.name).apply(
+    lambda args: storage.list_storage_account_keys(
+        resource_group_name=args[0], account_name=args[1]
+    )
+)
 # Primary storage account key
 primaryStorageKey = storageAccountKeys.apply(lambda accountKeys: accountKeys.keys[0].value)
 # Build a storage connection string out of it:
-storageConnectionString = Output.concat("DefaultEndpointsProtocol=https;AccountName=",account.name,";AccountKey=",primaryStorageKey)
+storageConnectionString = Output.concat(
+    "DefaultEndpointsProtocol=https;AccountName=", account.name, ";AccountKey=", primaryStorageKey
+)
 
 
 # Export the storageacountkey as a secret
 pulumi.export("storageaccountkeys", pulumi.Output.secret(storageAccountKeys))
 # Export the primarystoragekey as a secret
-pulumi.export('primarystoragekey',  pulumi.Output.secret(primaryStorageKey )) 
+pulumi.export('primarystoragekey', pulumi.Output.secret(primaryStorageKey))
 # Export the storageconnectionstring  as a secret
 pulumi.export('storageconnectionstring', pulumi.Output.secret(storageConnectionString))
 
 # Create the functionapp
-app = web.WebApp("functionapp", 
+app = web.WebApp(
+    "functionapp",
     resource_group_name=resource_group.name,
     location=resource_group.location,
     kind="functionapp",
     reserved=True,
     server_farm_id=plan.id,
-        site_config=web.SiteConfigArgs(
+    site_config=web.SiteConfigArgs(
         app_settings=[
-            web.NameValuePairArgs(name = "runtime", value="python"),
-            web.NameValuePairArgs(name = "FUNCTIONS_WORKER_RUNTIME", value ="python"),
-            web.NameValuePairArgs(name = "FUNCTIONS_EXTENSION_VERSION", value="~3"),
-            web.NameValuePairArgs(name = "AzureWebJobsStorage", value=storageConnectionString),
-            web.NameValuePairArgs(name=  "WEBSITE_RUN_FROM_PACKAGE", value="https://github.com/pulumi/examples/raw/master/azure-py-functions/app/HelloWithPython.zip"),
+            web.NameValuePairArgs(name="runtime", value="python"),
+            web.NameValuePairArgs(name="FUNCTIONS_WORKER_RUNTIME", value="python"),
+            web.NameValuePairArgs(name="FUNCTIONS_EXTENSION_VERSION", value="~3"),
+            web.NameValuePairArgs(name="AzureWebJobsStorage", value=storageConnectionString),
+            web.NameValuePairArgs(
+                name="WEBSITE_RUN_FROM_PACKAGE",
+                value="https://github.com/pulumi/examples/raw/master/azure-py-functions/app/HelloWithPython.zip",
+            ),
         ],
-    )
+    ),
 )
 
 # Export the function
 pulumi.export('function_app', app.name)
 
 # Full endpoint of your Function App
-function_endpoint = app.default_host_name.apply(lambda default_host_name: f"https://{default_host_name}/api/HelloWithPython")
+function_endpoint = app.default_host_name.apply(
+    lambda default_host_name: f"https://{default_host_name}/api/HelloWithPython"
+)
 pulumi.export('endpoint', function_endpoint)
--- azure-py-functions/app/HelloWithPython/__init__.py
+++ azure-py-functions/app/HelloWithPython/__init__.py
@@ -5,4 +5,6 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    return func.HttpResponse(f"Hello from Python in Pulumi! You have stood up a serverless function in Azure!")
+    return func.HttpResponse(
+        f"Hello from Python in Pulumi! You have stood up a serverless function in Azure!"
+    )
