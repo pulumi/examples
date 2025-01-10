beeac20beb8be8cc066b14f1020dbd4bb72c856c
Richard Shade <131337+rshade@users.noreply.github.com>
2025-01-10 15:33:43 -0600

    Converting to BucketV2, fixes #1716 (#1769)

--- testing-integration-py/resource_s3.py
+++ testing-integration-py/resource_s3.py
@@ -8,7 +8,7 @@ OUTPUT_KEY_REGION = "region"
 
 def create_s3_bucket():
     # Create an AWS resource (S3 Bucket)
-    bucket = s3.Bucket(BUCKET_NAME)
+    bucket = s3.BucketV2(BUCKET_NAME)
 
     # Export the value of the bucket
     pulumi.export(OUTPUT_KEY_BUCKET_NAME, bucket.bucket)
--- testing-unit-ts/mocha/bucket_pair.ts
+++ testing-unit-ts/mocha/bucket_pair.ts
@@ -3,17 +3,17 @@ import * as pulumi from "@pulumi/pulumi";
 
 export class BucketPair extends pulumi.ComponentResource {
 
-    contentBucket: aws.s3.Bucket;
-    logsBucket: aws.s3.Bucket;
+    contentBucket: aws.s3.BucketV2;
+    logsBucket: aws.s3.BucketV2;
 
     constructor(contentBucketName: string, logsBucketName: string, opts: any) {
         super("pulumi:examples:BucketPair", "BucketPair", {}, opts);
 
-        this.contentBucket = new aws.s3.Bucket("contentBucket", {
+        this.contentBucket = new aws.s3.BucketV2("contentBucket", {
             bucket: contentBucketName,
         }, { parent: this });
 
-        this.logsBucket = new aws.s3.Bucket("logsBucket", {
+        this.logsBucket = new aws.s3.BucketV2("logsBucket", {
             bucket: logsBucketName,
         }, { parent: this });
 
