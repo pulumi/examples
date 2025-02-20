8cd9c93dee5f879cdc4d4ac610ee6510e9b763b5
Richard Shade <131337+rshade@users.noreply.github.com>
2025-02-20 14:40:44 -0600

    Updating all packages at the same time and pinning the broken @aws-cd… (#2025)
    …k/cloud-assembly-schema

--- testing-unit-cs-mocks/Testing.cs
+++ testing-unit-cs-mocks/Testing.cs
@@ -13,38 +13,43 @@ namespace UnitTesting
         public Task<(string? id, object state)> NewResourceAsync(MockResourceArgs args)
         {
             var outputs = ImmutableDictionary.CreateBuilder<string, object>();
-            
+
             // Forward all input parameters as resource outputs, so that we could test them.
-            outputs.AddRange(args.Inputs);
-            
+            if (args.Inputs != null)
+            {
+                outputs.AddRange(args.Inputs);
+            }
+
             // Set the name to resource name if it's not set explicitly in inputs.
-            if (!args.Inputs.ContainsKey("name"))
+            if (args.Inputs?.ContainsKey("name") != true && args.Name != null)
+            {
                 outputs.Add("name", args.Name);
-            
+            }
+
             if (args.Type == "azure-native:storage:Blob")
             {
                 // Assets can't directly go through the engine.
                 // We don't need them in the test, so blank out the property for now.
                 outputs.Remove("source");
             }
-            
+
             // For a Storage Account...
-            if (args.Type == "azure-native:storage:StorageAccount")
+            if (args.Type == "azure-native:storage:StorageAccount" && args.Name != null)
             {
                 // ... set its web endpoint property.
-                // Normally this would be calculated by Azure, so we have to mock it. 
-                outputs.Add("primaryEndpoints", new Dictionary<string, string> 
-                { 
-                    { "web", $"https://{args.Name}.web.core.windows.net" },
-                }.ToImmutableDictionary());
+                // Normally this would be calculated by Azure, so we have to mock it.
+                var endpoints = new Dictionary<string, string>
+                {
+                    { "web", $"https://{args.Name}.web.core.windows.net" }
+                };
+                outputs.Add("primaryEndpoints", endpoints.ToImmutableDictionary());
             }
 
             // Default the resource ID to `{name}_id`.
-            // We could also format it as `/subscription/abc/resourceGroups/xyz/...` if that was important for tests.
-            args.Id ??= $"{args.Name}_id";
-            return Task.FromResult((args.Id, (object)outputs));
+            args.Id ??= args.Name != null ? $"{args.Name}_id" : "unknown_id";
+            return Task.FromResult((args.Id, (object)outputs.ToImmutable()));
         }
-        
+
         public Task<object> CallAsync(MockCallArgs args)
         {
             // We don't use this method in this particular test suite.
@@ -52,7 +57,7 @@ namespace UnitTesting
             return Task.FromResult((object)args.Args);
         }
     }
-    
+
     public static class TestingExtensions
     {
         /// <summary>
